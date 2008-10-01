9e1616a159bb1dcf735ef35383026523332fe1e6
limblab <miller.limblab@gmail.com>
2008-10-01 23:05:18 +0000

    Added firing rate pdf calculation in polar coordinates and a library function (from central file exchange).
    git-svn-id: http://citadel.physiology.northwestern.edu/svn/s1_analysis@11 984b1e3c-1c57-0410-ae62-c18f90d3b645

---
+++ vel_pdf_polar.m
@@ -0,0 +1,18 @@
+function [pdf theta rho] = vel_pdf_polar(vel)
+
+k_width = 10; % sigma of gaussian kernel
+
+[rho theta] = meshgrid(0:.5:50, 0:pi/32:2*pi);
+x = rho .* cos(theta);
+y = rho .* sin(theta);
+
+pdf = zeros(size(x));
+tic
+for i = 1:size(x,1)
+    disp(x(1,i))
+    toc
+    for j = 1:size(x,2)
+        dist = sqrt( (x(i,j)-vel(:,1)).^2 + (y(i,j)-vel(:,2)).^2 );
+        pdf(i,j) = sum(normpdf(dist,0,k_width));
+    end
+end
