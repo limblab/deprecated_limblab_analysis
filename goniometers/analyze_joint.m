c206cb9342308a7e050f940954d36c481c87480b
Brian London <brianmlondon@gmail.com>
2008-10-31 20:36:23 +0000

    can now analyze joint for all neurons in a file using goniometers/goniometers.m.
    git-svn-id: http://citadel.physiology.northwestern.edu/svn/s1_analysis@22 984b1e3c-1c57-0410-ae62-c18f90d3b645

--- goniometers/analyze_joint.m
+++ goniometers/analyze_joint.m
@@ -1,4 +1,4 @@
-function [f, p] = analyze_joint(bdf, chan, unit, start, stop)
+function [f, p] = analyze_joint(bdf, chan, unit, start, stop, quiet)
 % ANALYZE_JOINT - Performs joint level analysis based on goniometer signals
 %   ANALYZE_JOINT(BDF, CHAN, UNIT, START, STOP) performs joint level
 %   analysis on the specified channel and unit between START and STOP
@@ -8,6 +8,11 @@ function [f, p] = analyze_joint(bdf, chan, unit, start, stop)
 addpath ../spike
 addpath ../bdf
 addpath ../lib
+
+disp(nargchk(5, 6, nargin));
+if nargin < 6
+    quiet = 0;
+end
 
 % for now we are going to use GoniometerX
 g_chan = find(strcmp('GoniometerY', bdf.raw.analog.channels));
@@ -19,9 +24,9 @@ stop_idx  = find(g_time > stop, 1, 'first');
 
 [b,a] = butter(8, 10/bdf.raw.analog.adfreq);
 g = filtfilt(b, a, bdf.raw.analog.data{g_chan});
-dg = diff(g);
+%dg = diff(g);
 g = g(start_idx:stop_idx);
-dg = dg(start_idx:stop_idx);
+%dg = dg(start_idx:stop_idx);
 t = g_time(start_idx:stop_idx);
 
 % get units
@@ -29,13 +34,15 @@ s = get_unit(bdf, chan, unit);
 b = train2bins(s, t);
 b(1) = 0; % cover a bug in train2bins
 
-[means, bins, errors, ns, ts] = plot_posterior(g, b);
+[means, bins, errors, ns, ts] = plot_posterior(g, b, quiet);
 %subplot(2,2,1), plot_posterior(g, b);
 %subplot(2,2,2), plot_posterior(dg, b);
 %subplot(2,2,3), plot(t,g,'r-',t,b,'k-');
 %subplot(2,2,4), plot(t,dg,'r-',t,b,'k-');
 
-suptitle(sprintf('Unit: %d-%d', chan, unit));
+if ~quiet
+    suptitle(sprintf('Unit: %d-%d', chan, unit));
+end
 
 % get modified f-statistic on tuning curve
 f = var(means) / mean(errors);
@@ -44,7 +51,7 @@ f = var(means) / mean(errors);
 ps = binocdf( ns, ts, sum(ns) / sum(ts) );
 ps(ps > .5) = 1 - ps(ps > .5);
 ps = 2*ps;
-p = prod(ps);
+p = min(ps) < .05 / length(ps);
 
 % cleanup
 rmpath ../spike
--- goniometers/plot_posterior.m
+++ goniometers/plot_posterior.m
@@ -1,4 +1,4 @@
-function [means, bins, errors, ni, Ti] = plot_posterior(signal, spiketrain)
+function [means, bins, errors, ni, Ti] = plot_posterior(signal, spiketrain, quiet)
 % PLOT_POSTERIOR - Plots a posterior distribution of firing rate as a
 %                  function  fo the specified signal
 %
@@ -6,8 +6,16 @@ function [means, bins, errors, ni, Ti] = plot_posterior(signal, spiketrain)
 %       as a function of SIGNAL divided into 8 bins.  SPIKETRAIN is the 
 %       same length as signal and contains the number of spikes within each 
 %       sample.
+%
+%   PLOT_POSTERIOR( ... , QUIET ) - does not display the resulting plot if
+%   QUIET is true.
 
 % $Id$
+
+disp(nargchk(2, 3, nargin));
+if nargin < 3
+    quiet = 0;
+end
 
 b = spiketrain;
 
@@ -24,7 +32,9 @@ ns = hist(sig_spike, sbins);
 rate = 1000 * ns ./ n;
 rate_err = 1000 * sqrt(ns) ./ n;
 
-errorbar(sbins(2:end-1), rate(2:end-1), rate_err(2:end-1), 'bo-');
+if ~quiet
+    errorbar(sbins(2:end-1), rate(2:end-1), rate_err(2:end-1), 'bo-');
+end
 
 means = rate(2:end-1);
 bins = sbins(2:end-1);
