81d2c66d7f1186c2a48da2f1645f52be3a421f13
Brian London <brianmlondon@gmail.com>
2009-02-12 20:56:30 +0000

    Hopefully working version of this file.
    git-svn-id: http://citadel.physiology.northwestern.edu/svn/s1_analysis@39 984b1e3c-1c57-0410-ae62-c18f90d3b645

--- lib/stxcorr.m
+++ lib/stxcorr.m
@@ -17,7 +17,7 @@ function [c, t, lag] = stxcorr(x, y, window_size, varargin);
 %   [C, T, LAG] = STXCORR(...) also returns LAG the timing of the peak of
 %   the cross corelation for each value of C.
 
-% $Id: $
+% $Id$
 
 % get input arguments
 overlap = 0;
@@ -27,16 +27,45 @@ if nargin > 3
 end
 
 if nargin > 4
-    window_size = varargin{2};
+    max_offset = varargin{2};
+end
+
+% ensure x and y are same length and other variables are scalar
+if (length(x) ~= length(y))
+    error('X and Y must be the same length');
+end
+if (~isscalar(window_size))
+    error('WINDOW_SIZE must be scalar');
+end
+if (~isscalar(overlap))
+    error('OVERLAP must be scalar');
+end
+if (~isscalar(max_offset))
+    error('MAX_OFFSET must be scalar');
 end
 
 % get window start and stop times
+num_windows = floor(length(x) / (window_size-overlap));
+starts = (0:num_windows-1) .* (window_size - overlap);
+stops = starts + window_size;
+centers = (starts + stops) ./ 2;
 
+% get xcorr for each window
+c = zeros(num_windows, 1);
+lag = zeros(num_windows, 1);
+for i = 1:num_windows
+    x_win = x(starts(i):stops(i));
+    y_win = y(starts(i):stops(i));
+    c_win = xcorr(x_win, y_win, 'coeff');
+    c_win_roi = c_win(window_size-max_offset:window_size+max_offset);
+        
+    peak = find(c_win_roi == max(c_win_roi), 1, 'first');
+    c(i) = c_win_roi(peak);
+    lag(i) = peak - max_offset;
+end
 
-
-
-
-
+% dummy outputs for debugging
+t = centers;
 
 
 
