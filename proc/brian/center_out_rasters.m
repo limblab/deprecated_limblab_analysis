915fb423a88d3591ce799e35196becdb713ba818
Brian London <brianmlondon@gmail.com>
2009-02-18 02:25:26 +0000

    Progress check in before going home for the night.
    git-svn-id: http://citadel.physiology.northwestern.edu/svn/s1_analysis@51 984b1e3c-1c57-0410-ae62-c18f90d3b645

--- proc/brian/center_out_rasters.m
+++ proc/brian/center_out_rasters.m
@@ -1,6 +1,6 @@
-% load multiple files
+% load multiple files and draw center out perturbation rasters
 
-chan = 9;
+chan = 15;
 cell = 1;
 
 filename = '../../../data_cache/Arthur_S1_006-1of10.mat';
@@ -10,21 +10,20 @@ load(filename);
 ul = unit_list(bdf);
 cell_idx = find(ul(:,1) == chan & ul(:,2) == cell);
 
-% spikes = [];
-% words = [];
-% 
-% disp('Loading files');
-% for i=1:10
-%     filename = sprintf('../../../data_cache/Arthur_S1_006-%dof10.mat', i);
-%     disp(filename);
-%     load(filename);
-%     spikes = [spikes; bdf.units(cell_idx).ts];
-%     words = [words; bdf.words];
-%     clear bdf filename;
-% end
+spikes = [];
+words = [];
+
+disp('Loading files');
+for i=1:10
+    filename = sprintf('../../../data_cache/Arthur_S1_006-%dof10.mat', i);
+    disp(filename);
+    load(filename);
+    spikes = [spikes; bdf.units(cell_idx).ts];
+    words = [words; bdf.words];
+    clear bdf filename;
+end
 
 %%% Passive
-figure;
 bump_word_base = hex2dec('50');
 all_bumps = words(words(:,2) >= (bump_word_base) & words(:,2) <= (bump_word_base+5), 1)';
 all_bump_codes = words(words(:,2) >= (bump_word_base) & words(:,2) <= (bump_word_base+5), 2)';
@@ -44,8 +43,9 @@ for i = 2:length(all_bumps);
     end
 end
 
-% generate plot
-pasive_tuning = zeros(1,3);
+% generate plots
+figure;
+pasive_tuning = zeros(1,6);
 for dir = 0:5
     bump = center_hold_bumps(center_hold_bumps(:,2) == (bump_word_base+dir), 1);
     H = subplot(2,3,dir+1);
@@ -53,15 +53,62 @@ for dir = 0:5
     axis([-.5, 1.5, 0, 10]);
     pasive_tuning(dir+1) = sum(all > 0 & all < .1)/length(bump);
 end
+suptitle(sprintf('Passive %d-%d', chan, cell));
 
 %%% Active
-word_reward = hex2dec('');
+word_reward = hex2dec('20');
+reward_words = words(words(:,2) == word_reward, 1);
+
+% get unperturbed reach times
+reaches = [];
+for i = 2:length(reward_words)
+    reward = reward_words(i);
+    go = go_words(find(go_words < reward, 1, 'last'));
+    if sum(all_bumps > go & all_bumps < reward) == 0
+        reaches = [reaches; go];
+    end
+end
+
+% get reach directions
+word_ot_base = hex2dec('40');
+ot_word_times = words(find(words(:,2) >= word_ot_base & words(:,2) <= word_ot_base+5), 1);
+ot_word_words = words(find(words(:,2) >= word_ot_base & words(:,2) <= word_ot_base+5), 2);
+reaches = [reaches zeros(length(reaches), 1)];
+for i = 2:length(reaches)
+    last_ot_idx = find(ot_word_times < reaches(i), 1, 'last');
+    reaches(i,2) = ot_word_words(last_ot_idx);
+end
+
+% generate plots
+figure;
+active_tuning = zeros(1,6);
+for dir = 0:5
+    go = reaches(reaches(:,2) == word_ot_base+dir, 1);
+    H = subplot(2,3,dir+1);
+    [table, all] = raster(spikes, go, -.5, 1.5, H);
+    axis([-.5, 1.5, 0, 10]);
+    active_tuning(dir+1) = sum(all > 0 & all < 1.25)/length(go);
+end
+suptitle(sprintf('Active %d-%d', chan, cell));
 
 %%% Polar plot
 pasive_tuning = [pasive_tuning pasive_tuning(1)] ./ .1;
+active_tuning = [active_tuning active_tuning(1)] ./ 1.25;
 theta = 0:pi/3:2*pi;
 
 figure;
-polar(theta, pasive_tuning);
+polar(theta, pasive_tuning, 'b-');
+title(sprintf('Passive %d-%d', chan, cell));
+
+figure;
+polar(theta, active_tuning, 'r-');
+title(sprintf('Active %d-%d', chan, cell));
+
+% Final analysis
+
+x = active_tuning(1:6).*cos(th);
+y = active_tuning(1:6).*sin(th);
+
+pd = atan2(sum(x), sum(y))
 
 
