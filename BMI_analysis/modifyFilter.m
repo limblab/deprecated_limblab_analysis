efb118638676fcd875257baff2df3ecbf2f21138
Christian Ethier <christian.ethier@gmail.com>
2009-04-27 17:17:49 +0000

    added STA.m to create stimulus triggered averages from BDF structures and convertBDF2binned_IF.m to bin spikes using instantaneous firing rate.
    git-svn-id: http://citadel.physiology.northwestern.edu/svn/s1_analysis@84 984b1e3c-1c57-0410-ae62-c18f90d3b645

--- BMI_analysis/modifyFilter.m
+++ BMI_analysis/modifyFilter.m
@@ -4,10 +4,10 @@ P = filter.P;
 neuronIDs = filter.neuronIDs;
 
 %original filter columns:
-% 1- FDSu 2- 2-FDSm 3-FDPu 4-FDPm 5-FCR1 6-FCU 7-2/3IO 8-FDI 9-ECR 10-EDC 11-1/2IO 12-ECU
+% 1- FDSu 2- 2-FDSm 3-FDPu 4-FDPm 5-FCR1 6-FCU 7-FPB 8-FDI 9-ECR 10-EDC 11-ECU 12-1/2IO
 
 Notes = ['modified filter, prediction columns:'...
-            '[1-FDSu 2-FDSm 3-FDPu 4-FDPu 5-FDPm 6-FDPm 7-FCR 8-1/2IO 9-2/3IO 10-ECR 11-ECR 12-ECR 13-EDC 14-FDI]'];
+            '[1-FDSu 2-FDSm 3-FDPu 4-FDPu 5-FDPm 6-FDPm 7-FCR 8-1/2IO 9-FPB 10-ECR 11-ECR 12-ECR 13-EDC 14-FDI]'];
 
 %modify filter columns        
 Hmod = H;
@@ -15,7 +15,7 @@ Hmod(:,4) = H(:,3);
 Hmod(:,5) = H(:,4);
 Hmod(:,6) = H(:,4);
 Hmod(:,7) = H(:,5);
-Hmod(:,8) = H(:,11);
+Hmod(:,8) = H(:,12);
 Hmod(:,9) = H(:,7);
 Hmod(:,10) = H(:,9);
 Hmod(:,11) = H(:,9);
@@ -31,7 +31,7 @@ Pmod(4,:) = P(3,:);
 Pmod(5,:) = P(4,:);
 Pmod(6,:) = P(4,:);
 Pmod(7,:) = P(5,:);
-Pmod(8,:) = P(11,:);
+Pmod(8,:) = P(12,:);
 Pmod(9,:) = P(7,:);
 Pmod(10,:) = P(9,:);
 Pmod(11,:) = P(9,:);
