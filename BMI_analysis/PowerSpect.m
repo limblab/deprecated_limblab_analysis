53590382979ad218f58a9af0253b71babe116499
Christian Ethier <christian.ethier@gmail.com>
2010-03-24 22:03:26 +0000

    changed type of power analysis window
    git-svn-id: http://citadel.physiology.northwestern.edu/svn/s1_analysis@188 984b1e3c-1c57-0410-ae62-c18f90d3b645

--- BMI_analysis/PowerSpect.m
+++ BMI_analysis/PowerSpect.m
@@ -1,4 +1,5 @@
 function [Xpsd]=PowerSpect(X,Fs)
+%h=spectrum.welch('Hann',window,100*noverlap/window)
 h=spectrum.welch
 hopts = psdopts(h)
 
@@ -20,6 +21,8 @@ hopts = psdopts(h)
 
 numSigs = size(X,2);
 Data_temp=zeros(h.SegmentLength*2+1,numSigs);
+% Data_temp=zeros(129,numSigs);
+% Data_temp=zeros(ceil(h.SegmentLength*100/h.OverlapPercent),numSigs);
 for i=1:numSigs
     hpsd = psd(h,X(:,i),'Fs',Fs);
     Data_temp(:,i)= hpsd.Data;
