7edbce3930c0fc9d4654a87bc86ddd4752c1f671
Christian Ethier <christian.ethier@gmail.com>
2010-04-05 19:11:16 +0000

    updated comments
    git-svn-id: http://citadel.physiology.northwestern.edu/svn/s1_analysis@196 984b1e3c-1c57-0410-ae62-c18f90d3b645

--- proc/christian/Get_Words_ts_pairs.m
+++ proc/christian/Get_Words_ts_pairs.m
@@ -1,5 +1,12 @@
 function ts_pairs = Get_Words_ts_pairs(startWord, Word1, Word2, wordsVect)
 
+% This function returns ts_pairs, a 2 column matrix containing on each rows
+% the time stamps of 2 words within the same trial. Word1 and Word2 should
+% be sequential within the trial.
+% startWord: the start word for the task
+% Word1, Word2: the two words from which you want to extract the ts
+% WordsVect: a 2 column matrix of words as found in the bdf struct
+
 %get the words ts
 Start_ts = wordsVect(wordsVect(:,2)==startWord,1);
 Word1_ts = wordsVect(wordsVect(:,2)==Word1 & wordsVect(:,1)>Start_ts(1),1);
