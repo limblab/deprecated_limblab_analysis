028c788a0a5487a6f4e7d35cc52efcab0270975b
David Bontrager <davbontrager@gmail.com>
2010-04-07 18:02:57 +0000

    -mostly just added comments (David's edit)
    git-svn-id: http://citadel.physiology.northwestern.edu/svn/s1_analysis@199 984b1e3c-1c57-0410-ae62-c18f90d3b645

--- bdf/thor_map.m
+++ bdf/thor_map.m
@@ -1,30 +1,31 @@
+function output = thor_map(spike_list)
 
 
-function output=thor_map(spike_list)
-
-%mapping from electrode number to electrode position
-array_map=[93 92 94 95 75 96 85 97 86 98;...
-           87 88 77 99 66 89 76 90 67 79;...
-           58 80 78 100 68 60 69 50 59 40;...
-           49 30 83 84 73 74 63 64 53 54;...
-           43 55 44 45 33 46 34 65 24 56;...
-           35 47 25 57 26 36 27 37 28 38;...
-           29 48 19 39 81 82 71 72 61 62;...
-           51 52 41 42 31 32 21 22 11 12;...
-           1  23 3  13 4  14 15 5  16 6;...
-           17 91 8  18 20 10 0  0  0  0 ];
+%Putting electrode position into channel number matrix (that is, array_map(1,1) is
+%channel/pin #1 and maps to electrode position #93, array_map(1,2) is pin #2 and electrode position #92, etc.)
+array_map = [93 92 94 95 75 96 85 97 86 98;... %pins 1-10
+             87 88 77 99 66 89 76 90 67 79;... %pins 11-20
+             58 80 78 100 68 60 69 50 59 40;...%pins 21-30
+             49 30 83 84 73 74 63 64 53 54;... %pins 31-40
+             43 55 44 45 33 46 34 65 24 56;... %pins 41-50
+             35 47 25 57 26 36 27 37 28 38;... %pins 51-60
+             29 48 19 39 81 82 71 72 61 62;... %pins 61-70
+             51 52 41 42 31 32 21 22 11 12;... %pins 71-80
+             1  23 3  13 4  14 15 5  16 6 ;... %pins 81-90
+             17 91 8  18 20 10 0  0  0  0 ];   %pins 91-96, plus the four ground/reference electrodes (electrodes 2, 7, 9, 70)
  
-%because indexing increases as you go down a column instead of across a row...       
-array_map_T=transpose(array_map);       
+%because single-value indexing increases as you go down a column instead of across a row...       
+array_map = array_map';       
        
-num_units =size(spike_list,1); %number of units
+num_units = size(  spike_list, 1 ); %number of units
+subs      = zeros( num_units,  2 );
 
 %for each unit
 for i=1:num_units
     pin       = spike_list(i);
-    index     = array_map_T(pin);
-    cell      =ind2sub([10 10],index);
-    subs(i,:) =cell;
+    index     = array_map(pin);
+    cell      = ind2sub( [10 10], index );
+    subs(i)   = cell;
 end
 
-output=subs;
+output = subs;
