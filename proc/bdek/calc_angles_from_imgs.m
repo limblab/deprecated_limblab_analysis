ca14f904b568face8a3a45dd6e544afbd35ba440
Brian Dekleva <brian.dekleva@gmail.com>
2010-08-18 18:58:56 +0000

    Updated calc_angles_from_imgs.m to include return of handle position coordinates.
    git-svn-id: http://citadel.physiology.northwestern.edu/svn/s1_analysis@258 984b1e3c-1c57-0410-ae62-c18f90d3b645

--- proc/bdek/calc_angles_from_imgs.m
+++ proc/bdek/calc_angles_from_imgs.m
@@ -1,11 +1,16 @@
-function angles_list = calc_angles_from_imgs(path,beg_num,end_num,filetype)
+function [angles_list,handle_pos] = calc_angles_from_imgs(path,beg_num,end_num,filetype)
 
-% calc_angles_from_imgs(path,beg_num,end_num,filetype) takes as input the
+% calc_angles_from_imgs.m takes as input the
 % path of the image sequence, the image file numbers of the first and last
-% image, and the filetype ('jpg','png',etc). The path should be given as a
-% string, including the entire file path up until the image sequence
-% number. 
-
+% image, and the filetype ('jpg','png',etc). 
+% The path should be given as a string, including the entire file path up 
+% until the image sequence number. 
+%
+% The function returns an array of angles corresponding to the wrist
+% flexion angle, as well as a matrix containing handle coordinates.  The
+% handle coordinates are in image format (origin is upper left) and are
+% included for possible synchronization with recorded data. 
+%
 % To edit the image processing performance, see wrist_angle.m
 
 table = zeros(end_num-beg_num+1,6);
@@ -31,3 +36,5 @@ end
         
 angles_list = angles(pos_table);
         %runs angles.m to determine angles of flexion. 
+handle_pos = pos_table(:,1:2);
+        %handle position coordinates.
