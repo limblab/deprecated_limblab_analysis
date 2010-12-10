81826eaca21e1d2e2ffbc15588fd59908081b4a5
Ricardo Torres <ricardort@gmail.com>
2010-12-10 18:28:36 +0000

    git-svn-id: http://citadel.physiology.northwestern.edu/svn/s1_analysis@322 984b1e3c-1c57-0410-ae62-c18f90d3b645

---
+++ proc/ricardo/BC_detectionB_experiment_list.m
@@ -0,0 +1,91 @@
+function [datapath filelist] = BC_detectionB_experiment_list
+
+datapath = 'D:\Data\Pedro\';
+    
+filelist(1).name = 'Pedro_BC_detectionB_001';  
+filelist(1).pd = [3.142 3.142 3.142 3.142 0];
+filelist(1).codes = [0 0 0 0 1];
+filelist(1).electrodes = [89 91 93 95 0];
+filelist(1).pulsewidth = [200 200 200 200 0];
+filelist(1).current = [30 30 30 30 0];
+filelist(1).period = [3.3 3.3 3.3 3.3 0];
+filelist(1).pulses = [30 30 30 30 0];
+filelist(1).delay = [1 1.5 2 2.5 0];
+filelist(1).date = '2010-11-17';
+
+filelist(2).name = 'Pedro_BC_detectionB_002';  
+filelist(2).pd = [3.142 3.142 3.142 3.142 0];
+filelist(2).codes = [0 0 0 0 1];
+filelist(2).electrodes = [89 91 93 95 0];
+filelist(2).pulsewidth = [200 200 200 200 0];
+filelist(2).current = [30 30 30 30 0];
+filelist(2).period = [3.3 3.3 3.3 3.3 0];
+filelist(2).pulses = [30 30 30 30 0];
+filelist(2).delay = [1 1.5 2 2.5 0];
+filelist(2).date = '2010-11-22';
+
+filelist(3).name = 'Pedro_BC_detectionB_003_sub';  
+filelist(3).pd = [3.142 3.142 3.142 3.142 0];
+filelist(3).codes = [0 0 0 0 1];
+filelist(3).electrodes = [89 91 93 95 0];
+filelist(3).pulsewidth = [200 200 200 200 0];
+filelist(3).current = [30 30 30 30 0];
+filelist(3).period = [3.3 3.3 3.3 3.3 0];
+filelist(3).pulses = [30 30 30 30 0];
+filelist(3).delay = [1 1.5 2 2.5 0];
+filelist(3).date = '2010-11-29';
+
+filelist(4).name = 'Pedro_BC_detectionB_004';  
+filelist(4).pd = [3.142 3.142 3.142 3.142 0];
+filelist(4).codes = [0 0 0 0 1];
+filelist(4).electrodes = [89 91 93 95 0];
+filelist(4).pulsewidth = [200 200 200 200 0];
+filelist(4).current = [30 30 30 30 0];
+filelist(4).period = [3.3 3.3 3.3 3.3 0];
+filelist(4).pulses = [30 30 30 30 0];
+filelist(4).delay = [1 1.5 2 2.5 0];
+filelist(4).date = '2010-11-30';
+
+filelist(5).name = 'Pedro_BC_detectionB_005';  
+filelist(5).pd = [3.142 3.142 3.142 3.142 0];
+filelist(5).codes = [0 0 0 0 1];
+filelist(5).electrodes = [89 91 93 95 0];
+filelist(5).pulsewidth = [200 200 200 200 0];
+filelist(5).current = [30 30 30 30 0];
+filelist(5).period = [3.3 3.3 3.3 3.3 0];
+filelist(5).pulses = [30 30 30 30 0];
+filelist(5).delay = [1 1.5 2 2.5 0];
+filelist(5).date = '2010-12-01';
+
+filelist(6).name = 'Pedro_BC_detectionB_006';  
+filelist(6).pd = [3.142 3.142 3.142 3.142 0];
+filelist(6).codes = [0 0 0 0 1];
+filelist(6).electrodes = [89 91 93 95 0];
+filelist(6).pulsewidth = [200 200 200 200 0];
+filelist(6).current = [30 30 30 30 0];
+filelist(6).period = [3.3 3.3 3.3 3.3 0];
+filelist(6).pulses = [30 30 30 30 0];
+filelist(6).delay = [1 1.5 2 2.5 0];
+filelist(6).date = '2010-12-02';
+
+filelist(7).name = 'Pedro_BC_detectionB_007';  
+filelist(7).pd = [3.142 3.142 3.142 3.142 0];
+filelist(7).codes = [0 0 0 0 1];
+filelist(7).electrodes = [89 91 93 95 0];
+filelist(7).pulsewidth = [200 200 200 200 0];
+filelist(7).current = [30 30 30 30 0];
+filelist(7).period = [3.3 3.3 3.3 3.3 0];
+filelist(7).pulses = [30 30 30 30 0];
+filelist(7).delay = [1 1.5 2 2.5 0];
+filelist(7).date = '2010-12-03';
+
+filelist(8).name = 'Pedro_BC_detectionB_008';  
+filelist(8).pd = [3.142 3.142 3.142 3.142 0];
+filelist(8).codes = [0 0 0 0 1];
+filelist(8).electrodes = [89 91 93 95 0];
+filelist(8).pulsewidth = [200 200 200 200 0];
+filelist(8).current = [30 30 30 30 0];
+filelist(8).period = [3.3 3.3 3.3 3.3 0];
+filelist(8).pulses = [30 30 30 30 0];
+filelist(8).delay = [1 1.5 2 2.5 0];
+filelist(8).date = '2010-12-06';
---
+++ proc/ricardo/BC_detectionC_experiment_list.m
@@ -0,0 +1,39 @@
+function [datapath filelist] = BC_detectionC_experiment_list
+
+datapath = 'D:\Data\Pedro\';
+
+filelist(1).name = 'Pedro_BC_detectionC_001';  
+filelist(1).pd = [3.142 3.142 3.142 3.142];
+filelist(1).codes = [0 0 0 0];
+filelist(1).electrodes = [89 91 93 95];
+filelist(1).pulsewidth = [200 200 200 200];
+filelist(1).current = [30 30 30 30];
+filelist(1).period = [3.3 3.3 3.3 3.3];
+filelist(1).pulses = [30 30 30 30];
+filelist(1).delay = [31 31.5 32 32.5];
+filelist(1).date = '2010-12-07';
+filelist(1).serverdatapath = 'Z:\Miller\Pedro_4C2\S1 Array\Raw';
+
+filelist(2).name = 'Pedro_BC_detectionC_002';  
+filelist(2).pd = [3.142 3.142 3.142 3.142];
+filelist(2).codes = [0 0 0 0];
+filelist(2).electrodes = [89 91 93 95];
+filelist(2).pulsewidth = [200 200 200 200];
+filelist(2).current = [30 30 30 30];
+filelist(2).period = [3.3 3.3 3.3 3.3];
+filelist(2).pulses = [30 30 30 30];
+filelist(2).delay = [31 31.5 32 32.5];
+filelist(2).date = '2010-12-08';
+filelist(2).serverdatapath = 'Z:\Miller\Pedro_4C2\S1 Array\Raw';
+
+filelist(3).name = 'Pedro_BC_detectionC_003';  
+filelist(3).pd = [3.142 3.142 3.142 3.142];
+filelist(3).codes = [0 0 0 0];
+filelist(3).electrodes = [89 91 93 95];
+filelist(3).pulsewidth = [200 200 200 200];
+filelist(3).current = [40 40 40 40];
+filelist(3).period = [3.3 3.3 3.3 3.3];
+filelist(3).pulses = [30 30 30 30];
+filelist(3).delay = [31 31.5 32 32.5];
+filelist(3).date = '2010-12-08';
+filelist(3).serverdatapath = 'Z:\Miller\Pedro_4C2\S1 Array\Raw';
