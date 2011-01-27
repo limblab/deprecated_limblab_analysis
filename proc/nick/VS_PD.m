e9a68677429ad436bca547eddd52104e8e553d3d
Ricardo Torres <ricardort@gmail.com>
2011-01-27 23:02:04 +0000

    updated PD code
    git-svn-id: http://citadel.physiology.northwestern.edu/svn/s1_analysis@357 984b1e3c-1c57-0410-ae62-c18f90d3b645

--- proc/nick/VS_PD.m
+++ proc/nick/VS_PD.m
@@ -8,18 +8,19 @@ function PDMatrix = VS_PD(datafile,delay,window_length)
 %
 % Default values for window parameters are:
 %
-%   DELAY = 100ms;
-%   WINDOW_LENGTH = 200ms;
+%   DELAY = 100ms
+%   WINDOW_LENGTH = 200ms
 %
 % PDMatrix is in the form of a matrix [a, b] where each represents the
 % following:
 % 
 %   a - unit number
 %   b - data
-%       1 - preferred direction (angle in radians)
-%       2 - 95% confidence interval (in radians)
+%       1 - 5th percentile of preferred direction (in radians)
+%       2 - mean preferred direction (in radians)
+%       3 - 95th percentile of preferred direction (in radians)
 %
-% 'datafile' should be the BDF file of interest.  This version uses the
+% DATAFILE should be the BDF file of interest.  This version uses the
 % following words to determine timing of events and target positions:
 %
 %    32 = reward
@@ -34,67 +35,72 @@ function PDMatrix = VS_PD(datafile,delay,window_length)
 %            and make the code more flexible).
 %   128 = movement onset
 %
-% NOTE: target_number will begin with 0 in the 12 o'clock position, while
-% target_position will begin with 1 in the 12 o'clock position.  This is to
-% fit with the dual convention used in the Visual Search behavior code as
-% well as to simplify calculations.
+% NOTE: TARGET_POSITIONS begins with 1 in the 12 o'clock position and is
+% incremented clockwise.  This is to fit with the Visual Search behavior
+% code.  DIRECTIONS begins with 12 o'clock and is incremented counter-
+% clockwise.  Preferred directions are calcuated using DIRECTIONS and then
+% rotated by 90 degrees to make zero coincident with the x axis.  Hence PDs
+% are referenced to the x axis and increase in a counter-clockwise fashion.
 
 VS_BDF = load(datafile); % load BDF struct
 
+cd('../..'); % change directory to s1_analysis
+load_paths % load paths to include subdirectories with BOOTSTRAP and CPRCTILE functions
+
 if (nargin == 1)
-    delay = 0.1;
-    window_length = 0.2;
+    delay = 0.1; % set default delay
+    window_length = 0.2; % set default window length
 end
 
 total_units = 0;
 for unit_check = 1:length(VS_BDF.bdf.units); % calculate number of units
     if size(VS_BDF.bdf.units(1,unit_check).id) ~= 0 % check for empty struct value
-        total_units = total_units + 1;
+        total_units = total_units + 1; % increment number of units if struct is not empty
     end
 end
 
-% calculate number of targets
-target_words = VS_BDF.bdf.words(:,2) >= 64 & VS_BDF.bdf.words(:,2) <= 79;
+target_words = VS_BDF.bdf.words(:,2) >= 64 & VS_BDF.bdf.words(:,2) <= 79; % create logical array with indices of all words that represent outer target appearance
 
-total_targets = max(VS_BDF.bdf.words(target_words,2)) - 63;
+total_targets = max(VS_BDF.bdf.words(target_words,2)) - 63; % find maximum value for target words and use this to define total number of targets used
 
 spike_counts = cell(total_units, total_targets); % create a cell array to track the number of spikes for each reach in each direction
 
 trial_count = zeros(1,total_targets); % create a matrix to track the number of trials that are in the direction of each target
 
-% create logical arrays for words
-reward_words = VS_BDF.bdf.words(:,2) == 32;
+reward_words = VS_BDF.bdf.words(:,2) == 32; % create a logical array with indices of all words that represent rewards (successful trials)
 
-successful_move_words = VS_BDF.bdf.words(:,2) == 128 & [0; target_words(1:end-1)] & [reward_words(3:end); 0; 0];
+successful_move_words = VS_BDF.bdf.words(:,2) == 128 & [0; target_words(1:end-1)] & [reward_words(3:end); 0; 0]; % compare logical arrays for targets and rewards to create logical array with indices of successful movement onsets
 
-successful_target_words = logical([successful_move_words(2:end); 0]);
+successful_target_words = logical([successful_move_words(2:end); 0]); % create logical array with indices of targets that were reached successfully
 
-targets = VS_BDF.bdf.words(successful_target_words,2);
+targets = VS_BDF.bdf.words(successful_target_words,2); % create array of all targets that were successfully reached
 
-target_positions = targets - 63;
+target_positions = targets - 63; % convert successful target words to positions (1 = 12 o'clock, increasing clockwise)
 
-move_times = VS_BDF.bdf.words(successful_move_words,1);
+move_times = VS_BDF.bdf.words(successful_move_words,1); % create array off all successful movement onset times (this couples with previous array for successful target positions)
 
 for direction = 1:total_targets
-    trial_count(direction) = sum(target_positions == direction);
-    direction_times = move_times(target_positions == direction);
+    if direction == 1
+        position = direction;
+    else
+        position = total_targets + 2 - direction; % find directions from positions (1 = 12 o'clock, increasing counter-clockwise)
+    end
+    trial_count(direction) = sum(target_positions == position); % count number of successful trials for direction of interest
+    direction_times = move_times(target_positions == position); % find movement onset times for direction of interest
     for unit = 1:total_units
         for trial = 1:trial_count(direction)
-            spike_counts{unit}{direction}(trial) = sum((VS_BDF.bdf.units(1,unit).ts >= direction_times(trial) - delay) & (VS_BDF.bdf.units(1,unit).ts <= direction_times(trial) - delay + window_length));
+            spike_counts{unit}{direction}(trial) = sum((VS_BDF.bdf.units(1,unit).ts >= direction_times(trial) - delay) & (VS_BDF.bdf.units(1,unit).ts <= direction_times(trial) - delay + window_length)); % sum spikes for each unit in window of interest for movement trials in direction of interest
         end
     end
 end
 
-% Temporarily, PDMatrix will be output in the form of a cell array, where
-% each cell represents a neural unit and the matrix within each cell is Nx2
-% where N is the number or function calls in bootstrapping and the columns
-% represent preferred direction and vector magnitude calculations.
-
-addpath('../../lib/bootstrapping');
-
-PDMatrix = cell(1, total_units);
+bootstrapPDs = cell(1, total_units); % create a cell array to store the bootstrapped PDs for each unit 
+PDMatrix = zeros(total_units,3); % create a matrix to save the mean PD and error bounds for each unit
 
 for x = 1:total_units
-    PDMatrix{x} = bootstrap(@vector_sum_pd, spike_counts{x}, 'all', 1000);
-    % PDMatrix = bootstrap(@PD_angle_calc, spike_rates, 'all', 1000);
+    bootstrapPDs{x} = bootstrap(@vector_sum_pd, spike_counts{x}, 'all', 1000); % perform 1000 bootstrapped PD calculations for each unit (returns [PD magnitude] for each sample set)
+    bootstrapPDs{x}(:,1) = bootstrapPDs{x}(:,1) + pi/2; % rotate all bootstrapped PD calculations by 90 degrees (VECTOR_SUM_PD assumes 0 degrees is in direction of first target, we need to realign with x axis)
+    PDMatrix(x,:) = cprctile(bootstrapPDs{x}(:,1),[5 50 95]); % calculate 5th percentile, mean, and 95th percetile of PD for each unit based on bootstrapping results
 end
+
+% PDMatrix = PDMatrix * 180 / pi; % convert PDs to degrees
--- proc/nick/plot_VS_PDs.m
+++ proc/nick/plot_VS_PDs.m
@@ -1,6 +1,7 @@
 figure
 clear PD_means
 for x = 1:length(PDs)
-    PD_means(x) = mean(PDs{x}(:,1));
+    PD_means(x) = circ_mean(PDs{x}(:,1));
 end
 plot(0,PD_means,'x')
+axis([-1 1 -3.15 3.15])
