56f7668ea7b70ff2ffffc3c0d99032a2c27548ac
Mike Scheid <michaelscheid2015@northwestern.edu>
2011-05-04 19:10:15 +0000

    Created a function to determine PDs in muscle space, emgpds.m
    git-svn-id: http://citadel.physiology.northwestern.edu/svn/s1_analysis@471 984b1e3c-1c57-0410-ae62-c18f90d3b645

--- proc/michael/emgpds.m
+++ proc/michael/emgpds.m
@@ -1,30 +1,45 @@
+function [PDm,  = emgpds(bdf)
+
 % emgpds.m
 % Calculates STA based PDS in emgspace from bdf
 
 % $Id: $
 
-unit_id = 3;
+List=unit_list(bdf);
+List = List(List(:,2)~=0 & List(:,2) ~=255,:);
 
 nEmgs = size(bdf.emg.data,2) - 1;
 
-stas = zeros(nEmgs, 10001);
+timebefore= .25;
+timeafter=.25;
+stas = zeros(nEmgs, bdf.emg.emgfreq*timebefore+timeafter);
 t = bdf.emg.data(:,1);
 
-for emg_id = 1:nEmgs
-    disp(emg_id);
-    tmp_emg = bdf.emg.data(:,emg_id+1);
-    tmp_emg = tmp_emg ./ var(tmp_emg);
-    tmp_emg = smooth(abs(tmp_emg), 201);
+for i=1:length(List)
     
-    tmp_sta = STA(bdf.units(unit_id).ts, [t tmp_emg], 2.5, 2.5);
-    stas(emg_id,:) = tmp_sta(:,2) - repmat(mean(tmp_sta(:,2)), length(tmp_sta(:,2)), 1);
-    tsta = tmp_sta(:,1);
+    sprintf('Now running unit %g of %g', i,length(List));
+    
+    u= get_unit(bdf, List(i,1), List(i,2));
+    
+    for emg_id = 1:nEmgs
+        disp(emg_id);
+        tmp_emg = bdf.emg.data(:,emg_id+1);
+        tmp_emg = tmp_emg ./ var(tmp_emg);
+        tmp_emg = smooth(abs(tmp_emg), 201);
+
+        tmp_sta = STAsl(u, [t tmp_emg], timebefore, timeafter);
+        stas(emg_id,:) = tmp_sta(:,2) - repmat(mean(tmp_sta(:,2)), length(tmp_sta(:,2)), 1);
+        tsta = tmp_sta(:,1);
+    end
+
+    n = sqrt(sum(stas.^2));
+    opt_delay = find(n==max(n), 1, 'first');
+    opt_delay_t = tsta(opt_delay);
+    PDm= zeros(nEmgs+2,length(List));
+    PDm(1,i)= List(i,1);
+    PDm(2,i)= List(i,2);
+    PDm(3:end,i) = stas(:, opt_delay);
+    PDm(3:end,i) = PDm(3:end,i)./ sqrt(sum(PDm(3:end,i).^2));
 end
 
-n = sqrt(sum(stas.^2));
-opt_delay = find(n==max(n), 1, 'first');
-opt_delay_t = tsta(opt_delay);
-
-PDm = stas(:, opt_delay);
-PDm = PDm ./ sqrt(sum(PDm.^2));
 
