240cc2ccb9ca33c41e6c0bce5aa7ddfb6f1eadb0
Mike Scheid <michaelscheid2015@northwestern.edu>
2011-05-04 22:05:51 +0000

    Commented and runs
    git-svn-id: http://citadel.physiology.northwestern.edu/svn/s1_analysis@473 984b1e3c-1c57-0410-ae62-c18f90d3b645

--- proc/michael/emgpds.m
+++ proc/michael/emgpds.m
@@ -1,4 +1,4 @@
-function [PDm,  = emgpds(bdf)
+function [PDm,Allstas]  = emgpds(bdf)
 
 % emgpds.m
 % Calculates STA based PDS in emgspace from bdf
@@ -8,16 +8,25 @@ function [PDm,  = emgpds(bdf)
 List=unit_list(bdf);
 List = List(List(:,2)~=0 & List(:,2) ~=255,:);
 
-nEmgs = size(bdf.emg.data,2) - 1;
+exclEMG=1;
+%Last muscle EMG no good
 
-timebefore= .25;
-timeafter=.25;
-stas = zeros(nEmgs, bdf.emg.emgfreq*timebefore+timeafter);
+nEmgs = size(bdf.emg.data,2) - 1- exclEMG;
+%-1 for time column
+
+
+timebefore= 0.25;
+timeafter=0.25;
+stas = zeros(nEmgs, (bdf.emg.emgfreq*(timebefore+timeafter))+1);
 t = bdf.emg.data(:,1);
+
+Allstas = zeros(nEmgs,(bdf.emg.emgfreq*(timebefore+timeafter))+1,length(List));
+%Save All calculated STAs for each unit
 
 for i=1:length(List)
     
-    sprintf('Now running unit %g of %g', i,length(List));
+    update=sprintf('%s %g %s %g', 'Now running',i, 'of', length(List));
+    disp(update);
     
     u= get_unit(bdf, List(i,1), List(i,2));
     
@@ -26,20 +35,34 @@ for i=1:length(List)
         tmp_emg = bdf.emg.data(:,emg_id+1);
         tmp_emg = tmp_emg ./ var(tmp_emg);
         tmp_emg = smooth(abs(tmp_emg), 201);
+        %Rectify and smooth EMG signals
 
         tmp_sta = STAsl(u, [t tmp_emg], timebefore, timeafter);
+        
         stas(emg_id,:) = tmp_sta(:,2) - repmat(mean(tmp_sta(:,2)), length(tmp_sta(:,2)), 1);
+        %STAs matrix now row=Each Muscle, col=Time
+        
         tsta = tmp_sta(:,1);
     end
 
+    Allstas(:,:,i)=stas(:,:);
     n = sqrt(sum(stas.^2));
+    %Calculate magnitude of STAs across time
+    
     opt_delay = find(n==max(n), 1, 'first');
+    %Find index of maximum magnitude STA
+    
     opt_delay_t = tsta(opt_delay);
+    %Index time array tsta to find the optimum delay=opt_delay
+    
     PDm= zeros(nEmgs+2,length(List));
     PDm(1,i)= List(i,1);
     PDm(2,i)= List(i,2);
+    
     PDm(3:end,i) = stas(:, opt_delay);
     PDm(3:end,i) = PDm(3:end,i)./ sqrt(sum(PDm(3:end,i).^2));
+    %Assign STAs at opt_delay to PDM and convert STA vector to unit vector by dividing
+    %by vector length
 end
 
 
