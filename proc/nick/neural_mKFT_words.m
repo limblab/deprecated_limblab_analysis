f7f14c6536307ae382fd4b843097db85c586fe20
Ricardo Torres <ricardort@gmail.com>
2011-05-12 15:44:22 +0000

    changed window to 100-200ms prior
    git-svn-id: http://citadel.physiology.northwestern.edu/svn/s1_analysis@491 984b1e3c-1c57-0410-ae62-c18f90d3b645

--- proc/nick/neural_mKFT_words.m
+++ proc/nick/neural_mKFT_words.m
@@ -14,10 +14,13 @@ addpath('KPMtools');
 
 %%load 2 binned data files... call trainData and testData
 
-window = 0.500; % in seconds (for spike averaging) should match training
+% window = 0.500; % in seconds (for spike averaging) should match training
+window = 0.100; % in seconds (for spike averaging) should match training
+delay = 0.100;
 
 bin = double(trainData.timeframe(2) - trainData.timeframe(1));
 window_bins = floor(window/bin);
+delay_bins = floor(delay/bin);
 
 %%adjust timing of words and target info to coincide with bins
 trainData.words(:,1) = ceil(trainData.words(:,1)./bin).*bin;
@@ -44,19 +47,30 @@ end
 trainData.goals = [trainData.centergoals; trainData.outergoals];
 
 %%build training set
-training_set = zeros(length(trainData.timeframe),(length(trainData.spikeguide)-2)*window_bins); % removed channel 70 for Chewie data
+% training_set = zeros(length(trainData.timeframe),(length(trainData.spikeguide)-2)*window_bins); % removed channel 70 for Chewie data
+training_set = zeros(length(trainData.timeframe),(length(trainData.spikeguide)-2)); % removed channel 70 for Chewie data
 % training_set = zeros(length(trainData.timeframe),length(trainData.spikeguide)*window_bins);
 
 transitions = zeros(length(trainData.timeframe),1);
 targets = zeros(length(trainData.timeframe),2);
 
-for x = window_bins:length(trainData.timeframe)
-    observation = [];
-    for y = 1:window_bins
-        observation = [observation trainData.spikeratedata(x-(y-1),1:89) trainData.spikeratedata(x-(y-1),92:97)]; % removed channel 70 for Chewie data
-%         observation = [observation trainData.spikeratedata(x-(y-1),:)];
-    end
-    training_set(x,:) = observation;
+% for x = window_bins:length(trainData.timeframe)
+%     observation = [];
+%     for y = 1:window_bins
+%         observation = [observation trainData.spikeratedata(x-(y-1),1:89) trainData.spikeratedata(x-(y-1),92:97)]; % removed channel 70 for Chewie data
+% %         observation = [observation trainData.spikeratedata(x-(y-1),:)];
+%     end
+%     training_set(x,:) = observation;
+% 
+%     if sum(trainData.goals(:,1) == trainData.timeframe(x))
+%         transitions(x) = 1;
+%         targets(x,:) = trainData.goals((trainData.goals(:,1) == trainData.timeframe(x)),2:3);
+%     end
+% end
+
+for x = (window_bins + delay_bins):length(trainData.timeframe)
+    training_set(x,:) = [mean(trainData.spikeratedata(x-(window_bins + delay_bins-1):x-(delay_bins-1),1:89)) mean(trainData.spikeratedata(x-(window_bins + delay_bins-1):x-(delay_bins-1),92:97))]; % removed channel 70 for Chewie data
+%     training_set(x,:) = mean(trainData.spikeratedata(x-(window_bins + delay_bins-1):x-(delay_bins-1),:));
 
     if sum(trainData.goals(:,1) == trainData.timeframe(x))
         transitions(x) = 1;
@@ -77,8 +91,10 @@ for i = 1:length(startindex)
 %     v = [zeros(1,2); diff(p)];
 %     a = [zeros(2,2); diff(diff(p))];
     targ = repmat(targets(startindex(i),:),length(p),1); % make target new target position
-    X{n} = [p v a ones(length(p),1) targ];
-    X0{n} = [p v a ones(length(p),1)];
+%     X{n} = [p v a ones(length(p),1) targ];
+%     X0{n} = [p v a ones(length(p),1)];
+    X{n} = [p v ones(length(p),1) targ]; % no accel
+    X0{n} = [p v ones(length(p),1)]; % no accel
     Z{n} = training_set(startindex(i):endindex(i),:);
 end
 
@@ -111,30 +127,41 @@ testData.centergoals(:,1) = testData.words(testData.words(:,2) == 48,1);
 testData.centergoals(:,3) = testData.centergoals(:,3) - 33.5;
 % testData.outergoals = zeros(size(testData.targets.corners,1),3);
 testData.outergoals = repmat(testData.words([false; false; (testData.words(3:end-3,2) >= 64 & testData.words(3:end-3,2) < 80)],1),1,3);
-for x = 1:length(testData.outergoals)-1
+for x = 1:length(testData.outergoals)
     testData.outergoals(x,2) = mean(testData.targets.corners(find(testData.targets.corners(:,1) > testData.outergoals(x,1),1,'first'),[2 4]),2);
     testData.outergoals(x,3) = mean(testData.targets.corners(find(testData.targets.corners(:,1) > testData.outergoals(x,1),1,'first'),[3 5]),2) - 33.5;
 end
 testData.goals = [testData.centergoals; testData.outergoals];
 
 %%build test set
-test_set = zeros(length(testData.timeframe),length(testData.spikeguide)*window_bins);
+% test_set = zeros(length(testData.timeframe),length(testData.spikeguide)*window_bins);
+test_set = zeros(length(testData.timeframe),length(testData.spikeguide));
 
 transitions = zeros(length(testData.timeframe),1);
 targets = zeros(length(testData.timeframe),2);
 
-for x = window_bins:length(testData.timeframe)
-    observation = [];
-    for y = 1:window_bins
-        observation = [observation testData.spikeratedata(x-(y-1),:)];
-    end
-    test_set(x,:) = observation;
+% for x = window_bins:length(testData.timeframe)
+%     observation = [];
+%     for y = 1:window_bins
+%         observation = [observation testData.spikeratedata(x-(y-1),:)];
+%     end
+%     test_set(x,:) = observation;
+% 
+%     if sum(testData.goals(:,1) == testData.timeframe(x))
+%         transitions(x) = 1;
+%         targets(x,:) = testData.goals((testData.goals(:,1) == testData.timeframe(x)),2:3);
+%     end
+% end
+
+for x = (window_bins + delay_bins):length(testData.timeframe)
+    test_set(x,:) = mean(testData.spikeratedata(x-(window_bins + delay_bins-1):x-(delay_bins-1),:));
 
     if sum(testData.goals(:,1) == testData.timeframe(x))
         transitions(x) = 1;
         targets(x,:) = testData.goals((testData.goals(:,1) == testData.timeframe(x)),2:3);
     end
 end
+
 
 %%separate test data into reaches
 startindex = find(transitions);
@@ -150,8 +177,10 @@ for i = 1:length(startindex)
 %     a = [zeros(2,2); diff(diff(p))];
     targ = repmat(targets(startindex(i),:),length(p),1); % make target new target position
 %     targ = repmat([0 0],length(p),1); % make target new target position
-    X{n} = [p v a ones(length(p),1) targ];
-    X0{n} = [p v a ones(length(p),1)];
+%     X{n} = [p v a ones(length(p),1) targ];
+%     X0{n} = [p v a ones(length(p),1)];
+    X{n} = [p v ones(length(p),1) targ]; % no accel
+    X0{n} = [p v ones(length(p),1)]; % no accel
     Z{n} = test_set(startindex(i):endindex(i),:);
 end
 
@@ -159,7 +188,8 @@ fprintf('Finished building test set\n')
 toc
 
 %%test KF
-xpredc=zeros(length(transitions),6);
+% xpredc=zeros(length(transitions),6);
+xpredc=zeros(length(transitions),4); % no accel
 xpred0c = xpredc;
 
 for i = 1:length(X)
@@ -179,8 +209,10 @@ for i = 1:length(X)
     [xpred{i}, Vpred{i}, VV{i}, loglik(i)] = kalman_filter(Z{i}', A, C, Q, R, initx, initV); %%with target
     [xpred0{i}, Vpred0{i}, VV0{i}, loglik0(i)] = kalman_filter(Z{i}', A0, C0, Q0, R0, initx0, initV0); %%without target
 
-    xpredc(startindex(i):endindex(i),:) = xpred{i}(1:6,:)';
-    xpred0c(startindex(i):endindex(i),:) = xpred0{i}(1:6,:)';
+%     xpredc(startindex(i):endindex(i),:) = xpred{i}(1:6,:)';
+%     xpred0c(startindex(i):endindex(i),:) = xpred0{i}(1:6,:)';
+    xpredc(startindex(i):endindex(i),:) = xpred{i}(1:4,:)'; % no accel
+    xpred0c(startindex(i):endindex(i),:) = xpred0{i}(1:4,:)'; % no accel
     
     toc
 end
