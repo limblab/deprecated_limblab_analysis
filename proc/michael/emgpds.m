40506dd46e123d854c6de2cb724bf96acf97f57a
Mike Scheid <michaelscheid2015@northwestern.edu>
2011-07-06 21:48:45 +0000

    Improved EMG processing, improved STA normalization, made function faster
    git-svn-id: http://citadel.physiology.northwestern.edu/svn/s1_analysis@544 984b1e3c-1c57-0410-ae62-c18f90d3b645

--- proc/michael/emgpds.m
+++ proc/michael/emgpds.m
@@ -1,18 +1,14 @@
-function [PDm,stas]  = emgpds(bdf)
 function [PDm stasnorm]  = emgpds(bdf)
+%% Variable Declaration and Memory Pre-allocation
 
-
-% emgpds.m
-% Calculates STA based PDS in emgspace from bdf
-
+%Calculates STA based PDS in emgspace from bdf
 % $Id: $
 
-
-
-
 List=unit_list(bdf);
-List = List(List(:,2)~=0 & List(:,2) ~=255,:);
+%Gets list of all units from bdf
 
+List = List(List(:,2)~=0 & List(:,2) ~=255,:);
+%Excludes all units which are not well-defined/sorted
 
 exclEMG=1;
 %Last muscle EMG no good
@@ -20,18 +16,18 @@ exclEMG=1;
 nEmgs = size(bdf.emg.data,2) - 1- exclEMG;
 %-1 for time column
 
-
 timebefore= 2;
 timeafter=2;
+%Define time window before and after spike for STA
+
 stas = zeros((bdf.emg.emgfreq*(timebefore+timeafter))+1,nEmgs,length(List));
-t = bdf.emg.data(:,1);
-tmp_emg = zeros(size(bdf.emg.data,1),size(bdf.emg.data,2) - exclEMG);
-
-
-
-
+stasnorm = zeros((bdf.emg.emgfreq*(timebefore+timeafter))+1,nEmgs,length(List));
 PDm= zeros(length(List), nEmgs+4);
+%Preallocate memory for speed
 
+%% EMG Pre-processing
+
+t = bdf.emg.data(:,1);
 tmp_emg = bdf.emg.data(:, 2 : size(bdf.emg.data,2) - exclEMG);
 
 %Find 1st 50th and 99th percentile of data in order to normalize properly
@@ -39,8 +35,6 @@ p=prctile(tmp_emg,[1,50,99]);
 
 %Subtract mean (50th %tile) and divide by difference of 99th and 1st %tile
 tmp_emg=(tmp_emg-repmat(p(2,:),length(tmp_emg),1))./(repmat(p(3,:),length(tmp_emg),1)-repmat(p(1,:),length(tmp_emg),1));
-
-
 
 for emg_id=1:nEmgs
     tmp_emg(:,emg_id) = smooth(abs(tmp_emg(:,emg_id)), 201);
@@ -53,6 +47,9 @@ EMG_baseline_end = 46000;
 
 tmp_emg(:,:)= tmp_emg(:,:)-repmat(mean(tmp_emg(EMG_baseline_start:EMG_baseline_end,:)),length(tmp_emg),1);
 %Subtract baseline of EMG envelope
+
+
+%% STA and PDm Calculation
 tic
 for i=1:length(List)
 %for i=1
@@ -61,36 +58,17 @@ for i=1:length(List)
     disp(update);
 
     u= get_unit(bdf, List(i,1), List(i,2));
+    %Gets spike times for all sorted units
 
-    tmp_emg = bdf.emg.data(:, 2 : size(bdf.emg.data,2) - exclEMG);
-    %var_emg = var(tmp_emg);
-    p=prctile(tmp_emg,[5,50,95]);
-    tmp_emg=(tmp_emg-p(2))/(p(3)-p(1));
-    %tmp_emg = tmp_emg.*(repmat(var_emg, size(tmp_emg,1),1).^-1);
-
-    for emg_id=1:nEmgs
-     
-        tmp_emg(:,emg_id) = smooth(abs(tmp_emg(:,emg_id)), 201);
-
-    
-    end
-    %Rectify and smooth EMG signals
-
-    %u=rand(1,3000)*bdf.meta.duration;
-    
     tmp_sta = STAsl(u, [t tmp_emg], timebefore, timeafter);
-
     %Calculate STA with smoothed, rectified, normalized, and baseline
-    %subtracted EMG envelope
-    
-    %stas(:,:,i) = tmp_sta(:,2:end) - repmat(mean(tmp_sta(:,2:end)), size(tmp_sta,1), 1);
+    %subtracted EMG envelope-(tmp_emg)
     
     stas(:,:,i) = tmp_sta(:,2:end);
     %STAs matrix row=time, col=Each muscle
 
     tsta = tmp_sta(:,1);
 
-    
     stasnorm(:,:,i) = tmp_sta(:,2:end) - repmat((mean(tmp_sta(1:1000,2:end))+mean(tmp_sta(7000:8000,2:end)))/2, size(tmp_sta,1), 1);
     %Subtract mean to normalize
 
