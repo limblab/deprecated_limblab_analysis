f804667581b08ab04088ce1097af06f2689dda79
Mike Scheid <michaelscheid2015@northwestern.edu>
2011-07-06 20:15:44 +0000

    Resolved conflicts
    git-svn-id: http://citadel.physiology.northwestern.edu/svn/s1_analysis@543 984b1e3c-1c57-0410-ae62-c18f90d3b645

--- proc/michael/emgpds.m
+++ proc/michael/emgpds.m
@@ -1,21 +1,18 @@
-<<<<<<< .mine
-function PDm = emgpds(bdf)
-
-=======
 function [PDm,stas]  = emgpds(bdf)
+function [PDm stasnorm]  = emgpds(bdf)
 
->>>>>>> .r499
+
 % emgpds.m
 % Calculates STA based PDS in emgspace from bdf
 
 % $Id: $
 
-<<<<<<< .mine
-List=unit_list(bdf)
-=======
+
+
+
 List=unit_list(bdf);
 List = List(List(:,2)~=0 & List(:,2) ~=255,:);
->>>>>>> .r499
+
 
 exclEMG=1;
 %Last muscle EMG no good
@@ -30,11 +27,33 @@ stas = zeros((bdf.emg.emgfreq*(timebefore+timeafter))+1,nEmgs,length(List));
 t = bdf.emg.data(:,1);
 tmp_emg = zeros(size(bdf.emg.data,1),size(bdf.emg.data,2) - exclEMG);
 
-<<<<<<< .mine
-for i=1:length(List)
-=======
+
+
+
 PDm= zeros(length(List), nEmgs+4);
 
+tmp_emg = bdf.emg.data(:, 2 : size(bdf.emg.data,2) - exclEMG);
+
+%Find 1st 50th and 99th percentile of data in order to normalize properly
+p=prctile(tmp_emg,[1,50,99]);
+
+%Subtract mean (50th %tile) and divide by difference of 99th and 1st %tile
+tmp_emg=(tmp_emg-repmat(p(2,:),length(tmp_emg),1))./(repmat(p(3,:),length(tmp_emg),1)-repmat(p(1,:),length(tmp_emg),1));
+
+
+
+for emg_id=1:nEmgs
+    tmp_emg(:,emg_id) = smooth(abs(tmp_emg(:,emg_id)), 201);
+end
+%Rectify and smooth EMG signals
+
+EMG_baseline_start= 37000;
+EMG_baseline_end = 46000;
+%Pick a period of data which is representative of baseline activity
+
+tmp_emg(:,:)= tmp_emg(:,:)-repmat(mean(tmp_emg(EMG_baseline_start:EMG_baseline_end,:)),length(tmp_emg),1);
+%Subtract baseline of EMG envelope
+tic
 for i=1:length(List)
 %for i=1
 
@@ -52,36 +71,17 @@ for i=1:length(List)
     for emg_id=1:nEmgs
      
         tmp_emg(:,emg_id) = smooth(abs(tmp_emg(:,emg_id)), 201);
->>>>>>> .r499
+
     
-<<<<<<< .mine
-    u= get_unit(bdf, List(i,1), List(i,2));
-    
-    for emg_id = 1:nEmgs
-        disp(emg_id);
-        tmp_emg = bdf.emg.data(:,emg_id+1);
-        tmp_emg = tmp_emg ./ var(tmp_emg);
-        tmp_emg = smooth(abs(tmp_emg), 201);
-
-        tmp_sta = STA(u, [t tmp_emg], 2.5, 2.5);
-        stas(emg_id,:) = tmp_sta(:,2) - repmat(mean(tmp_sta(:,2)), length(tmp_sta(:,2)), 1);
-        tsta = tmp_sta(:,1);
-    end
-
-    n = sqrt(sum(stas.^2));
-    opt_delay = find(n==max(n), 1, 'first');
-    opt_delay_t = tsta(opt_delay);
-
-    PDm = stas(:, opt_delay);
-    PDm(1,i)= List(i,1);
-    PDm(2:length(PDm),i) = PDm./ sqrt(sum(PDm.^2));
-=======
     end
     %Rectify and smooth EMG signals
 
+    %u=rand(1,3000)*bdf.meta.duration;
     
     tmp_sta = STAsl(u, [t tmp_emg], timebefore, timeafter);
 
+    %Calculate STA with smoothed, rectified, normalized, and baseline
+    %subtracted EMG envelope
     
     %stas(:,:,i) = tmp_sta(:,2:end) - repmat(mean(tmp_sta(:,2:end)), size(tmp_sta,1), 1);
     
@@ -89,7 +89,10 @@ for i=1:length(List)
     %STAs matrix row=time, col=Each muscle
 
     tsta = tmp_sta(:,1);
->>>>>>> .r499
+
+    
+    stasnorm(:,:,i) = tmp_sta(:,2:end) - repmat((mean(tmp_sta(1:1000,2:end))+mean(tmp_sta(7000:8000,2:end)))/2, size(tmp_sta,1), 1);
+    %Subtract mean to normalize
 
     n = sqrt(sum(stas(:,:,i).^2,2));
     %Calculate magnitude of STAs across time
@@ -112,6 +115,7 @@ for i=1:length(List)
     PDm(i,5:end) = PDm(i,5:end)./ sqrt(sum(PDm(i,5:end).^2));
     %Assign STAs at opt_delay to PDM and convert STA vector to unit vector by dividing
     %by vector length
+    toc
 end
 
 
