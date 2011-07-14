c02de3c83b915b41ea30874460f65741c6c39f33
Mike Scheid <michaelscheid2015@northwestern.edu>
2011-07-14 17:39:10 +0000

    Deleted redundant file
    git-svn-id: http://citadel.physiology.northwestern.edu/svn/s1_analysis@548 984b1e3c-1c57-0410-ae62-c18f90d3b645

--- proc/michael/LFP Spike Pos Decode/RDFstartup.m
+++
@@ -1,6 +0,0 @@
-addpath(genpath('C:\Documents and Settings\Administrator\Desktop\RobertF\MarcSmatlabCode'))
-addpath(genpath('C:\Documents and Settings\Administrator\Desktop\RobertF\my code_mods'))
-addpath(genpath('C:\Documents and Settings\Administrator\Desktop\s1_analysis'))
-cd('C:\Documents and Settings\Administrator\Desktop\RobertF\data')
-dbstop if error
-clc;
