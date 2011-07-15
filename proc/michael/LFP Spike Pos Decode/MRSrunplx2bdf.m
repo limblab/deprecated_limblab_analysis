ba045cce4b4f195435177ce43f60f19417a145ea
Mike Scheid <michaelscheid2015@northwestern.edu>
2011-07-15 19:24:23 +0000

    Corrected file paths names
    git-svn-id: http://citadel.physiology.northwestern.edu/svn/s1_analysis@551 984b1e3c-1c57-0410-ae62-c18f90d3b645

--- proc/michael/LFP Spike Pos Decode/MRSrunplx2bdf.m
+++ proc/michael/LFP Spike Pos Decode/MRSrunplx2bdf.m
@@ -7,7 +7,7 @@
 % final-03','ChewieSpikeLFP114-final-02',
 % filelist={'MiniSpikeLFP046-10','MiniSpikeLFP083-08','MiniSpikeLFP108-25','MiniSpikeLFP144&145-11'}
 % 'ChewieSpikeLFP270-03',
-filelist={'ChewieSpikeLFP105.nev','ChewieSpikeLFP115','ChewieSpikeLFP116','ChewieSpikeLFP117','ChewieSpikeLFP118'};
+filelist={'Chewie_Spike_LFP2_105','Chewie_Spike_LFP2_115','Chewie_Spike_LFP2_116','Chewie_Spike_LFP2_117','Chewie_Spike_LFP2_118'};
 for i=1:length(filelist)
 fnam=filelist{i}
 bdf=get_plexon_data([fnam,'.plx'],1);   %number is the LAB NUMBER!! Should be 1 for Mini
