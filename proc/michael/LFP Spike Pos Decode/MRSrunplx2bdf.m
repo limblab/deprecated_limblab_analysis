6dde513034a13b341744c299946073815e00b650
Mike Scheid <michaelscheid2015@northwestern.edu>
2011-07-15 16:20:08 +0000

    Renamed
    git-svn-id: http://citadel.physiology.northwestern.edu/svn/s1_analysis@550 984b1e3c-1c57-0410-ae62-c18f90d3b645

---
+++ proc/michael/LFP Spike Pos Decode/MRSrunplx2bdf.m
@@ -0,0 +1,17 @@
+% fnam='Thor_11-3-10_prone_iso_001'
+% fnam='ChewieSpikeLFP202-final'
+% bdf=get_plexon_data([fnam,'.plx'],0);
+% save([fnam,'.mat'],'bdf')
+% clear
+% 'ChewieSpikeLFP113-final',ChewieSpikeLFP068-final-02','ChewieSpikeLFP069-
+% final-03','ChewieSpikeLFP114-final-02',
+% filelist={'MiniSpikeLFP046-10','MiniSpikeLFP083-08','MiniSpikeLFP108-25','MiniSpikeLFP144&145-11'}
+% 'ChewieSpikeLFP270-03',
+filelist={'ChewieSpikeLFP105.nev','ChewieSpikeLFP115','ChewieSpikeLFP116','ChewieSpikeLFP117','ChewieSpikeLFP118'};
+for i=1:length(filelist)
+fnam=filelist{i}
+bdf=get_plexon_data([fnam,'.plx'],1);   %number is the LAB NUMBER!! Should be 1 for Mini
+save([fnam,'.mat'],'bdf')
+end
+% [fparr,movetimes,words,mg,rt,eventsTable,etall] = bdf2fparr_mg(fnam,2500,2500,'rew');
+
