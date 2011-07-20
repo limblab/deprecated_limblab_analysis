4db994fae2171a80d4a209180a58665a36c35969
Robert Flint <r-flint@northwestern.edu>
2011-07-20 17:48:44 +0000

    git-svn-id: http://citadel.physiology.northwestern.edu/svn/s1_analysis@564 984b1e3c-1c57-0410-ae62-c18f90d3b645

--- proc/robert/buildLFPpositionDecoder.m
+++ proc/robert/buildLFPpositionDecoder.m
@@ -1,52 +1,54 @@
 %% Identify the file for loading
 [FileName,PathName,FilterIndex] = uigetfile('C:\Documents and Settings\Administrator\Desktop\RobertF\data\','select a *.plx file','*.plx');
 cd(PathName)
-%% load the file (skip if already loaded in .mat file)
-bdf=get_plexon_data(FileName);
+%% load the file (skip if you've just loaded in the .mat file instead of the .plx)
+out_struct=get_plexon_data(FileName);
+save([regexp(FileName,'.*(?=\.plx)','match','once'),'.mat'],'out_struct')
 disp(sprintf('\n\n\n\n\n=====================\nFILE LOADED\n===================='))
 
 %% input parameters - Do not Change, just run.
 disp('assigning static parameters')
 
 fnam=FileName(1:end-4);
-disJoint=find(diff(cellfun(@length,bdf.raw.analog.data)));
+disJoint=find(diff(cellfun(@length,out_struct.raw.analog.data)),1);
 if ~isempty(disJoint)
-    disp('error, mismatched lengths in bdf.raw.analog.data.  quitting...')
-    return
+    disp('error, mismatched lengths in out_struct.raw.analog.data.  attempting to correct...')
+	setLength=min(unique(cellfun(@length,out_struct.raw.analog.data)));
+	for n=1:length(out_struct.raw.analog.data)
+		out_struct.raw.analog.data{n}=out_struct.raw.analog.data{n}(1:setLength);
+	end
 end
-% possible solution for the disJoint problem.  
-% for n=disJoint+1:length(bdf.raw.analog.data)
-%     bdf.raw.analog.data{n}(end)=[];
-% end
-% different possible solution
-% for n=1:disJoint
-%     bdf.raw.analog.data{n}(end)=[];
-% end
-fpchans=find(cellfun(@isempty,regexp(bdf.raw.analog.channels,'FP[0-9]+'))==0);
-fp=double(cat(2,bdf.raw.analog.data{fpchans}))';
-samprate=bdf.raw.analog.adfreq(fpchans(1));
+disJoint=find(diff(cellfun(@length,out_struct.raw.analog.data)));
+if ~isempty(disJoint)
+    disp('still mismatched lengths in out_struct.raw.analog.data.  quitting...')
+end
+% downsample?  If brainreader is operating with a wsz of 256 and a sampling
+% rate of 2000, then so should the offline decoder build I guess.  delta
+% band is going to be empty...
+fpchans=find(cellfun(@isempty,regexp(out_struct.raw.analog.channels,'FP[0-9]+'))==0);
+fp=double(cat(2,out_struct.raw.analog.data{fpchans}))';
+samprate=out_struct.raw.analog.adfreq(fpchans(1));
 
-sig=bdf.vel;
+sig=out_struct.vel;
 signal='vel';
 numfp=size(fp,1);
 numsides=1;
-fptimes=1/samprate:1/samprate:size(bdf.raw.analog.data{1},1)/samprate;
+fptimes=1/samprate:1/samprate:size(out_struct.raw.analog.data{1},1)/samprate;
 Use_Thresh=0; words=[]; emgsamplerate=[]; lambda=1;
 analog_times=sig(:,1);
 disp('done')
 %% Input parameters to play with.
 disp('assigning tunable parameters and building the decoder...')
-folds=2; 
 numlags=10; 
 wsz=256; 
 nfeat=100;
 PolynomialOrder=2; 
-smoothfeats=0;
+smoothfeats=1;
 binsize=0.05;
 
 [vaf,vmean,vsd,y_test,y_pred,r2mean,r2sd,r2,vaftr,bestf,bestc,H,bestfeat,x,y, ...
     featMat,ytnew,xtnew,predtbase,P,featind,sr] = ...
-    predictionsfromfp6(sig,signal,numfp,binsize,folds,numlags,numsides, ...
+    buildModel_fp(sig,signal,numfp,binsize,numlags,numsides, ...
     samprate,fp,fptimes,analog_times,fnam,wsz,nfeat,PolynomialOrder, ...
     Use_Thresh,words,emgsamplerate,lambda,smoothfeats);
 
@@ -63,87 +65,50 @@ formatstr=[formatstr, '\n'];
 fprintf(1,formatstr,mean(vaf,1))
 fprintf(1,'overall mean vaf %.4f\n',mean(vaf(:)))
 
-
-%% pick a fold, and save (from EWL Convert2ReachDecoder)
-FOLDTOSAVE=ind; % defaults to highest sum.  Can of course modify in any way.
-if(~exist('Hall', 'var'))
-	Hall = H;
-end
-H = Hall{FOLDTOSAVE};
-nChans = numfp;
+% transpose P?
 
 chanIDs = unique(bestc');
-% detect when the recording was done; shift appropriately if earlier .plx.
-% don't shift at all if .nev.  If later .plx, shift everything so that
-% BrainReader doesn't have to.
-% for i = 1:size(chanIDs,1);
-% 	if(chanIDs(i) > 32)
-% 		chanIDs(i) = chanIDs(i) + 32;
-% 	end
-% end
-[~,~,extension,~]=fileparts(FileName);
-% if isequal(extension,'.plx')
-%     chanIDs=chanIDs+32;
-% end
-
 samplingFreq = samprate;
+fillen=numlags*binsize;
+neuronIDs='';
 freq_bands =  [0,0;0,4;7,20;70,115;130,200;200,300];
-
 featmat = [bestc', bestf']; 
-nFeats = size(featmat,1);
-f_bands = cell(nChans,1);
+f_bands = cell(numfp,1);
 
-for i=1:nChans
+for i=1:numfp
     f_bands{i} = sortrows(freq_bands(featmat(featmat(:,1)==i,2),:),2);
 end
 
-cH = cell(nFeats,3);
-
-for i=1:nFeats
+cH = cell(nfeat,3);
+for i=1:nfeat
     cH{i, 1} = bestc(i);
     cH{i, 2} = bestf(i);
     cHfirstr = numlags*(i-1)+1;
     cHlastr  = numlags*i;
     a = H(cHfirstr:cHlastr,:);
-  %  a = flipdim(a,1);
     cH{i, 3} = a;
- %   cH{i, 3} = H(cHfirstr:cHlastr,:);
 end
 
-[trash, is] = sortrows(cell2mat(cH(:,1)),1);
+[~, is] = sortrows(cell2mat(cH(:,1)),1);
 cH = cH(is,:,:);
 % do a secondary sort based on bestf
-for i=1:nChans
+for i=1:numfp
     f = find([cH{:,1}]==i);
     if(~isempty(f))
-        [trash, is] = sortrows(cell2mat(cH(f,2)),1);
+        [~, is] = sortrows(cell2mat(cH(f,2)),1);
         cH(f,:) = cH(((min(f)-1)+is), :);
     end
 end
-
 H_sorted = cell2mat(cH(:,3));
-filterLength=numlags*binsize;
+
 
 % I don't think the filter.xxxx are necessary.  Just the variables.
-% H has to be from 1 fold.  NxM, with M the number of outputs
-% P should be included
-% must have neuronsIDs.  Can be empty if only going to do LFP decoding.
-% fillen.
-% chanIDs.  
-% samplingFreq
-% f_bands
-%
+
 % yes, H should probably be sorted, because the H matrix comes out of
 % something that's ordered on the features, and the order of the features
 % is caused by correlation to pos/vel, NOT channel order.
-save(fullfile(PathName,[fnam,'-decoder.mat']))
-
-% save(fullfile(PathName,[fnam,'-decoder.mat']),'FOLDTOSAVE','H','H_sorted','Hall',...
-%     'PolynomialOrder','Use_Thresh','bestc','bestf','binsize','cH','chanIDs', ...
-%     'f_bands','featmat','fnam','folds','freq_bands','lambda','nChans','nFeats', ...
-%     'nfeat','numlags','numsides','r2','r2mean','r2sd','samplingFreq','samprate', ...
-%     'signal','smoothfeats','vaf','vaftr','vmean','vsd','wsz','y_pred','y_test','filterLength')
-
+save(fullfile(PathName,[fnam,'-decoder.mat']),'H','P','neuronIDs','fillen','binsize', ...
+	'chanIDs','samplingFreq','f_bands')
 
 
 disp(sprintf('decoder saved in %s',PathName))
