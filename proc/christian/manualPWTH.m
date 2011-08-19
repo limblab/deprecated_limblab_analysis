5edbf45d11d45f1ccbd69e6355a8ac34bc888819
Christian Ethier <christian.ethier@gmail.com>
2011-08-19 22:36:21 +0000

    git-svn-id: http://citadel.physiology.northwestern.edu/svn/s1_analysis@588 984b1e3c-1c57-0410-ae62-c18f90d3b645

--- proc/christian/manualPWTH.m
+++ proc/christian/manualPWTH.m
@@ -2,8 +2,8 @@
 wordsVect= binnedData.words;
 W = BD_Words;
 wordToAve = W.Pickup;
-timeBefore = -1 ;
-timeAfter  =  1 ;
+timeBefore =  0.8 ;
+timeAfter  =  3 ;
 
 % Jaco
 % FDSr FDSu FDPr FDPu ECR1 FCR2 ECRb FCU2 EDCu OP ECU2 EPL
@@ -13,16 +13,39 @@ timeAfter  =  1 ;
 % FDSu FDSm FDPu FDPm FCR1  FCU  FPB  FDI ECR EDC 1/2IO ECU
 %  1     2   3    4    5     6    7    8   9  10    11   12
 
-EMGsToPlot = [1 3 5 9 10];
+EMGsToPlot = [2 3 5];
 
 signals = [binnedData.timeframe binnedData.emgdatabin(:,EMGsToPlot)];
 ActualAveEMGs = PWTH(signals,wordsVect,wordToAve,timeBefore,timeAfter);
 
-signals = [PredData.timeframe preddatabin(:,EMGsToPlot)];
+signals = [OLPredData.timeframe OLPredData.preddatabin(:,EMGsToPlot)];
 PredAveEMGs = PWTH(signals,wordsVect,wordToAve,timeBefore,timeAfter);
 figure;
 plot(ActualAveEMGs(:,1),ActualAveEMGs(:,2:end));
 hold on;
-plot(PredAveEMGs(:,1),PredAveEMGs(:,2:end));
+plot(PredAveEMGs(:,1),PredAveEMGs(:,2:end),':');
 
 legend(deblank(binnedData.emgguide(EMGsToPlot,:)));
+
+
+%-----------------------------------------------------------------------
+
+
+W = BD_Words;
+timeBefore =  2 ;
+timeAfter  =  2 ;
+% EMGsToPlot = [2 4 5 8 9];
+EMGsToPlot = [2 3 5];
+
+
+EarlyAve = PWTH([OLPredDataEarly0211.timeframe OLPredDataEarly0211.preddatabin(:,EMGsToPlot)],...
+                 binnedDataEarly0211.words, W.Pickup, timeBefore, timeAfter);
+LateAve  = PWTH([OLPredDataLate0211.timeframe OLPredDataLate0211.preddatabin(:,EMGsToPlot)],...
+                 binnedDataLate0211.words, W.Pickup, timeBefore, timeAfter);
+             
+figure;
+plot(EarlyAve(:,1),EarlyAve(:,2:end),'Linewidth',2);
+hold on;
+plot(LateAve(:,1),LateAve(:,2:end),':','Linewidth',2);
+legend(deblank(binnedDataEarly0211.emgguide(EMGsToPlot,:)));
+Title('Predictions around pick up time \newline Jaco\_02-11-11 \newline - Early(\_006) -- Late(\_015)');
