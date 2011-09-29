9734d918509f255f5e7c9be1eb7237e4822094d3
Christian Ethier <christian.ethier@gmail.com>
2011-09-29 19:29:25 +0000

    changed get_stim_commands.m to include extraction of some of the stim commands for manual stimulation mode.
    git-svn-id: http://citadel.physiology.northwestern.edu/svn/s1_analysis@609 984b1e3c-1c57-0410-ae62-c18f90d3b645

--- bdf/get_stim_commands.m
+++ bdf/get_stim_commands.m
@@ -2,7 +2,7 @@ function stim = get_stim_commands(bdf)
 
 
 % Data recorded before the stimulator was updated with the termination pound caracter.
-if datenum(bdf.meta.datetime) - datenum('03-Mar-2011 00:00:00') < 0 
+% if datenum(bdf.meta.datetime) - datenum('03-Mar-2011 00:00:00') < 0 
     
     %find 1 ms gaps between bytes - normally 1 byte every 0.1 ms during a command.
     % so a 1ms delay should mean we have a new command.
@@ -14,13 +14,16 @@ if datenum(bdf.meta.datetime) - datenum('03-Mar-2011 00:00:00') < 0
         cmd_start = cmd_start(1:end-1);
         cmd_end = cmd_end(1:end-1);
     end
-        
-else
-    %There is a pound character at the end of each command
-    
-    cmd_end   = find(bdf.raw.serial(:,2) == double('#'));
-    cmd_start = cmd_end(1:end-1) + 1; %skip first cmd, probably incomplete anyways.
-    cmd_end   = cmd_end(2:end) - 1; % -1 to skip # character
+
+% else  % Using the pound sign is not as useful because it forces us to skip the first command.
+%       % In manual mode, the first command is normally complete, and important...
+%     %There is a pound character at the end of each command
+%     cmd_end   = find(bdf.raw.serial(:,2) == double('#'));
+%     cmd_start = cmd_end(1:end-1) + 1; %skip first cmd, probably incomplete anyways.
+
+if datenum(bdf.meta.datetime) - datenum('03-Mar-2011 00:00:00') > 0 
+%     cmd_end   = find(bdf.raw.serial(:,2) == double('#'));    
+    cmd_end   = cmd_end - 1; % -1 to skip # character
 end
 
 num_cmd = length(cmd_start);
@@ -29,6 +32,7 @@ ts   = zeros(num_cmd,1);
 chan = zeros(num_cmd,1);
 I    = zeros(num_cmd,1);
 PW   = zeros(num_cmd,1);
+stim = [];
 
 for cmd=1:num_cmd
     asciicmd = char(bdf.raw.serial(cmd_start(cmd):cmd_end(cmd),2)');
@@ -41,21 +45,71 @@ for cmd=1:num_cmd
     end
 %     stim{cmd,1} = bdf.raw.serial(cmd_end(cmd),1);
 %     stim{cmd,2} = decoded_cmd;
-    if decoded_cmd(1)~= 0
-        %TODO: extract all stim commands.
-        %   For now, only commands with prefix 0 are decoded (load and run immediately)
-        %   and this assumes commands are issued to only one channel at a time (NLMSv6 auto mode)
-        continue;
+
+    switch decoded_cmd(1)
+        case 0  %load and run immediately
+            ts_tmp   = bdf.raw.serial(cmd_end(cmd),1);
+            chan_tmp = find(dec2binvec(decoded_cmd(2)));
+            I_tmp    = decoded_cmd(5)/1000; %µA to mA
+            PW_tmp   = decoded_cmd(7); %µs
+
+            num_chan = length(chan_tmp);
+
+            ts   = repmat(ts_tmp,num_chan,1);
+            chan = chan_tmp';
+            I    = repmat(I_tmp,num_chan,1);
+            PW   = repmat(PW_tmp,num_chan,1);       
+
+        case 1 % run all channels with programs
+            % TODO
+
+        case 2 % halt all running channels
+            % TODO
+
+        case 3 % run list of channels
+            % TODO
+
+        case 4 % halt list of channels
+            ts_tmp   = bdf.raw.serial(cmd_end(cmd),1);
+            chan_tmp = find(dec2binvec(decoded_cmd(2)));
+
+            num_chan = length(chan_tmp);
+
+            ts   = repmat(ts_tmp,num_chan,1);
+            chan = chan_tmp';
+            I    = repmat(0,num_chan,1);
+            PW   = repmat(0,num_chan,1); 
+
+        case 5 % program to follow (load but do not run)
+            % TODO
+        otherwise
+            ts = bdf.raw.serial(cmd_end(cmd),1);
+            chan = -1;
+            I    = -1;
+            PW   = -1;
+            
     end
-    ts(cmd)   = bdf.raw.serial(cmd_end(cmd),1);
-    chan(cmd) = log2(decoded_cmd(2));
-    I(cmd)    = decoded_cmd(5)/1000; %µA to mA
-    PW(cmd)   = decoded_cmd(7); %µs
+
+stim = [stim; [ts chan I PW]];
+
 end
 
-stim = [ts chan I PW];
-
-%remove lines with skipped commands (not prefix 0)
-stim = stim(any(stim,2),:);
+% if decoded_cmd(1)~= 0
+%         %TODO: extract all stim commands.
+%         %   For now, only commands with prefix 0 are decoded (load and run immediately)
+%         %   and this assumes commands are issued to only one channel at a time (NLMSv6 auto mode)
+%         continue;
+%     end
+%     
+%     ts(cmd)   = bdf.raw.serial(cmd_end(cmd),1);
+%     chan(cmd) = log2(decoded_cmd(2));
+%     I(cmd)    = decoded_cmd(5)/1000; %µA to mA
+%     PW(cmd)   = decoded_cmd(7); %µs
+% end
+% 
+% stim = [ts chan I PW];
+% 
+% %remove lines with skipped commands (not prefix 0)
+% stim = stim(any(stim,2),:);
     
     
