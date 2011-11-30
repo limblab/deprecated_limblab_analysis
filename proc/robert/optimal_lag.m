47fed300121bd85fad5f43926d701f7c8dfa68d3
Robert Flint <r-flint@northwestern.edu>
2011-11-30 22:07:06 +0000

    git-svn-id: http://citadel.physiology.northwestern.edu/svn/s1_analysis@657 984b1e3c-1c57-0410-ae62-c18f90d3b645

--- proc/robert/optimal_lag.m
+++ proc/robert/optimal_lag.m
@@ -1,7 +1,14 @@
-function [vaf_L,vaf_S]=optimal_lag(PathName)
+function [vaf_L,vaf_S]=optimal_lag(PathName,processFlag)
+
+% processFlag is 1 for LFP only, 2 for spikes only.  3 is both, or just
+% leave the input off altogether, the default is to do both.
 
 if ~nargin
     PathName=pwd;
+end
+if nargin < 2
+    % default to 'both'
+    processFlag=3;
 end
 cd(PathName)
 % cd('/Volumes/limblab/user_folders/Robert/data/monkey/outputs/Spike_LFP_EMG/outputs_1lag')
@@ -15,61 +22,64 @@ spikeFiles=spikeFiles(sortCustomRegex({D(spikeFiles).name},{'Chewie','Mini','Jac
 
 vaf_L=[];
 % LFP files
-for n=1:length(LFPfiles)
-	recordingName=regexp(D(LFPfiles(n)).name,['.*(?=',regexpi(D(LFPfiles(n)).name, ...
-		'sortedtik|tik','match','once'),')'],'match','once')
-    disp('LFPs')
-    x_L{n}=load(D(LFPfiles(n)).name,'x');
-    x_L{n}=x_L{n}.x;
-    y_L{n}=load(D(LFPfiles(n)).name,'y');
-    y_L{n}=y_L{n}.y;
-	allEMGnamesL{n}=load(D(LFPfiles(n)).name,'EMGchanNames');	
-	allEMGnamesL{n}=allEMGnamesL{n}.EMGchanNames;
-
-    % it is a mistake to take out bad EMG days  from this function, because
-    % it takes a while to run it would be better to include all days, then
-    % exclude anything that requires exclusion only at the plotting level.
-    for k=1:length(allEMGnamesL{n})
-        [startInd,endInd]=regexp(allEMGnamesL{n}{k},'EMG_');
-        allEMGnamesL{n}{k}(startInd:endInd)='';
+if processFlag~=2
+    for n=1:length(LFPfiles)
+        recordingName=regexp(D(LFPfiles(n)).name,['.*(?=',regexpi(D(LFPfiles(n)).name, ...
+            'sortedtik|tik','match','once'),')'],'match','once')
+        disp('LFPs')
+        x_L{n}=load(D(LFPfiles(n)).name,'x');
+        x_L{n}=x_L{n}.x;
+        y_L{n}=load(D(LFPfiles(n)).name,'y');
+        y_L{n}=y_L{n}.y;
+        allEMGnamesL{n}=load(D(LFPfiles(n)).name,'EMGchanNames');
+        allEMGnamesL{n}=allEMGnamesL{n}.EMGchanNames;
+        
+        % it is a mistake to take out bad EMG days  from this function, because
+        % it takes a while to run it would be better to include all days, then
+        % exclude anything that requires exclusion only at the plotting level.
+        for k=1:length(allEMGnamesL{n})
+            [startInd,endInd]=regexp(allEMGnamesL{n}{k},'EMG_');
+            allEMGnamesL{n}{k}(startInd:endInd)='';
+        end
+        
+        for k=10:-1:1                          % [1 5 10 15 20]
+            shiftedY=circshift(y_L{n},k);
+            [~,vaf_L{n,k},~,~,~,~,y_pred,~,ytnew]=predonlyxy_nofeatselect(x_L{n}(k+1:end,:), ...
+                shiftedY(k+1:end,:),3,0,1,1,1,1,10,0);
+        end
     end
-    
-    for k=10:-1:1                          % [1 5 10 15 20]
-        shiftedY=circshift(y_L{n},k);
-        [~,vaf_L{n,k},~,~,~,~,y_pred,~,ytnew]=predonlyxy_nofeatselect(x_L{n}(k+1:end,:), ...
-            shiftedY(k+1:end,:),3,0,1,1,1,1,10,0);
-    end
+    vaf_L(:,sum(cellfun(@isempty,vaf_L),1)>0)=[];
 end
-vaf_L(:,sum(cellfun(@isempty,vaf_L),1)>0)=[];
 
 vaf_S=[];
 % spike files
-for n=1:length(spikeFiles)
-    % leave as LFPfiles in recordingName, for the sake of identifying the
-    % badChannels
-	recordingName=regexp(D(LFPfiles(n)).name,['.*(?=',regexpi(D(LFPfiles(n)).name, ...
-		'sortedtik|tik','match','once'),')'],'match','once')
-    disp('spikes')
-    x_S{n}=load(D(spikeFiles(n)).name,'x');
-    x_S{n}=x_S{n}.x;
-    y_S{n}=load(D(spikeFiles(n)).name,'y');
-    y_S{n}=y_S{n}.y;
-	allEMGnamesS{n}=load(D(spikeFiles(n)).name,'EMGchanNames');	
-	allEMGnamesS{n}=allEMGnamesS{n}.EMGchanNames;
-    
-    for k=1:length(allEMGnamesS{n})
-        [startInd,endInd]=regexp(allEMGnamesS{n}{k},'EMG_');
-        allEMGnamesS{n}{k}(startInd:endInd)='';
+if processFlag > 1
+    for n=1:length(spikeFiles)
+        % leave as LFPfiles in recordingName, for the sake of identifying the
+        % badChannels
+        recordingName=regexp(D(LFPfiles(n)).name,['.*(?=',regexpi(D(LFPfiles(n)).name, ...
+            'sortedtik|tik','match','once'),')'],'match','once')
+        disp('spikes')
+        x_S{n}=load(D(spikeFiles(n)).name,'x');
+        x_S{n}=x_S{n}.x;
+        y_S{n}=load(D(spikeFiles(n)).name,'y');
+        y_S{n}=y_S{n}.y;
+        allEMGnamesS{n}=load(D(spikeFiles(n)).name,'EMGchanNames');
+        allEMGnamesS{n}=allEMGnamesS{n}.EMGchanNames;
+        
+        for k=1:length(allEMGnamesS{n})
+            [startInd,endInd]=regexp(allEMGnamesS{n}{k},'EMG_');
+            allEMGnamesS{n}{k}(startInd:endInd)='';
+        end
+        
+        for k=10:-1:1                          % [1 5 10 15 20]
+            shiftedY=circshift(y_S{n},k);
+            [~,vaf_S{n,k},~,~,~,~,y_pred,~,ytnew]=predonlyxy_nofeatselect(x_S{n}(k+1:end,:), ...
+                shiftedY(k+1:end,:),3,0,1,1,1,1,10,0);
+        end
     end
-    
-    for k=10:-1:1                          % [1 5 10 15 20]
-        shiftedY=circshift(y_S{n},k);
-        [~,vaf_S{n,k},~,~,~,~,y_pred,~,ytnew]=predonlyxy_nofeatselect(x_S{n}(k+1:end,:), ...
-            shiftedY(k+1:end,:),3,0,1,1,1,1,10,0);
-    end
+    vaf_S(:,sum(cellfun(@isempty,vaf_S),1)>0)=[];
 end
-vaf_S(:,sum(cellfun(@isempty,vaf_S),1)>0)=[];
-
 
 
 
