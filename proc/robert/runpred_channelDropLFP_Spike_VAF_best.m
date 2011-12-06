dc36dd8a8114b41749d2611dfd238f70e21b092c
Robert Flint <r-flint@northwestern.edu>
2011-12-06 21:54:09 +0000

    git-svn-id: http://citadel.physiology.northwestern.edu/svn/s1_analysis@665 984b1e3c-1c57-0410-ae62-c18f90d3b645

--- proc/robert/runpred_channelDropLFP_Spike_VAF_best.m
+++ proc/robert/runpred_channelDropLFP_Spike_VAF_best.m
@@ -17,7 +17,6 @@ end
 status=mkdir(folderNew);
 
 if status
-    
     % different for LFPs vs. spikes.  for LFPs, part of the decoder file is
     % bestc, so we can load that up and know what the ranking of channels was.
     % For spikes, the decoder uses all the channels, so use the weightings of
@@ -39,78 +38,98 @@ if status
     end
     
     
-    for i=1:length(LFPfiles)
-        FileName=FileNames{LFPfiles(i)};
-        load(FileName,'x')
-        load(FileName,'y')
-        load(FileName,'bestc')
-        load(FileName,'EMGchanNames')
-        
-        EMGVAFmallLFP=nan(length(unique(bestc)),size(EMGchanNames,2));
-        EMGVAFsdallLFP=nan(length(unique(bestc)),size(EMGchanNames,2));
-        
-        n=1;
-        while length(unique(bestc)) > 1
-            x(:,ismember(bestc,bestc(1)))=[];
-            bestc(:,ismember(bestc,bestc(1)))=[];
-            disp([FileName,' LFP: ',num2str(length(unique(bestc))),' channels'])
-            
-            [vmean,~,~,~,~,~,~,~,~,~,~,~,~,vsd]=predonlyxy_nofeatselect(x,y,2,0,1,1,1,1,10,0);
-            close
-            EMGVAFmallLFP(n,:)=vmean;
-            EMGVAFsdallLFP(n,:)=vsd;
-            n=n+1;
-        end
-        save(fullfile(folderNew,[FileName(1:end-4),'chan drop.mat']),'EMGVAFmallLFP','EMGVAFsdallLFP')
-    end
+%     for i=1:length(LFPfiles)
+%         FileName=FileNames{LFPfiles(i)};
+%         load(FileName,'x')
+%         load(FileName,'y')
+%         load(FileName,'bestc')
+%         load(FileName,'EMGchanNames')
+%         
+%         EMGVAFmallLFP=nan(length(unique(bestc)),size(EMGchanNames,2));
+%         EMGVAFsdallLFP=nan(length(unique(bestc)),size(EMGchanNames,2));
+%         
+%         n=1;
+%         while length(unique(bestc)) > 1
+%             x(:,ismember(bestc,bestc(1)))=[];
+%             bestc(:,ismember(bestc,bestc(1)))=[];
+%             disp([FileName,' LFP: ',num2str(length(unique(bestc))),' channels'])
+%             
+%             [vmean,~,~,~,~,~,~,~,~,~,~,~,~,vsd]=predonlyxy_nofeatselect(x,y,2,0,1,1,1,1,10,0);
+%             close
+%             EMGVAFmallLFP(n,:)=vmean;
+%             EMGVAFsdallLFP(n,:)=vsd;
+%             n=n+1;
+%         end
+%         save(fullfile(folderNew,[FileName(1:end-4),'chan drop.mat']),'EMGVAFmallLFP','EMGVAFsdallLFP')
+%     end
     
     for i=1:length(spikeFiles)
         % spikes have to be done differently.
         FileName=FileNames{spikeFiles(i)};
         load(FileName,'x')
         load(FileName,'y')
-        load(FileName,'H')
+%         load(FileName,'H')
         load(FileName,'EMGchanNames')
         
-        % get the unit list from the bdf.  we'll need it.
-        BDFfilesList=dir('C:\Documents and Settings\Administrator\Desktop\RobertF\data\sorted');
-        recordingName=regexp(FileName,'.*(?=spikes tik)','match','once');
-        if any(cellfun(@isempty,regexp({BDFfilesList.name},recordingName))==0)
-            try
-                load(fullfile('C:\Documents and Settings\Administrator\Desktop\RobertF\data\sorted', ...
-                    recordingName),'bdf')
-            catch ME
-                if strcmp(ME.identifier,'MATLAB:UndefinedFunction')
-                    load(fullfile('C:\Documents and Settings\Administrator\Desktop\RobertF\data\sorted', ...
-                        recordingName),'out_struct')
-                    bdf=out_struct;
-                else
-                    rethrow(ME)
-                end
-            end
-            cells=unit_list(bdf);
+%         % get the unit list from the bdf.  we'll need it.
+%         BDFfilesList=dir('C:\Documents and Settings\Administrator\Desktop\RobertF\data\sorted');
+%         recordingName=regexp(FileName,'.*(?=spikes tik)','match','once');
+%         if any(cellfun(@isempty,regexp({BDFfilesList.name},recordingName))==0)
+%             try
+%                 load(fullfile('C:\Documents and Settings\Administrator\Desktop\RobertF\data\sorted', ...
+%                     recordingName),'bdf')
+%             catch ME
+%                 if strcmp(ME.identifier,'MATLAB:UndefinedFunction')
+%                     load(fullfile('C:\Documents and Settings\Administrator\Desktop\RobertF\data\sorted', ...
+%                         recordingName),'out_struct')
+%                     bdf=out_struct;
+%                 else
+%                     rethrow(ME)
+%                 end
+%             end
+%             cells=unit_list(bdf);
+            % in the newer version, there is a variable, uList, in the
+            % decoder file.
+            cells=load(FileName,'uList');
+            cells=cells.uList;
             cells(cells(:,2)==0,:)=[];
+            rmat=load(FileName,'rmat');
+            % WON'T ALWAYS BE 9.  but for the current plot this suffices in
+            % place of something more complicated.
+            rmat=rmat.rmat(:,9);
+            % average across channels with >1 neuron
+            uniqueCells=unique(cells(:,1));
+            for n=1:length(uniqueCells)
+                % replace each entry with the average of all entries.
+                rmat(cells(:,1)==uniqueCells(n))=mean(abs(rmat(cells(:,1)==uniqueCells(n))));
+            end
+            [~,bestR,~]=unique(rmat);
+            bestR=flipud(bestR);
+            bestc=cells(bestR,1);
             
             EMGVAFmallSpike=nan(length(unique(cells(:,1))),size(EMGchanNames,2));
             EMGVAFsdallSpike=nan(length(unique(cells(:,1))),size(EMGchanNames,2));
             
-            bestHc=[];
-            for n=1:length(H)
-                %             sortInd=sortUnitsOnH(cells,H{n},10);          % 10 is numlags
-                % a strong negative weight means a powerful contribution.
-                % Right?
-                [~,bestH]=sort(abs(H{n}(10:10:size(H{n},1),1)),'descend');
-                bestHc(:,n)=cells(bestH,1);
-            end
-            
-            for n=1:size(bestHc,2)
-                Euc(n)=pdist([bestHc(:,n)'; floor(median(bestHc,2))']);
-            end
-            [~,position]=min(Euc);
-            bestc=bestHc(:,position);
+            % old school.  using H matrix.  outdated.
+%             bestHc=[];
+%             for n=1:length(H)
+%                 %             sortInd=sortUnitsOnH(cells,H{n},10);          % 10 is numlags
+%                 % a strong negative weight means a powerful contribution.
+%                 % Right?
+%                 [~,bestH]=sort(abs(H{n}(10:10:size(H{n},1),1)),'descend');
+%                 bestHc(:,n)=cells(bestH,1);
+%             end
+%             
+%             for n=1:size(bestHc,2)
+%                 Euc(n)=pdist([bestHc(:,n)'; floor(median(bestHc,2))']);
+%             end
+%             [~,position]=min(Euc);
+%             bestc=bestHc(:,position);
             
             n=1;
             while length(unique(bestc)) > 1
+                % this is overkill when bestc is already a unique vector,
+                % but it still should work.
                 x(:,ismember(bestc,bestc(1)))=[];
                 bestc(ismember(bestc,bestc(1)))=[];
                 disp([FileName,' Spike: ',num2str(length(unique(bestc))),' channels'])
@@ -122,9 +141,8 @@ if status
                 n=n+1;
             end
             save(fullfile(folderNew,[FileName(1:end-4),'chan drop.mat']),'EMGVAFmallSpike','EMGVAFsdallSpike')
-        end
+%         end
     end
-    
 else
     error('folder not created')
 end
