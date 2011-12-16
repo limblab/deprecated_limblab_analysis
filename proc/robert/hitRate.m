4e511d678c167a8db39a9a2116c57060e9bb20dc
Robert Flint <r-flint@northwestern.edu>
2011-12-16 08:14:21 +0000

    git-svn-id: http://citadel.physiology.northwestern.edu/svn/s1_analysis@677 984b1e3c-1c57-0410-ae62-c18f90d3b645

--- proc/robert/hitRate.m
+++ proc/robert/hitRate.m
@@ -10,9 +10,15 @@ first_trial_start=find(bdf.words(:,2)==18,1,'first');
 if first_trial_start~=1
     bdf.words(1:(first_trial_start-1),:)=[];
 end
-last_trial_end=find(bdf.words(:,2)==32,1,'last');
+% success (32) | abort (33) | fail (34)
+last_trial_end=find(bdf.words(:,2)==32 | bdf.words(:,2)==33 | bdf.words(:,2)==34,1,'last');  
 if last_trial_end < size(bdf.words,1)
     bdf.words((last_trial_end+1):end,:)=[];
 end
+
+fprintf(1,'number of rewards was %d.\n',sum(bdf.words(:,2)==32))
+fprintf(1,'number of trials was %d.\n',sum(bdf.words(:,2)==18))
+fprintf(1,'number of aborts was %d.\n',sum(bdf.words(:,2)==33))
+fprintf(1,'rewards/min: %.4f\n',sum(bdf.words(:,2)==32)/diff(bdf.words([1 end],1))*60)
 
 hr=sum(bdf.words(:,2)==32)/sum(bdf.words(:,2)==18);
