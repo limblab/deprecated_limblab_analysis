ba1f3c58e256d002a5bd8f6e2e9cb58dd452dc7f
Robert Flint <r-flint@northwestern.edu>
2012-01-20 20:44:01 +0000

    git-svn-id: http://citadel.physiology.northwestern.edu/svn/s1_analysis@706 984b1e3c-1c57-0410-ae62-c18f90d3b645

--- proc/robert/buildListOfBDFs.m
+++ proc/robert/buildListOfBDFs.m
@@ -53,7 +53,7 @@ if mod(length(varargin),2)~=0
     error('arguments beyond startFolder must occur in property/value pairs')
 end
 % determine file type.  
-ftInputIndex=strcmpi(varargin,'file_type');
+ftInputIndex=find(strcmpi(varargin,'file_type'));
 if ~isempty(ftInputIndex)
     ft=varargin{ftInputIndex+1};
 else
@@ -61,28 +61,28 @@ else
 end
 if strcmp(ft,'all'), ft='plx|nev'; end
 % determine duration.  
-durInputIndex=strcmpi(varargin,'duration');
+durInputIndex=find(strcmpi(varargin,'duration'));
 if ~isempty(durInputIndex)
     duration=varargin{durInputIndex+1};
 else
     duration=600;
 end
 % determine hold time.  
-holdInputIndex=strcmpi(varargin,'hold_time');
+holdInputIndex=find(strcmpi(varargin,'hold_time'));
 if ~isempty(holdInputIndex)
     hold_time=varargin{holdInputIndex+1};
 else
     hold_time=0.2;
 end
 % determine type of control.  
-controlInputIndex=strcmpi(varargin,'control');
+controlInputIndex=find(strcmpi(varargin,'control'));
 if ~isempty(controlInputIndex)
     control=varargin{controlInputIndex+1};
 else
     control='hand';
 end
 % determine ordering.  
-orderInputIndex=strcmpi(varargin,'order');
+orderInputIndex=find(strcmpi(varargin,'order'));
 if ~isempty(orderInputIndex)
     order=varargin{orderInputIndex+1};
 else
@@ -101,31 +101,56 @@ fileIndex=0;
 returns=[0 regexp(result,sprintf('\n'))];
 for n=2:length(returns)
     candidatePath=result(returns(n-1)+1:returns(n)-1);
+    fprintf(1,'file:\n')
+    fprintf(1,'%s\n',candidatePath)
     if exist(candidatePath,'file')==2        
         S=load(candidatePath);
         fname=fieldnames(S); 
         fname(cellfun(@isempty,regexpi(fname,'bdf|out_struct')))=[];
         bdf=S.(fname{1}); clear S
+        % date.  Let's impose the additional restriction that the data file
+        % for these purposes be 08/24 or later for Mini, or 09/01 or later
+        % for Chewie
+        if strcmp(animal,'Chewie') && (datenum(bdf.meta.datetime) < datenum('09-01-2011'))
+            fprintf(1,'Chewie data file recorded before 09-01-2011.  skipping...\n')
+            continue                
+        end
+        if strcmp(animal,'Mini') && (datenum(bdf.meta.datetime) < datenum('08-24-2011'))
+            fprintf(1,'Mini data file recorded before 08-24-2011.  skipping...')
+            continue
+        end
         % file type
         if isempty(regexpi(bdf.meta.filename,ft))
+            fprintf(1,'file type was %s\n.  skipping...\n',ft)
             continue
         end
         % duration
         if (bdf.meta.duration < 0.9*duration || bdf.meta.duration > 1.1*duration)
+            fprintf(1,'duration was %f.  skipping...\n',bdf.meta.duration)
             continue
         end
-        % hold time
-        reward_words=find(out_struct.words(:,2)==32);
-        bdfMeanHold=mean(out_struct.words(reward_words,1)- ...
-            out_struct.words(reward_words-1,1));
+        % hold time.  For recordings before 09-12-2011 this is a problem.
+        % Accept everything and weed out later?
+        reward_words=find(bdf.words(:,2)==32);
+        bdfMeanHold=mean(bdf.words(reward_words,1)-bdf.words(reward_words-1,1));
         if bdfMeanHold > hold_time
-            continue
+            if datenum(bdf.meta.datetime) >= datenum('09-14-2011')
+                fprintf(1,'Mean hold time was %f. skipping...\n',bdfMeanHold)
+                continue
+            else
+                fprintf(1,'Mean hold time was %f in a file before 09-14-2011.\n',bdfMeanHold)
+                fprintf(1,'Hold time could not be assessed\n')
+            end
         end
         % order
-        mean(bdf.vel(:)) >= 10
-        % determine hand control by looking at bdf.pos and bdf.vel, both
-        % range of values and size of arrays
-        
+        if (mean(range(bdf.vel(:,2:3))) > 10) && (size(bdf.pos,1)==size(bdf.acc,1))
+            % hand control file
+        else
+            % unmodified brain control file (if the sizes are even but it fails
+            % the ranges test), modified brain control file (if it passes the
+            % range test but the sizes are uneven)
+            
+        end        
     end
 end
 
