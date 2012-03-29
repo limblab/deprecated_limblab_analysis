76f4a35ea63503e21589d6d20f5651b1efc17acf
Paul Wanda <paulw@gmail.com>
2012-03-29 22:06:18 +0000

    git-svn-id: http://citadel.physiology.northwestern.edu/svn/s1_analysis@765 984b1e3c-1c57-0410-ae62-c18f90d3b645

---
+++ proc/paul/getBDF.m
@@ -0,0 +1,26 @@
+function out_struct = getBDF(filename)
+
+% GETBDF Grabs and saves a BDF struct from a .nev file
+%   OUT_STRUCT = GETBDF(FILENAME) returns a BDF populated by the
+%   string FILENAME.  Saves it locally as a .mat file.
+% 
+
+if(~strcmp(filename(end-3:end),'.nev'))
+    error('ERROR: File must be .nev extension');
+    return;
+end
+
+% CHANGE THIS TO YOUR LOCAL ANALYSIS FOLDER (i.e. where load_paths is
+% located)
+cd C:\Users\limblab\Desktop\s1_analysis\;
+load_paths;
+
+% CHANGE THE PATH HERE TO WHERE YOUR FILES ARE LOCALLY STORED
+bdf = get_cerebus_data(['C:\Users\limblab\Documents\MATLAB\Paul_Uncertainty\jaco_dat\' filename],3);
+
+% CHANGE THIS PATH TO YOUR LOCAL ANALYSIS SUBDIR
+cd C:\Users\limblab\Desktop\s1_analysis\proc\Paul;
+fn =[filename(1:end-4) '.mat'];
+save(fn,'bdf');
+out_struct = bdf;
+return;
