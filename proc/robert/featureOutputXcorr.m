679ac1fdbf508ac93cfbdad0c523db89b27004b8
Robert Flint <r-flint@northwestern.edu>
2012-04-17 20:54:55 +0000

    git-svn-id: http://citadel.physiology.northwestern.edu/svn/s1_analysis@807 984b1e3c-1c57-0410-ae62-c18f90d3b645

--- proc/robert/featureOutputXcorr.m
+++ proc/robert/featureOutputXcorr.m
@@ -1,31 +1,10 @@
-function [XCx,XCy,timelags,peakInd_x,peakInd_y]=featureOutputXcorr(nameIn,numlags)
+function [XCx,XCy,timelags,peakInd_x,peakInd_y]=...
+    featureOutputXcorr(out_struct,numlags,featMat,sigTrimmed,numfp)
 
-% syntax [XCx,XCy,timelags,peakInd_x,peakInd_y]=featureOutputXcorr(nameIn,numlags)
+% syntax [XCx,XCy,timelags,peakInd_x,peakInd_y]= ...
+%           featureOutputXcorr(out_struct,numlags,featMat,sigTrimmed)
 %
 % 
-
-
-
-[BDFpathIn,BDFnameIn,ext,~]=FileParts(nameIn);
-
-if isempty(BDFpathIn)
-    BDFfullPath=findBDFonCitadel([BDFnameIn,ext]);
-else
-    BDFfullPath=nameIn;
-end
-load(BDFfullPath)
-fpAssignScript
-wsz=256;
-binsize=0.05;
-numfp=size(fp,1);
-
-% behavior
-signal='vel';
-sig=out_struct.(signal);
-analog_times=sig(:,1);
-
-% calculate features of the fps as they happened during this brain-control file.
-[featMat,sigTrimmed,~]=makefmatc_causal(fp,fptimes,numfp,binsize,samprate,analog_times,wsz,sig);
 
 % find the decoder that was used, pull from it bestc,bestf.  Use these to
 % index above-calculated feature matrix.  NO SORTING OF featMat, since
@@ -43,7 +22,7 @@ FMindexed=featMat(:,featind);
 for k=1:size(FMindexed,2)
     if k==1
         [XCx(:,k),lags]=xcorr(FMindexed(:,k),sigTrimmed(:,2),numlags);
-        [XCy(:,k),lags]=xcorr(FMindexed(:,k),sigTrimmed(:,3),numlags);
+        [XCy(:,k)]=xcorr(FMindexed(:,k),sigTrimmed(:,3),numlags);
         timelags=-1*length(lags)*0.05/2:0.05:(length(lags)*0.05/2-0.05);
     else
         [XCx(:,k)]=xcorr(FMindexed(:,k),sigTrimmed(:,2),numlags);
---
+++ proc/robert/run_makefmatc_causal.m
@@ -0,0 +1,42 @@
+function run_makefmatc_causal(nameIn,numlags)
+
+% syntax run_makefmatc_causal(nameIn,numlags)
+%
+% numlags is optional, if want to automatically run featureOutputXcorr.m
+% inline.
+
+
+
+[BDFpathIn,BDFnameIn,ext,~]=FileParts(nameIn);
+
+if isempty(BDFpathIn)
+    BDFfullPath=findBDFonCitadel([BDFnameIn,ext]);
+else
+    BDFfullPath=nameIn;
+end
+load(BDFfullPath)
+fpAssignScript
+wsz=256;
+binsize=0.05;
+numfp=size(fp,1);
+
+% behavior
+signal='vel';
+sig=out_struct.(signal);
+analog_times=sig(:,1);
+
+% calculate features of the fps as they happened during this brain-control file.
+[featMat,sigTrimmed,~]=makefmatc_causal(fp,fptimes,numfp,binsize,samprate,analog_times,wsz,sig);
+
+if nargin > 1
+    [~,~,timelags,peakInd_x,peakInd_y]=featureOutputXcorr(out_struct,numlags,featMat,sigTrimmed,numfp);
+    assignin('base','out_struct',out_struct)
+    assignin('base','featMat',featMat)
+    assignin('base','sigTrimmed',sigTrimmed)
+    assignin('base','timelags',timelags)
+    assignin('base','peakInd_x',peakInd_x)
+    assignin('base','peakInd_y',peakInd_y)
+end
+ 
+ 
+ 
