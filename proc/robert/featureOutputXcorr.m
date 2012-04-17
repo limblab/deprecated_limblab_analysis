b0c304fbcdda3e4836bb7fcad9e9f23340fd35b5
Robert Flint <r-flint@northwestern.edu>
2012-04-17 20:29:26 +0000

    git-svn-id: http://citadel.physiology.northwestern.edu/svn/s1_analysis@806 984b1e3c-1c57-0410-ae62-c18f90d3b645

--- proc/robert/featureOutputXcorr.m
+++ proc/robert/featureOutputXcorr.m
@@ -1,4 +1,10 @@
-function [XCx,XCy]=featureOutputXcorr(nameIn)
+function [XCx,XCy,timelags,peakInd_x,peakInd_y]=featureOutputXcorr(nameIn,numlags)
+
+% syntax [XCx,XCy,timelags,peakInd_x,peakInd_y]=featureOutputXcorr(nameIn,numlags)
+%
+% 
+
+
 
 [BDFpathIn,BDFnameIn,ext,~]=FileParts(nameIn);
 
@@ -19,42 +25,34 @@ sig=out_struct.(signal);
 analog_times=sig(:,1);
 
 % calculate features of the fps as they happened during this brain-control file.
-[featMat,y,t]=makefmatc_causal(fp,fptimes,numfp,binsize,samprate,analog_times,wsz,sig);
+[featMat,sigTrimmed,~]=makefmatc_causal(fp,fptimes,numfp,binsize,samprate,analog_times,wsz,sig);
 
 % find the decoder that was used, pull from it bestc,bestf.  Use these to
-% index above-calculated feature matrix.
+% index above-calculated feature matrix.  NO SORTING OF featMat, since
+% makefmatc_causal doens't re-do the feature selection, featMat is still in
+% the order of channels.
 pathToDecoderMAT=decoderPathFromBDF(out_struct);
-load(pathToDecoderMAT,'bestf','bestc')
+load(pathToDecoderMAT,'bestc','bestf')
 
-[~,featind]=sortrows([bestc', bestf']);
+bestfeats=sortrows([bestc' bestf']);
+featind=sub2ind([6,numfp],bestfeats(:,2),bestfeats(:,1));
 FMindexed=featMat(:,featind);
 
 % from the set of selected features, calculate the cross-correlation with
 % outputs.  
-for k=1:150
-    [XCx(:,k),lags]=xcorr(FMindexed(:,k),sig(:,2),500);
-    [XCy(:,k),lags]=xcorr(FMindexed(:,k),sig(:,3),500);
+for k=1:size(FMindexed,2)
+    if k==1
+        [XCx(:,k),lags]=xcorr(FMindexed(:,k),sigTrimmed(:,2),numlags);
+        [XCy(:,k),lags]=xcorr(FMindexed(:,k),sigTrimmed(:,3),numlags);
+        timelags=-1*length(lags)*0.05/2:0.05:(length(lags)*0.05/2-0.05);
+    else
+        [XCx(:,k)]=xcorr(FMindexed(:,k),sigTrimmed(:,2),numlags);
+        [XCy(:,k)]=xcorr(FMindexed(:,k),sigTrimmed(:,3),numlags);
+    end
+    [~,indx]=max(XCx(:,k));
+    peakInd_x(k)=timelags(indx);
+    [~,indy]=max(XCy(:,k));
+    peakInd_y(k)=timelags(indy);
 end
 XCx=double(XCx);
 XCy=double(XCy);
-
-
-
-
-
-
-
-
-return
-
-
-
-% for n=1:10
-%     for k=1:150        
-%         [XCx(:,k,n),lags]=xcorr(xtnew{n}(:,k),ytnew{n}(:,1),75);
-%         [XCy(:,k,n),lags]=xcorr(xtnew{n}(:,k),ytnew{n}(:,2),75);
-%     end
-% end
-% XCx=double(XCx);
-% XCy=double(XCy);
-% clear n k
