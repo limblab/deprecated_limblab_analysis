6115ee93d5df433b1b4f63f6e9e812ff6df6c0f5
Robert Flint <r-flint@northwestern.edu>
2012-05-22 00:44:40 +0000

    git-svn-id: http://citadel.physiology.northwestern.edu/svn/s1_analysis@850 984b1e3c-1c57-0410-ae62-c18f90d3b645

--- proc/robert/batch_Xcorr_featuresToOutputs.m
+++ proc/robert/batch_Xcorr_featuresToOutputs.m
@@ -11,7 +11,7 @@ days={...
     '03-30-2012', ...
     };
 
-days={...
+Chewie_days={...
     '09-02-2011', ...
     '09-06-2011', ...
     '09-07-2011', ...
@@ -48,22 +48,69 @@ days={...
     '03-30-2012', ...
     };
 
-peakIndAll_x=[]; peakIndAll_y=[]; peakValAll_x=[]; peakValAll_y=[];
+peakIndAll_x_Chewie=[]; peakIndAll_y_Chewie=[];
+peakValAll_x_Chewie=[]; peakValAll_y_Chewie=[];
 BDFlist_all=[];
 originalDir=pwd;
 if isempty(regexp(pwd,'featMats','once')), mkdir('featMats'), end
-for n=1:length(days)
+for n=1:length(Chewie_days)
     % take a day, find the kinStruct, and identifies all the
     % files of the given control type that were included.
-    BDFlist=findBDF_withControl('Chewie',days{n},'hand');
+    BDFlist=findBDF_withControl('Chewie',Chewie_days{n},'LFP');
     BDFlist_all=[BDFlist_all; BDFlist'];
     for k=1:length(BDFlist)
         if isempty(regexp(pwd,'featMats','once')), cd('featMats'), end
-        run_makefmatc_causal(BDFlist{k},500)
-        peakIndAll_x=[peakIndAll_x; evalin('base','peakInd_x')];
-        peakIndAll_y=[peakIndAll_y; evalin('base','peakInd_y')];
-        peakValAll_x=[peakValAll_x; evalin('base','peakVal_x')];
-        peakValAll_y=[peakValAll_y; evalin('base','peakVal_y')];
+%         if ~exist(BDFlist{k},'file')~=2
+            run_makefmatc_causal(BDFlist{k},500)
+            peakIndAll_x_Chewie=[peakIndAll_x_Chewie; evalin('base','peakInd_x')];
+            peakIndAll_y_Chewie=[peakIndAll_y_Chewie; evalin('base','peakInd_y')];
+            peakValAll_x_Chewie=[peakValAll_x_Chewie; evalin('base','peakVal_x')];
+            peakValAll_y_Chewie=[peakValAll_y_Chewie; evalin('base','peakVal_y')];
+            clear peakInd_* peakVal_*
+%         else
+%             % not going to work in current incarnation
+%             load(BDFlist{k},'featMat','sigTrimmed','out_struct')
+%             [~,~,timelags,peakInd_x,peakInd_y,peakVal_x,peakVal_y]= ...
+%                 featureOutputXcorr(out_struct,numlags,featMat,sigTrimmed,numfp);
+%         end
     end
 end
+
+% save('C:\Documents and Settings\Administrator\Desktop\Mike_Data\Spike LFP Decoding\Chewie','peak*')
 cd(originalDir)
+
+peakIndAll_x_Mini=[]; peakIndAll_y_Mini=[];
+peakValAll_x_Mini=[]; peakValAll_y_Mini=[];
+% days for Mini
+Mini_days={...
+    '09-02-2011', ...
+    '09-07-2011', ...
+    '09-08-2011', ...
+    '09-09-2011', ...
+    '09-12-2011', ...
+    '03-19-2012', ...
+    '03-21-2012', ...
+    '03-28-2012', ...
+    '03-30-2012', ...
+    '04-02-2012', ...
+    };
+% for n=1:length(Mini_days)
+%     % take a day, find the kinStruct, and identifies all the
+%     % files of the given control type that were included.
+%     BDFlist=findBDF_withControl('Mini',Mini_days{n},'LFP');
+%     BDFlist_all=[BDFlist_all; BDFlist'];
+%     for k=1:length(BDFlist)
+%         if isempty(regexp(pwd,'featMats','once')), cd('featMats'), end
+%         run_makefmatc_causal(BDFlist{k},500)
+%         peakIndAll_x_Mini=[peakIndAll_x_Mini; evalin('base','peakInd_x')];
+%         peakIndAll_y_Mini=[peakIndAll_y_Mini; evalin('base','peakInd_y')];
+%         peakValAll_x_Mini=[peakValAll_x_Mini; evalin('base','peakVal_x')];
+%         peakValAll_y_Mini=[peakValAll_y_Mini; evalin('base','peakVal_y')];
+%     end
+% end
+% cd(originalDir)
+
+% save('Y:\user_folders\Robert\data\monkey\outputs\LFPcontrol\Xcorr stuff\XCcorr_batch_peakVal_All.mat', ...
+%     'peakIndAll_x_Chewie','peakIndAll_y_Chewie','peakValAll_x_Chewie', ...
+%     'peakValAll_y_Chewie','peakIndAll_x_Mini','peakIndAll_y_Mini', ...
+%     'peakValAll_x_Mini','peakValAll_y_Mini')
