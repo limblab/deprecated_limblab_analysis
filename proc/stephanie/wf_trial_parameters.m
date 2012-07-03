5aa10bd7ccf4734a87ce4ce6b23a6ebd4c7c26d7
Stephanie Naufel <steph.naufel@gmail.com>
2012-07-03 20:36:35 +0000

    git-svn-id: http://citadel.physiology.northwestern.edu/svn/s1_analysis@880 984b1e3c-1c57-0410-ae62-c18f90d3b645

--- proc/stephanie/wf_trial_parameters.m
+++ proc/stephanie/wf_trial_parameters.m
@@ -4,4 +4,23 @@ function tp = wf_trial_parameters(bdf)
 % Gonna need to put this function in convertBDF2binned
 
 
+% Make this a struct
+% Epochs
+%   Center hold time
+%   Target hold time
+%   Delay time
+%   Reach time
+
+% TargetInfo
+% Target No  % x  % y  %h  %w
+
+
+epochs = {'Center hold time', 'Target hold time', 'Delay'; 1, 2, 3};
+     
+     
+%trial Parameters is a struct
+trialParameters = struct('epochs',epochs,...
+                         'targetInfo',targetinfo);
+
+
 end
