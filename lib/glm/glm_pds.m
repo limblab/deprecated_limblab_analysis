b94550ce069a25ecd55b38d602e06537b1e3b488
Stephan <stephan@mycompany.com>
2012-10-07 19:30:40 +0000

    added functionality: a second input to glm_pds which is >0 results in unsorted neurons also being used for preferred direction calculation
    git-svn-id: http://citadel.physiology.northwestern.edu/svn/s1_analysis@928 984b1e3c-1c57-0410-ae62-c18f90d3b645

--- lib/glm/glm_pds.m
+++ lib/glm/glm_pds.m
@@ -1,11 +1,19 @@
-function [pds, errs, moddepth] = glm_pds(bdf)
+function [pds, errs, moddepth] = glm_pds(varargin)
 % GLM_PDS returns PDS calculated using a velocity GLM
-%   PDS = GLM_PDS(BDF) returns the velocity PDs from each unit in the
-%       supplied BDF object
+%   PDS = GLM_PDS(BDF) returns the velocity PDs, in radians, from each unit in the
+%       supplied BDF object 
+%   PDS = GLM_PDS(BDF,include_unsorted) - Same as above, second input includes
+%       unsorted units in unit list if different from zero.
 
 % $Id$
 
-ul = unit_list(bdf);
+bdf = varargin{1};
+include_unsorted = 0;
+if length(varargin)>1;
+    include_unsorted = varargin{2};
+end
+
+ul = unit_list(bdf,include_unsorted);
 pds = zeros(length(ul),1);
 errs = zeros(length(ul),1);
 moddepth = zeros(length(ul),1);
@@ -16,13 +24,13 @@ for i = 1:length(ul)
     fprintf(1, 'ET: %f (%d of %d)\n', et, i, length(ul));
     
     [b, dev, stats] = glm_kin(bdf, ul(i,1), ul(i,2), 0, 'posvel'); %#ok<ASGLU>
-    bv = [b(4) b(5)];
+    bv = [b(4) b(5)]; % glm weights on x and y velocity
     dbv = [stats.se(4) stats.se(5)];
     J = [-bv(2)/(bv(1)^2+bv(2)^2); bv(1)/(bv(1)^2+bv(2)^2)];
     moddepth(i) = norm(bv,2);
     pds(i,:) = atan2(bv(2), bv(1));
-    errs(i,:) = dbv*J; % Error on either side of the mean, to get confidence interval multiply by 1.96.
-    moddepth(i,:) = sqrt(bv(1).^2 + bv(2).^2);
+    errs(i,:) = dbv*J;
+    moddepth(i,:) = sqrt(bv(1).^2 + bv(2).^2); 
 end
 
 
