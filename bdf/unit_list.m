6029469e0b53ad4a61dde653cc121b2d790defa2
Stephan <stephan@mycompany.com>
2012-10-07 19:31:45 +0000

    added functionality: a second input to unit_list which is >0 results in unsorted neurons listed as zeros in the second column
    git-svn-id: http://citadel.physiology.northwestern.edu/svn/s1_analysis@929 984b1e3c-1c57-0410-ae62-c18f90d3b645

--- bdf/unit_list.m
+++ bdf/unit_list.m
@@ -1,11 +1,18 @@
-function list = unit_list(data)
+function list = unit_list(varargin)
 % UNIT_LIST(DATA) - gives a list of the unit ids contained in the datafile
 %   LIST = UNIT_LIST(DATA) - returns LIST, a two column matrix containing
 %   the channel number (column 1) and sort code (column 2) of every unit
 %   that has >0 spikes in BDF structure DATA.
+% UNIT_LIST(DATA,include_unsorted) - Same as above, second input includes
+%   unsorted units in unit list if different from zero.
 
 % $Id$
 
+data = varargin{1};
+include_unsorted = 0;
+if length(varargin)>1
+    include_unsorted = varargin{2};
+end
 if regexp(data.meta.filename, 'FAKE SPIKES')
     warning('BDF:fakeData', 'Using BDF with fake spike data');
 end
@@ -20,4 +27,6 @@ for i = 1:L
 end
 
 list = list(list(:,2)~=255,:);
-list = list(list(:,2)~=0,:);
+if ~include_unsorted
+    list = list(list(:,2)~=0,:);
+end
