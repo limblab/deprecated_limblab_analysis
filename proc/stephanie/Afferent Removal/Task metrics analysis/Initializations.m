1c3cb2bc56374f0512465f5a89883cb73ab1a282
Stephanie Naufel <steph.naufel@gmail.com>
2012-10-12 22:21:44 +0000

    git-svn-id: http://citadel.physiology.northwestern.edu/svn/s1_analysis@954 984b1e3c-1c57-0410-ae62-c18f90d3b645

--- proc/stephanie/Afferent Removal/Task metrics analysis/Initializations.m
+++ proc/stephanie/Afferent Removal/Task metrics analysis/Initializations.m
@@ -7,6 +7,8 @@ trialtable = wf_trial_table(out_struct);
 timeframe = out_struct.pos(:,1);
 Goodtrialtable = trialtable(trialtable(:,9)==82,:);
 
+
+% Get the coordinates for the center of the outer target for each trial
 for N = 1:length(Goodtrialtable)
         xCenter(N,1) = (Goodtrialtable(N,4)+Goodtrialtable(N,2))/2;
         yCenter(N,1) = (Goodtrialtable(N,5)+Goodtrialtable(N,3))/2;
