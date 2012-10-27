eadb8fb705358fd0bd56295ab8618a6dfadb74ee
Tucker Tomlinson <tuckertomlinson@gmail.com>
2012-10-27 18:31:01 +0000

    git-svn-id: http://citadel.physiology.northwestern.edu/svn/s1_analysis@982 984b1e3c-1c57-0410-ae62-c18f90d3b645

--- proc/Tucker/autoconvert_nev_to_bdf.m
+++ proc/Tucker/autoconvert_nev_to_bdf.m
@@ -3,24 +3,22 @@
 
 %
 %set the mount drive to scan and convert
-folderpath='C:\Documents and Settings\Administrator\Desktop\Tucker\trial_data_set\';
-%addpath(folderpath);
+folderpath='Z:\Kramer_10I1\BumpDirection\Data Raw\Post-implant\';
+date='10262012';
 foldercontents=dir(folderpath);
 fnames={foldercontents.name};%extracts just the names from the foldercontents
 for i=1:length(foldercontents)
     if (length(fnames{i})>3)
-        if strcmp(fnames{i}((length(fnames{i})-3):end),'.nev')
+        if (strcmp(fnames{i}((length(fnames{i})-3):end),'.nev') & ~isempty( strfind(fnames{i},date)))
+            
             if isempty(strmatch( strcat( fnames{i}(1:(length(fnames{i})-3)), 'mat'),fnames))
                 %if we haven't found a .mat file to match the .nev then make
                 %one
 
                 disp(strcat('Working on: ',folderpath, fnames{i}))
 
-                bdf=get_cerebus_data(strcat(folderpath, fnames{i}),'verbose','noforce','nokin','noeye');
-
-                [bdf.tt, bdf.tt_hdr]= bc_trial_table3(bdf);
-                savename=strcat(folderpath, fnames{i}(1:(length(fnames{i})-4)), '_tt_only.mat');
-                save(savename,  'bdf');
+                bdf=get_cerebus_data(strcat(folderpath, fnames{i}),'verbose','noeye');
+                save( strcat(folderpath, fnames{i}(1:(length(fnames{i})-3)), 'mat'), 'bdf')
                 clear bdf
             end
         end
