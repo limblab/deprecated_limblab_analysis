09e7a47c27d4997e5cf12aa826c527597e19294f
Robert Flint <r-flint@northwestern.edu>
2012-11-08 22:11:37 +0000

    git-svn-id: http://citadel.physiology.northwestern.edu/svn/s1_analysis@991 984b1e3c-1c57-0410-ae62-c18f90d3b645

--- proc/robert/buildModel_fp.m
+++ proc/robert/buildModel_fp.m
@@ -282,6 +282,8 @@ end
 r1=reshape(r,1,[]);
 r1(isnan(r1))=0;    %If any NaNs, set them to 0 to not mess up the sorting
 [sr,featind]=sort(r1,'descend');
+assignin('base','sr',sr)
+assignin('base','R',r1)
 [bestf,bestc]=ind2sub(size(r),featind((1:nfeat)+0));
 bestPB=single(zeros(nfeat,length(y)));
 clear r     %clear this so we can reuse r later on
