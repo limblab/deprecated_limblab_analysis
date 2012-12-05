c88464df1109674b69e288aec2bd9947f6f943d8
Tucker Tomlinson <tuckertomlinson@gmail.com>
2012-12-05 18:26:49 +0000

    updated handling of glm models other than posvel.
    git-svn-id: http://citadel.physiology.northwestern.edu/svn/s1_analysis@1028 984b1e3c-1c57-0410-ae62-c18f90d3b645

--- lib/glm/glm_pds.m
+++ lib/glm/glm_pds.m
@@ -9,8 +9,15 @@ function [pds, errs, moddepth] = glm_pds(varargin)
 
 bdf = varargin{1};
 include_unsorted = 0;
+model='posvel';
 if length(varargin)>1;
-    include_unsorted = varargin{2};
+    for i=2:length(varargin)
+        if ischar(varargin{i})
+            model=varargin{i};
+        else
+            include_unsorted = varargin{i};
+        end
+    end
 end
 
 ul = unit_list(bdf,include_unsorted);
@@ -23,9 +30,35 @@ for i = 1:length(ul)
     et = toc;
     fprintf(1, 'ET: %f (%d of %d)\n', et, i, length(ul));
     
-    [b, dev, stats] = glm_kin(bdf, ul(i,1), ul(i,2), 0, 'posvel'); %#ok<ASGLU> 
-    bv = [b(4) b(5)]; % glm weights on x and y velocity
-    dbv = [stats.se(4) stats.se(5)];
+    [b, dev, stats] = glm_kin(bdf, ul(i,1), ul(i,2), 0, model); %#ok<ASGLU> 
+    switch model
+        case 'posvel'
+            bv = [b(4) b(5)]; % glm weights on x and y velocity
+            dbv = [stats.se(4) stats.se(5)];
+        case 'forceonly'
+            bv = [b(2) b(3)]; % glm weights on x and y force
+            dbv = [stats.se(2) stats.se(3)];
+        case 'pos'
+            error('s1_analysis:lib:glm:glm_pds:UnmappedPDCase',strcat('No output case defined for model type: ',model))
+        case 'vel'
+            error('s1_analysis:lib:glm:glm_pds:UnmappedPDCase',strcat('No output case defined for model type: ',model))
+        case 'nospeed'
+            error('s1_analysis:lib:glm:glm_pds:UnmappedPDCase',strcat('No output case defined for model type: ',model))
+        case 'forcevel'
+            bv = [b(4) b(5)]; % glm weights on x and y velocity
+            dbv = [stats.se(4) stats.se(5)];
+        case 'forceposvel'
+            bv = [b(6) b(7)]; % glm weights on x and y velocity
+            dbv = [stats.se(6) stats.se(7)];
+        case 'ppforcevel'
+            error('s1_analysis:lib:glm:glm_pds:UnmappedPDCase',strcat('No output case defined for model type: ',model))
+        case 'ppforceposvel'
+            error('s1_analysis:lib:glm:glm_pds:UnmappedPDCase',strcat('No output case defined for model type: ',model))
+        case 'powervel'
+            error('s1_analysis:lib:glm:glm_pds:UnmappedPDCase',strcat('No output case defined for model type: ',model))
+        case 'ppcartfvp'
+            error('s1_analysis:lib:glm:glm_pds:UnmappedPDCase',strcat('No output case defined for model type: ',model))
+    end
     J = [-bv(2)/(bv(1)^2+bv(2)^2); bv(1)/(bv(1)^2+bv(2)^2)];
     moddepth(i) = norm(bv,2);
     pds(i,:) = atan2(bv(2), bv(1));
