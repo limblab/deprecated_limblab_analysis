31dd6121de164ca9e1b8dcf581f0247723b6ee11
Mike Scheid <michaelscheid2015@northwestern.edu>
2012-12-06 00:23:04 +0000

    git-svn-id: http://citadel.physiology.northwestern.edu/svn/s1_analysis@1033 984b1e3c-1c57-0410-ae62-c18f90d3b645

--- proc/mike/Spike LFP Code/MRSstartup.m
+++ proc/mike/Spike LFP Code/MRSstartup.m
@@ -1,6 +1,6 @@
 addpath(genpath('\\Citadel\limblab\user_folders\Marc\matlab code'))
-addpath(genpath('C:\Documents and Settings\Administrator\Desktop\s1_analysis'))
 addpath(genpath('C:\Documents and Settings\Administrator\Desktop\RobertF\my code_mods'))
+addpath(genpath('C:\Documents and Settings\Administrator\My Documents\s1_analysis'))
 cd('C:\Documents and Settings\Administrator\Desktop\Mike_Data\Spike LFP Decoding')
 dbstop if error
 clc;
---
+++ proc/mike/Spike LFP Code/removefreqband.m
@@ -0,0 +1,20 @@
+function Hnew = removefreqband(bandNum, H, featind, bestc, bestf)
+
+FreqBand_Index = [];
+
+for i = 1:length(Bandnum)
+    FreqBand_Index = [FreqBand_Index bandNum:6:576];
+end
+
+FreqBand_Index = FreqBand_Index';
+
+featind_bychan = sortrows([featind' bestc' bestf'],[2 3]);
+[c, ic, id] = intersect(featind_bychan(:,1), Delta_Mu_Gamma_Index);
+Hnew = H;
+
+for i = 1:length(ic)
+    Hnew((ic(i)-1)*10+1:ic(i)*10,:) = 0;
+end
+
+
+end
