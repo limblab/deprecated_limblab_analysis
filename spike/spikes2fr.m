a40852f28f2b709dfe90978c8b159e18580cd3f1
Ricardo Torres <ricardort@gmail.com>
2013-01-15 17:57:41 +0000

    Replaced for-loop with convolution, much faster.
    git-svn-id: http://citadel.physiology.northwestern.edu/svn/s1_analysis@1045 984b1e3c-1c57-0410-ae62-c18f90d3b645

--- spike/spikes2fr.m
+++ spike/spikes2fr.m
@@ -9,8 +9,20 @@ function fr = spikes2fr( s, t, k )
 % k = 0.05;     % pick a smoothing parameter (in this case 50 ms)
 
 % s = sort(randn(100,1)) + 2.5; % asign some random spike times 
-fr = zeros(size(t));
-for tau = s'
-    fr = fr + (t-tau > 0) .* (t-tau) .* exp((tau-t)/k) / k^2;
-end
 
+% fr = zeros(size(t));
+% for tau = s'
+%     fr = fr + (t-tau > 0) .* (t-tau) .* exp((tau-t)/k) / k^2;
+% end
+
+t_kernel = 0:mean(diff(t)):20*k;
+kernel = (t_kernel > 0) .* (t_kernel).*exp((-t_kernel)/k)/k^2;
+
+s = round(s*1000)/1000;
+[~,~,idx] = intersect(s,t);
+
+spike_train = zeros(size(t));
+spike_train(idx) = 1;
+
+fr = conv(spike_train,kernel,'full'); 
+fr = fr(1:length(t));
