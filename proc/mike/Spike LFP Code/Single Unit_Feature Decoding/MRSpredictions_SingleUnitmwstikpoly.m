0a34f8181f99a19da6391a14404fef1dc04e7918
Mike Scheid <michaelscheid2015@northwestern.edu>
2013-01-24 01:29:26 +0000

    Changed temporarily to just extract NeuronIDs
    git-svn-id: http://citadel.physiology.northwestern.edu/svn/s1_analysis@1067 984b1e3c-1c57-0410-ae62-c18f90d3b645

--- proc/mike/Spike LFP Code/Single Unit_Feature Decoding/MRSpredictions_SingleUnitmwstikpoly.m
+++ proc/mike/Spike LFP Code/Single Unit_Feature Decoding/MRSpredictions_SingleUnitmwstikpoly.m
@@ -1,4 +1,5 @@
-function [vaf, vmean,vsd,y_test,y_pred,varargout] = predictions_mwstikpoly(bdf, signal, cells, binsize, folds,numlags,numsides,lambda,PolynomialOrder,Use_Thresh,varargin)
+function [cells] = MRSpredictions_SingleUnitmwstikpoly(bdf, signal, cells, binsize, folds,numlags,numsides,lambda,PolynomialOrder,Use_Thresh,varargin)
+%[vaf, vmean,vsd,y_test,y_pred,varargout]
 
 % $Id: predictions_mwstikpoly.m 385 2011-02-20 19:05:39Z marc $
 
@@ -167,274 +168,279 @@ else
     cells = unit_list(bdf);
 end
 
-binsamprate=floor(1/binsize);
+
+
+% binsamprate=floor(1/binsize);
 % Using binsize ms bins
-
-if strcmpi(signal,'vel') ||  strcmpi(signal, 'pos') ||  strcmpi(signal, 'acc')
-    t = bdf.vel(1,1):binsize:bdf.vel(end,1);
-end
-if strcmpi(signal,'emg')
-    %     if isfield(bdf.emg,'ts')
-    %     t = bdf.emg.ts(1):binsize:bdf.emg.ts(end);
-    t=temg(1):binsize:temg(end);
-    if t(1)<temg(1)
-        t(1)=temg(1);   %Do this to avoid NaNs when interpolating
-    end
-    y=interp1(temg',y,t);   %if y is a n by 4 matrix then interp will work
-else
-    y = [interp1(bdf.vel(:,1), y(:,1), t); interp1(bdf.vel(:,1), y(:,2), t)]';
-end
-
-x = zeros(length(y), length(cells));
-for i = 1:length(cells)
-    if cells(i,1) ~= 0
-        ts = get_unit(bdf, cells(i, 1), cells(i, 2));
-        b = train2bins(ts, t);
-        x(:,i) = b;
-    else
-        x(:,i) = zeros(length(y),1);
-    end
-end
-
+% 
+% if strcmpi(signal,'vel') ||  strcmpi(signal, 'pos') ||  strcmpi(signal, 'acc')
+%     t = bdf.vel(1,1):binsize:bdf.vel(end,1);
+% end
+% if strcmpi(signal,'emg')
+%         if isfield(bdf.emg,'ts')
+%         t = bdf.emg.ts(1):binsize:bdf.emg.ts(end);
+%     t=temg(1):binsize:temg(end);
+%     if t(1)<temg(1)
+%         t(1)=temg(1);   %Do this to avoid NaNs when interpolating
+%     end
+%     y=interp1(temg',y,t);   %if y is a n by 4 matrix then interp will work
+% else
+%     y = [interp1(bdf.vel(:,1), y(:,1), t); interp1(bdf.vel(:,1), y(:,2), t)]';
+% end
+% 
+% x = zeros(length(y), length(cells));
+% for i = 1:length(cells)
+%     if cells(i,1) ~= 0
+%         ts = get_unit(bdf, cells(i, 1), cells(i, 2));
+%         b = train2bins(ts, t);
+%         x(:,i) = b;
+%     else
+%         x(:,i) = zeros(length(y),1);
+%     end
+% end
+% 
 % filter out inactive regions
 % Find active regions
-
-if ~strcmpi(signal,'emg')
-    q = find_active_regions_words(bdf.words,bdf.vel(:,1));
-    q = interp1(bdf.vel(:,1), q, t);
-else
-    q=ones(size(t))';
-end
-
-x = x(q==1,:);
-y = y(q==1,:);
-t = t(:,q==1)';
-
-vaf = zeros(folds,size(y,2));
-r2 = zeros(folds,size(y,2));
-fold_length = floor(length(y) ./ folds);
-
-t_final = zeros(length(t)-9*folds,1);
-
-for i = 1:size(x,2)
-    
-    x_train = x(:,i);
-    y_train = y;
-    x_test = x(:,i);
-    y_test = y;
-    t_fold = t;
-    
-    %% subtract off the mean to reduce offset
-    %     y_train = y_train - repmat(mean(y_train),size(y_train,1),1);
-    %     y_test{i}= y_test{i} - repmat(mean(y_test{i}),size(y_test{i},1),1);
-    %     x_test{i} = x_test{i} - repmat(mean(x_test{i}),size(x_test{i},1),1);
-    %     x_train = x_train - repmat(mean(x_train),size(x_train,1),1);
-    %
-    if ~exist('H','var')
-        [H{i},v,mcc] = FILMIMO3_tik(x_train, y_train, numlags, numsides,lambda,binsamprate);
-        
-        [y_pred{i},xtnew{i},ytnew{i}] = predMIMO3(x_test,H{i},numsides,binsamprate,y_test);
-        
-    elseif exist('H','var') && length(H) < i
-        
-        [H{i},v,mcc] = FILMIMO3_tik(x_train, y_train, numlags, numsides,lambda,binsamprate);
-        
-        [y_pred{i},xtnew{i},ytnew{i}] = predMIMO3(x_test,H{i},numsides,binsamprate,y_test);
-        
-    else
-        
-        [y_pred{i},xtnew{i},ytnew{i}] = predMIMO3(x_test,H{i},numsides,binsamprate,y_test);
-        
-    end
-    %%Polynomial section
-    
-    T=[];
-    patch = [];
-    
-    if  PolynomialOrder
-        %%%Find a Wiener Cascade Nonlinearity
-        for z=1:size(y_pred{i},2)
-            if Use_Thresh
-                %Find Threshold
-                T_default = 1.25*std(y_pred{i}(:,z));
-                [T(z,1), T(z,2), patch(z)] = findThresh(ytnew{i}(:,z),y_pred{i}(:,z),T_default);
-                IncludedDataPoints = or(y_pred{i}(:,z)>=T(z,2),y_pred{i}(:,z)<=T(z,1));
-                
-                %Apply Threshold to linear predictions and Actual Data
-                PredictedData_Thresh = y_pred{i}(IncludedDataPoints,z);
-                ActualData_Thresh = ytnew{i}(IncludedDataPoints,z);
-                
-                %Replace thresholded data with patches consisting of 1/3 of the data to find the polynomial
-                Pred_patches = [ (patch(z)+(T(z,2)-T(z,1))/4)*ones(1,round(length(nonzeros(IncludedDataPoints))*4)) ...
-                    (patch(z)-(T(z,2)-T(z,1))/4)*ones(1,round(length(nonzeros(IncludedDataPoints))*4)) ];
-                Act_patches = mean(ytnew{i}(~IncludedDataPoints,z)) * ones(1,length(Pred_patches));
-                
-                %Find Polynomial to Thresholded Data
-                if exist('P','var')
-                    [P(z,:)] = WienerNonlinearity([PredictedData_Thresh; Pred_patches'], [ActualData_Thresh; Act_patches'], PolynomialOrder,'plot');
-                end
-                
-                %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-                %%%%%% Use only one of the following 2 lines:
-                %
-                %   1-Use the threshold only to find polynomial, but not in the model data
-                T=[]; patch=[];
-                %
-                %   2-Use the threshold both for the polynomial and to replace low predictions by the predefined value
-                %                 y_pred{i}(~IncludedDataPoints,z)= patch(z);
-                %
-                %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-            elseif ~exist('P','var')
-                %Find and apply polynomial
-                [P(z,:,i)] = WienerNonlinearity(y_pred{i}(:,z), ytnew{i}(:,z), PolynomialOrder);
-            end
-            y_pred{i}(:,z) = polyval(P(:,z),y_pred{i}(:,z));
-        end
-    end
-    %%
-    if exist('v','var')
-        vaftr(i,:)=v/100;
-    end
-    %     vaf(i,:) = 1 - var(y_pred{i} - y_test{i}) ./ var(y_test{i});
-    %     vaf(i,:) = 1 - var(y_pred{i} - ytnew{i}) ./ var(ytnew{i});
-    vaf(i,:)=RcoeffDet(y_pred{i},ytnew{i});
-    
-    for j=1:size(y,2)
-        r{i,j}=corrcoef(y_pred{i}(:,j),ytnew{i}(:,j));
-        if size(r{i,j},2)>1
-            r2(i,j)=r{i,j}(1,2)^2;
-        else
-            r2(i,j)=r{i,j}^2;
-        end
-    end
-    
-end
-
-t_final = t_final(t_final~=0,1);
-
-if (vaf(9,1)-vaf(10,1))>0.5
-    vmean=mean(vaf(1:9,:));
-    vsd=std(vaf(1:9,:));
-else
-    vmean=mean(vaf);
-    vsd=std(vaf);
-end
-
-if (r2(9,1)-r2(10,1))>0.5    %if big disparity in last fold, don't include it in mean
-    r2mean=mean(r2(1:9,:));
-    r2sd=std(r2(1:9,:));
-else
-    r2mean=mean(r2);
-    r2sd=std(r2);
-end
-
+% 
+% if ~strcmpi(signal,'emg')
+%     q = find_active_regions_words(bdf.words,bdf.vel(:,1));
+%     q = interp1(bdf.vel(:,1), q, t);
+% else
+%     q=ones(size(t))';
+% end
+% 
+% x = x(q==1,:);
+% y = y(q==1,:);
+% t = t(:,q==1)';
+% 
+% vaf = zeros(folds,size(y,2));
+% r2 = zeros(folds,size(y,2));
+% fold_length = floor(length(y) ./ folds);
+% 
+% t_final = zeros(length(t)-9*folds,1);
+% 
+% for i = 1:size(x,2)
+%     
+%     x_train = x(:,i);
+%     y_train = y;
+%     x_test = x(:,i);
+%     y_test = y;
+%     t_fold = t;
+%     
+%     % subtract off the mean to reduce offset
+%         y_train = y_train - repmat(mean(y_train),size(y_train,1),1);
+%         y_test{i}= y_test{i} - repmat(mean(y_test{i}),size(y_test{i},1),1);
+%         x_test{i} = x_test{i} - repmat(mean(x_test{i}),size(x_test{i},1),1);
+%         x_train = x_train - repmat(mean(x_train),size(x_train,1),1);
+%     
+%     if ~exist('H','var')
+%         [H{i},v,mcc] = FILMIMO3_tik(x_train, y_train, numlags, numsides,lambda,binsamprate);
+%         
+%         [y_pred{i},xtnew{i},ytnew{i}] = predMIMO3(x_test,H{i},numsides,binsamprate,y_test);
+%         
+%     elseif exist('H','var') && length(H) < i
+%         
+%         [H{i},v,mcc] = FILMIMO3_tik(x_train, y_train, numlags, numsides,lambda,binsamprate);
+%         
+%         [y_pred{i},xtnew{i},ytnew{i}] = predMIMO3(x_test,H{i},numsides,binsamprate,y_test);
+%         
+%     else
+%         
+%         [y_pred{i},xtnew{i},ytnew{i}] = predMIMO3(x_test,H{i},numsides,binsamprate,y_test);
+%         
+%     end
+%     %Polynomial section
+%     
+%     T=[];
+%     patch = [];
+%     
+%     if  PolynomialOrder
+%         %%Find a Wiener Cascade Nonlinearity
+%         for z=1:size(y_pred{i},2)
+%             if Use_Thresh
+%                 Find Threshold
+%                 T_default = 1.25*std(y_pred{i}(:,z));
+%                 [T(z,1), T(z,2), patch(z)] = findThresh(ytnew{i}(:,z),y_pred{i}(:,z),T_default);
+%                 IncludedDataPoints = or(y_pred{i}(:,z)>=T(z,2),y_pred{i}(:,z)<=T(z,1));
+%                 
+%                 Apply Threshold to linear predictions and Actual Data
+%                 PredictedData_Thresh = y_pred{i}(IncludedDataPoints,z);
+%                 ActualData_Thresh = ytnew{i}(IncludedDataPoints,z);
+%                 
+%                 Replace thresholded data with patches consisting of 1/3 of the data to find the polynomial
+%                 Pred_patches = [ (patch(z)+(T(z,2)-T(z,1))/4)*ones(1,round(length(nonzeros(IncludedDataPoints))*4)) ...
+%                     (patch(z)-(T(z,2)-T(z,1))/4)*ones(1,round(length(nonzeros(IncludedDataPoints))*4)) ];
+%                 Act_patches = mean(ytnew{i}(~IncludedDataPoints,z)) * ones(1,length(Pred_patches));
+%                 
+%                 Find Polynomial to Thresholded Data
+%                 if exist('P','var')
+%                     [P(z,:)] = WienerNonlinearity([PredictedData_Thresh; Pred_patches'], [ActualData_Thresh; Act_patches'], PolynomialOrder,'plot');
+%                 end
+%                 
+%                 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%                 %%%%% Use only one of the following 2 lines:
+%                 
+%                   1-Use the threshold only to find polynomial, but not in the model data
+%                 T=[]; patch=[];
+%                 
+%                   2-Use the threshold both for the polynomial and to replace low predictions by the predefined value
+%                                 y_pred{i}(~IncludedDataPoints,z)= patch(z);
+%                 
+%                 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%             elseif ~exist('P','var')
+%                 Find and apply polynomial
+%                 [P(z,:,i)] = WienerNonlinearity(y_pred{i}(:,z), ytnew{i}(:,z), PolynomialOrder);
+%             end
+%             y_pred{i}(:,z) = polyval(P(:,z),y_pred{i}(:,z));
+%         end
+%     end
+%     %
+%     if exist('v','var')
+%         vaftr(i,:)=v/100;
+%     end
+%         vaf(i,:) = 1 - var(y_pred{i} - y_test{i}) ./ var(y_test{i});
+%         vaf(i,:) = 1 - var(y_pred{i} - ytnew{i}) ./ var(ytnew{i});
+%     vaf(i,:)=RcoeffDet(y_pred{i},ytnew{i});
+%     
+%     for j=1:size(y,2)
+%         r{i,j}=corrcoef(y_pred{i}(:,j),ytnew{i}(:,j));
+%         if size(r{i,j},2)>1
+%             r2(i,j)=r{i,j}(1,2)^2;
+%         else
+%             r2(i,j)=r{i,j}^2;
+%         end
+%     end
+%     
+% end
+% 
+% t_final = t_final(t_final~=0,1);
+% 
+% if (vaf(9,1)-vaf(10,1))>0.5
+%     vmean=mean(vaf(1:9,:));
+%     vsd=std(vaf(1:9,:));
+% else
+%     vmean=mean(vaf);
+%     vsd=std(vaf);
+% end
+% 
+% if (r2(9,1)-r2(10,1))>0.5    %if big disparity in last fold, don't include it in mean
+%     r2mean=mean(r2(1:9,:));
+%     r2sd=std(r2(1:9,:));
+% else
+%     r2mean=mean(r2);
+%     r2sd=std(r2);
+% end
+% 
 % snam=[fnam,signal,' predict tikpoly.mat'];
 % save(snam,'v*','x','y*','Poly*','Use*','num*','bin*','H','lambda')
-
-if nargout>5
-    varargout{1}=r2mean;
-    varargout{2}=r2sd;
-    if nargout>7
-        varargout{3}=r2;
-        if nargout>8
-            if exist('vaftr','var')
-                varargout{4}=vaftr;
-            else
-                varargout{4}=[];
-            end
-            
-            if nargout>9
-                varargout{5}=H;
-            end
-            if nargout>10
-                varargout{6}=x;
-                varargout{7}=y;
-                
-                if nargout>12
-                    varargout{8}=ytnew;
-                    varargout{9}=xtnew;
-                    if exist('P','var') && nargout>14
-                        varargout{10}=P;
-                    else
-                        varargout(10)={0};
-                    end
-                    
-                    %                     if nargout>15
-                    %                         varargout{11}=t_final;
-                    %                     end
-                end
-            end
-        end
-    end
-    
-end
-
-
-    function [Tinf, Tsup, patch] = findThresh(ActualData,LinPred,T)
-        
-        thresholding = 1;
-        h = figure;
-        xT = [0 length(LinPred)];
-        offset = mean(LinPred)-mean(ActualData);
-        LinPred = LinPred-offset;
-        Tsup=mean(LinPred)+T;
-        Tinf=mean(LinPred)-T;
-        patch = mean(ActualData);
-        
-        while thresholding
-            hold off; axis('auto');
-            plot(ActualData,'b');
-            hold on;
-            plot(LinPred,'r');
-            plot(xT,[Tsup Tsup],'k--',xT,[Tinf Tinf],'k--');
-            legend('Actual Data', 'Linear Fit','Threshold');
-            axis('manual');
-            reply = input(sprintf('Redefine High Threshold? [%g] : ',Tsup));
-            if ~isempty(reply)
-                Tsup = reply;
-            else
-                thresholding=0;
-            end
-        end
-        thresholding=1;
-        while thresholding
-            axis('auto');
-            hold off;
-            plot(ActualData,'b');
-            hold on;
-            plot(LinPred,'r');
-            plot(xT,[Tsup Tsup],'k--',xT,[Tinf Tinf],'k--');
-            legend('Actual Data', 'Linear Fit','Threshold');
-            axis('manual');
-            reply = input(sprintf('Redefine Low Threshold? [%g] : ',Tinf));
-            if ~isempty(reply)
-                Tinf = reply;
-            else
-                thresholding=0;
-            end
-        end
-        thresholding=1;
-        while thresholding
-            axis('auto');
-            hold off;
-            plot(ActualData,'b');
-            hold on;
-            plot(LinPred,'r');
-            plot(xT,[Tsup Tsup],'k--',xT,[Tinf Tinf],'k--', xT,[patch patch],'g');
-            legend('Actual Data', 'Linear Fit','Threshold');
-            axis('manual');
-            reply = input(sprintf('Redefine Threshold Value? [%g] : ',patch));
-            if ~isempty(reply)
-                patch = reply;
-            else
-                thresholding=0;
-            end
-        end
-        Tsup = Tsup+offset;
-        Tinf = Tinf+offset;
-        patch = patch+offset;
-        
-        close(h);
-    end
-end
+% 
+% if nargout>5
+%     varargout{1}=r2mean;
+%     varargout{2}=r2sd;
+%     if nargout>7
+%         varargout{3}=r2;
+%         if nargout>8
+%             if exist('vaftr','var')
+%                 varargout{4}=vaftr;
+%             else
+%                 varargout{4}=[];
+%             end
+%             
+%             if nargout>9
+%                 varargout{5}=H;
+%             end
+%             if nargout>10
+%                 varargout{6}=x;
+%                 varargout{7}=y;
+%                 
+%                 if nargout>12
+%                     varargout{8}=ytnew;
+%                     varargout{9}=xtnew;
+%                     if exist('P','var') && nargout>14
+%                         varargout{10}=P;
+%                     else
+%                         varargout(10)={0};
+%                         if nargout > 15
+%                             varargout{11} = cells
+%                         end
+%                     end
+%                     
+%                                         if nargout>15
+%                                             varargout{11}=t_final;
+%                                         end
+%                 end
+%             end
+%         end
+%     end
+%     
+% end
+% 
+% 
+%     function [Tinf, Tsup, patch] = findThresh(ActualData,LinPred,T)
+%         
+%         thresholding = 1;
+%         h = figure;
+%         xT = [0 length(LinPred)];
+%         offset = mean(LinPred)-mean(ActualData);
+%         LinPred = LinPred-offset;
+%         Tsup=mean(LinPred)+T;
+%         Tinf=mean(LinPred)-T;
+%         patch = mean(ActualData);
+%         
+%         while thresholding
+%             hold off; axis('auto');
+%             plot(ActualData,'b');
+%             hold on;
+%             plot(LinPred,'r');
+%             plot(xT,[Tsup Tsup],'k--',xT,[Tinf Tinf],'k--');
+%             legend('Actual Data', 'Linear Fit','Threshold');
+%             axis('manual');
+%             reply = input(sprintf('Redefine High Threshold? [%g] : ',Tsup));
+%             if ~isempty(reply)
+%                 Tsup = reply;
+%             else
+%                 thresholding=0;
+%             end
+%         end
+%         thresholding=1;
+%         while thresholding
+%             axis('auto');
+%             hold off;
+%             plot(ActualData,'b');
+%             hold on;
+%             plot(LinPred,'r');
+%             plot(xT,[Tsup Tsup],'k--',xT,[Tinf Tinf],'k--');
+%             legend('Actual Data', 'Linear Fit','Threshold');
+%             axis('manual');
+%             reply = input(sprintf('Redefine Low Threshold? [%g] : ',Tinf));
+%             if ~isempty(reply)
+%                 Tinf = reply;
+%             else
+%                 thresholding=0;
+%             end
+%         end
+%         thresholding=1;
+%         while thresholding
+%             axis('auto');
+%             hold off;
+%             plot(ActualData,'b');
+%             hold on;
+%             plot(LinPred,'r');
+%             plot(xT,[Tsup Tsup],'k--',xT,[Tinf Tinf],'k--', xT,[patch patch],'g');
+%             legend('Actual Data', 'Linear Fit','Threshold');
+%             axis('manual');
+%             reply = input(sprintf('Redefine Threshold Value? [%g] : ',patch));
+%             if ~isempty(reply)
+%                 patch = reply;
+%             else
+%                 thresholding=0;
+%             end
+%         end
+%         Tsup = Tsup+offset;
+%         Tinf = Tinf+offset;
+%         patch = patch+offset;
+%         
+%         close(h);
+%     end
+% end
 
