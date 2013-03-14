08ca849a28ca8f4e54dddec6eaf90a95bcc91571
Robert Flint <r-flint@northwestern.edu>
2013-03-14 19:04:31 +0000

    git-svn-id: http://citadel.physiology.northwestern.edu/svn/s1_analysis@1098 984b1e3c-1c57-0410-ae62-c18f90d3b645

--- proc/robert/FileParts.m
+++ proc/robert/FileParts.m
@@ -13,24 +13,28 @@ function varargout=FileParts(nameIn)
 % ones use ~ for earlier outputs).  If the current version of 
 % fileparts.m doesn't support the 4th argument and you ask for it anyway, 
 % it will be returned as NaN.
+%
+% does not use ~ because that is illegal for matlab 2007a.
+
+
 
 if verLessThan('matlab','7.11')
     switch nargout
         case 1
-            [varargout{1},~,~,~]=fileparts(nameIn);
+            [varargout{1},junk1,junk2,junk3]=fileparts(nameIn); clear junk*
         case 2
-            [varargout{1},varargout{2},~,~]=fileparts(nameIn);
+            [varargout{1},varargout{2},junk1,junk2]=fileparts(nameIn); clear junk*
         case 3            
-            [varargout{1},varargout{2},varargout{3},~]=fileparts(nameIn);
+            [varargout{1},varargout{2},varargout{3},junk1]=fileparts(nameIn); clear junk1
         case 4
             [varargout{1},varargout{2},varargout{3},varargout{4}]=fileparts(nameIn);
     end
 else
     switch nargout
         case 1
-            [varargout{1},~,~]=fileparts(nameIn);
+            [varargout{1},junk1,junk2]=fileparts(nameIn); clear junk*
         case 2
-            [varargout{1},varargout{2},~]=fileparts(nameIn);
+            [varargout{1},varargout{2},junk1]=fileparts(nameIn); clear junk1
         case 3            
             [varargout{1},varargout{2},varargout{3}]=fileparts(nameIn);
         case 4
