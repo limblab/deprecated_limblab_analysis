55a4058765b17668bbcce8621b867a9c690875c4
Ricardo Torres <ricardort@gmail.com>
2013-04-26 22:26:02 +0000

    git-svn-id: http://citadel.physiology.northwestern.edu/svn/s1_analysis@1129 984b1e3c-1c57-0410-ae62-c18f90d3b645

---
+++ proc/ricardo/SpikeSorting/mergeNEVSpikesForSorting.m
@@ -0,0 +1,30 @@
+
+file_prefix = 'Kevin_2013-04-18_';
+filepath = 'D:\Data\Kevin_12A2\Data\';
+NEVlist = dir([filepath file_prefix '*.nev']);
+
+if isempty(strfind({NEVlist.name},'-spikes-s'))
+    NEVlist = NEVlist(cellfun('isempty',(regexp({NEVlist(:).name},'-s'))));
+    NEVNSx_all = concatenate_NEVs(filepath,file_prefix);
+    saveNEVOnlySpikes(NEVNSx_all.NEV, filepath, [file_prefix '-spikes.nev'])
+else
+    % Un-merge
+    NEV_sorted = openNEV('read',[filepath file_prefix '-spikes-s.nev']);
+    for iFile = 1:length(NEVNSx_all.MetaTags.NEVlist) 
+        t_offset = (NEVNSx_all.MetaTags.FileStartSec(iFile))*30000;
+        NEV_spikes_struct(iFile) = NEV_sorted.Data.Spikes;
+        first_idx = find(NEV_sorted.Data.Spikes.TimeStamp >= NEVNSx_all.MetaTags.FileStartSec(iFile)*30000,1,'first');
+        if iFile < length(NEVNSx_all.MetaTags.NEVlist)
+            last_idx = find(NEV_sorted.Data.Spikes.TimeStamp < NEVNSx_all.MetaTags.FileStartSec(iFile+1)*30000,1,'last');
+        else
+            last_idx = length(NEV_sorted.Data.Spikes.TimeStamp);
+        end
+        NEV_spikes_struct(iFile).TimeStamp =  NEV_spikes_struct(iFile).TimeStamp(first_idx:last_idx) - t_offset;
+        NEV_spikes_struct(iFile).Electrode =  NEV_spikes_struct(iFile).Electrode(first_idx:last_idx);
+        NEV_spikes_struct(iFile).Unit =  NEV_spikes_struct(iFile).Unit(first_idx:last_idx);
+        NEV_spikes_struct(iFile).Waveform =  NEV_spikes_struct(iFile).Waveform(:,first_idx:last_idx);
+        load([filepath NEVNSx_all.MetaTags.NEVlist{iFile}(1:end-4)])
+        NEV.Data.Spikes = NEV_spikes_struct(iFile);
+        save([filepath NEVNSx_all.MetaTags.NEVlist{iFile}(1:end-4) '-s'],'NEV')
+    end
+end
