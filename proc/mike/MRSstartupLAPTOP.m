f585ba3fcff15484629b635702c1f70858def6c0
Mike Scheid <michaelscheid2015@northwestern.edu>
2013-05-04 19:17:11 +0000

    Startup script for Mike's laptop
    git-svn-id: http://citadel.physiology.northwestern.edu/svn/s1_analysis@1139 984b1e3c-1c57-0410-ae62-c18f90d3b645

---
+++ proc/mike/MRSstartupLAPTOP.m
@@ -0,0 +1,10 @@
+%addpath(genpath('\\Citadel\limblab\user_folders\Marc\matlab code'))
+
+%addpath(genpath('C:\Documents and Settings\Administrator\Desktop\RobertF\my code_mods'))
+
+addpath(genpath('C:\Users\M.R.Scheid\Desktop\s1_analysis'))
+
+%rmpath('C:\Documents and Settings\Administrator\My Documents\s1_analysis\proc\Tucker')
+cd('C:\Users\M.R.Scheid\Documents\Mike_Data')
+dbstop if error
+clc;
