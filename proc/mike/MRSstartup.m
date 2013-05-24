dc50c17f824935585daf7851ae73cc9462f95584
Mike Scheid <michaelscheid2015@northwestern.edu>
2013-05-24 22:22:43 +0000

    startup for GOB
    git-svn-id: http://citadel.physiology.northwestern.edu/svn/s1_analysis@1172 984b1e3c-1c57-0410-ae62-c18f90d3b645

--- proc/mike/MRSstartup.m
+++ proc/mike/MRSstartup.m
@@ -1,6 +1,7 @@
 addpath(genpath('\\Citadel\limblab\user_folders\Marc\matlab code'))
 addpath(genpath('C:\Documents and Settings\Administrator\Desktop\RobertF\my code_mods'))
 addpath(genpath('C:\Documents and Settings\Administrator\My Documents\s1_analysis'))
+addpath(genpath('Y:\user_folders\Marc\circ_stat'))
 rmpath('C:\Documents and Settings\Administrator\My Documents\s1_analysis\proc\Tucker')
 cd('C:\Documents and Settings\Administrator\Desktop\Mike_Data\Spike LFP Decoding')
 dbstop if error
