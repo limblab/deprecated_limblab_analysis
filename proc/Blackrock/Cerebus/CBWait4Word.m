329edd200a28dc64203a747660a3f1183d8329c6
Ricardo Torres <ricardort@gmail.com>
2013-06-06 16:23:30 +0000

    Script now works, added masks, etc.
    git-svn-id: http://citadel.physiology.northwestern.edu/svn/s1_analysis@1183 984b1e3c-1c57-0410-ae62-c18f90d3b645

--- proc/Blackrock/Cerebus/CBWait4Word.m
+++ proc/Blackrock/Cerebus/CBWait4Word.m
@@ -1,37 +1,58 @@
-function [ Status lower4bits Delay ] = CBWait4Word( myword, interval, maxwait, mode )
+function [ Status words Delay ] = CBWait4Word( myword, interval, maxwait, mask, mode )
 % CBWait4Word: Run a tight loop, returning when the required word appears at Cerebus
 %
 % myword:   code, the upper 4 bits of which must appear before the function returns
 % interval: time to wait between Cerebus queries
 % maxwait:  time to return if myword does not appear
+% mask:     use if looking for a particular group of bits, default is 0xFF
 % mode:     'test' to read mat file, '' to wait for Cerebus
+Status = 'fail';
+words = [];
+Delay = [];
 
-if nargin < 3 | nargin > 4
+if nargin < 3 || nargin > 5	
     help CBWait4Word
     return
 end
-if nargin == 3,mode='open';end
+if nargin == 3 || nargin == 4 ,mode='open';end
+if nargin == 3 , mask = 'FF';end
 switch mode
     case 'test'
         cyclenum = 1;
 end
 
-% Data words use this code:
+% Digital input is channel 151:
 CHAN = 151;
 % Words from Cerebus are shifted left 16 bits
 % Ignore the data specified in the lower 4 bits; for stimulus this is "g"
-WordCode = bitshift(bitand(myword,double(hex2dec('F0'))),8);
+try 
+	myword = hex2dec(myword);
+catch
+	help CBWait4Word
+	return
+end
+
+try 
+	mask = hex2dec(mask);
+	mask = bitshift(mask,8);
+catch
+	help CBWait4Word
+	return
+end
+
+% myword = bitand(mask,myword);
+WordCode = myword;
+% WordCode = bitshift(bitand(myword,double(hex2dec('F0'))),8);
 et_col = tic;
 Delay = toc(et_col); % elapsed time of collection
-Status = 'Success';
-lower4bits=-1;
+
+words=-1;
 %% MAIN LOOP
 % Data Collection section
 while (Delay < maxwait)
     if strcmp(mode, 'test')
         trialdata = cbmextest(cyclenum);
         cyclenum = cyclenum+1;
-        %            bCollect=false;
     else
         try
             trialdata = cbmex('trialdata',1); % read some data
@@ -39,25 +60,29 @@ while (Delay < maxwait)
             CBInitWordRead(mode);
             trialdata = cbmex('trialdata',1); % read some data
         end
-    end
-    %t_col0 = tic;   % restart timer for next collection period
+	end    
     
     if numel(trialdata)==0 % no words in this raw batch
         continue
     end
     raw.ts=trialdata{CHAN,2};
     raw.codes=trialdata{CHAN,3};
-    % Data words have some of their top 4 bits set.
-    % Here are their indices in the raw buffer
-    RawIndices = find(bitand(raw.codes, hex2dec('f000'))==WordCode,1,'first');
+	% Find our masked word in raw data
+	RawIndices = find(bitshift(bitand(raw.codes,mask),-8)==bitand(bitshift(mask,-8),WordCode));
+
     if RawIndices
-        lower4bits = bitand(raw.codes(RawIndices), 15);
+		% Return timestamps and words
+        words = [double(raw.ts(RawIndices))/30000 double(bitshift(bitand(raw.codes(RawIndices),hex2dec('FF00')),-8))];
+		if size(words,1) > 1
+			words = words(diff(words(:,1))>.0005,:);
+		end
+		Delay = toc(et_col); % elapsed time of collection
+		Status = 'Success';
         return
     end
     if interval
         pause(interval)
-    end
-    Delay = toc(et_col); % elapsed time of collection
+	end    
 end
 Status = 'Timeout';
 end
