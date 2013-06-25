d7d109ba2d7904188016e89b9a267d6815557940
Robert Flint <r-flint@northwestern.edu>
2013-06-25 18:55:10 +0000

    git-svn-id: http://citadel.physiology.northwestern.edu/svn/s1_analysis@1202 984b1e3c-1c57-0410-ae62-c18f90d3b645

---
+++ proc/mike/MRSstartupAPU.m
@@ -0,0 +1,6 @@
+addpath(genpath('\\Citadel\limblab\user_folders\Marc\matlab code'))
+addpath(genpath('E:\s1_analysis'))
+rmpath('C:\Documents and Settings\Administrator\My Documents\s1_analysis\proc\nick')
+cd('C:\Users\Apu\Desktop\Mike_Data')
+dbstop if error
+clc;
