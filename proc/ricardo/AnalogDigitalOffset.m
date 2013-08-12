b70acfd12f49063d0f2608f95d2d1aebc1a7d978
Ricardo Torres <ricardort@gmail.com>
2013-08-12 21:30:49 +0000

    git-svn-id: http://citadel.physiology.northwestern.edu/svn/s1_analysis@1254 984b1e3c-1c57-0410-ae62-c18f90d3b645

--- proc/ricardo/AnalogDigitalOffset.m
+++ proc/ricardo/AnalogDigitalOffset.m
@@ -12,20 +12,20 @@ titles = {'No sync, 30kHz','Synced, 30kHz','Synced, recording from both, 30kHz',
 
 %%
 figure; 
-for iBdf = 1:length(bdf)
-    subplot(2,3,iBdf)
-    plot(bdf{iBdf}.analog.ts,bdf{iBdf}.analog.data{27})
+for iBDF = 1:length(bdf)
+    subplot(2,3,iBDF)
+    plot(bdf{iBDF}.analog.ts,bdf{iBDF}.analog.data{27})
     hold on
-    plot([bdf{iBdf}.units(1).ts bdf{iBdf}.units(1).ts]',[0 4000]','r')    
-    analog_onset = find(diff(bdf{iBdf}.analog.data{27})>1000);
+    plot([bdf{iBDF}.units(1).ts bdf{iBDF}.units(1).ts]',[0 4000]','r')    
+    analog_onset = find(diff(bdf{iBDF}.analog.data{27})>1000);
     analog_onset(find(diff(analog_onset) < 2)+1) = [];
-    analog_spikes = bdf{iBdf}.analog.ts(analog_onset)';
-    analog_spikes = analog_spikes(analog_spikes>2 & analog_spikes<bdf{iBdf}.analog.ts(end)-2);
-    digital_spikes = bdf{iBdf}.units(1).ts;
-    digital_spikes = digital_spikes(digital_spikes>2 & digital_spikes<bdf{iBdf}.analog.ts(end)-2);
+    analog_spikes = bdf{iBDF}.analog.ts(analog_onset)';
+    analog_spikes = analog_spikes(analog_spikes>2 & analog_spikes<bdf{iBDF}.analog.ts(end)-2);
+    digital_spikes = bdf{iBDF}.units(1).ts;
+    digital_spikes = digital_spikes(digital_spikes>2 & digital_spikes<bdf{iBDF}.analog.ts(end)-2);
 
     average_delay = mean(analog_spikes-digital_spikes);
-    title({titles{iBdf};['Average delay: ' num2str(average_delay*1000) ' ms']})
+    title({titles{iBDF};['Average delay: ' num2str(average_delay*1000) ' ms']})
     xlabel('t (s)')
     legend('Digital','Analog')
     xlim([analog_spikes(2)-.03 analog_spikes(2)+.03])
