4b90e77860ef72aa732bc9216c81b870afa3703c
Ted Ballou <ted@mycompany.com>
2013-08-27 21:54:25 +0000

    new files to interleave multiple electrodes on one module
    git-svn-id: http://citadel.physiology.northwestern.edu/svn/s1_analysis@1272 984b1e3c-1c57-0410-ae62-c18f90d3b645

---
+++ proc/Blackrock/Cerestim_M96/oneModuleMultElect.m
@@ -0,0 +1,93 @@
+% oneModuleMultElect([electrode_list], delay)
+% 
+% csmex interface code to interleave multiple electrodes on a single current
+% module, with interleaving delay "delay"
+
+%         <----stimdur-------->
+%          -----
+%         |  ^  |          <-->slack
+%         |  |  |
+% --------   |   ----       ------------------------------------------
+%     duration    ^  |     |
+%                 |  |     |
+%        interphase   -----    <--delay->
+%                                         -----
+%                                        |     |
+%                                        |     |
+% ---------------------------------------       ----       -----------
+%                                                   |     |
+%          <-----Period(msec)=1000/freq-->          |     |
+%                                                    -----
+
+function retv = oneModuleMultElect(electrodes, delay)
+
+if nargin ~= 2
+    help oneModuleMultElect
+    errstr = sprintf('%s\n%s\n','Not enough arguments',...
+        'select electrode list and delay>StimDuration');
+    error(errstr);
+end
+
+numels = numel(electrodes);
+if numels < 1 || floor(delay) ~= delay
+    help oneModuleMultElect
+    errstr = sprintf('%s\n%s\n',...
+        'electrode list must provide at least one electrode',...
+        'delay must be an integer number of milliseconds');
+    error(errstr)
+end
+
+try
+    [retval] = csmex('connect');
+catch
+    if retval~=-10  % code for already connected, fail for other error
+        error('fail to initialize csmex')
+    end
+end
+
+% All times are in microseconds
+interphase = 54;
+duration = 500;
+slack = 200;    % put in a little extra time between electrode switches
+stimdur = 2*duration+interphase+slack;
+
+if numels > 1 && delay < stimdur
+    error('delay too short!')
+end
+% configure pattern #1
+configID = 1;   % Use stimulator module #1
+freq = 20;      % period = 50 msec
+polarity = 0;   % anode first
+numpuls = 60;
+ampl = 20;      % pulse amplitude in  microAmps
+
+if 1000/freq <= numels*stimdur/1000
+    help oneModuleMultElect
+    errmsg=sprintf('Period is %d ms but last electrode delay is %d ms', ...
+        1000/freq, (numel(electrodes)-1)*delay);
+    error(errmsg)
+end
+
+csmex('configure',configID,polarity,numpuls,ampl,ampl,...
+    duration,duration,freq,interphase);
+
+csmex('beginningOfSequence');
+csmex('beginningOfGroup');
+for i=1:numel(electrodes)
+    if electrodes(i)==0
+        break
+    end
+    if i>1
+        csmex('wait',stimdur);
+    end
+    as=tic;
+    csmex('autoStimulus',electrodes(i),configID);
+    fprintf('Time to run (ms)=%.2f\n',toc(as)*1000)
+end
+csmex('endOfGroup');
+csmex('endOfSequence');
+
+csmex('play',1);
+
+csmex('disconnect');
+end
