ea4b6f6aefbf972429628ef27823420b87f18e52
Ricardo Torres <ricardort@gmail.com>
2013-09-25 18:21:13 +0000

    git-svn-id: http://citadel.physiology.northwestern.edu/svn/s1_analysis@1305 984b1e3c-1c57-0410-ae62-c18f90d3b645

--- lib/Blackrock/Cerebus/cbmextest.m
+++
@@ -1,8 +0,0 @@
-function trialdata = cbmextest()
-% cbmextest: function for use with cbmex testing, that reads in data from a
-% Matlab 'trialdata' file, useful for debugging. Set 'mode' parameter for
-% CBWait4Word to 'test' to use this.
-
-name = uigetfile('*.mat', 'Select a "trialdata" file') 
-load(name, 'trialdata');
-end
