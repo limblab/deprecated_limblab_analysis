80345d682738da417b7f5893bf9c3c6ad7b01bae
Robert Flint <r-flint@northwestern.edu>
2013-10-14 05:00:28 +0000

    git-svn-id: http://citadel.physiology.northwestern.edu/svn/s1_analysis@1316 984b1e3c-1c57-0410-ae62-c18f90d3b645

---
+++ proc/robert/pngAllFigs.m
@@ -0,0 +1,22 @@
+function pngAllFigs(pathName)
+
+currPath=pwd;
+
+if nargin==0
+    pathName=uigetdir('','select a folder with .figs');
+else
+    if exist(pathName,'file')~=7
+        pathName=uigetdir(pathName,'select a folder with .figs');
+    end
+end
+cd(pathName)
+D=dir(pathName);
+D(cellfun(@isempty,regexp({D.name},'\.fig')))=[];
+
+for n=1:length(D)
+    open(D(n).name)
+    print(gcf,regexprep(D(n).name,'\.fig','.png'),'-dpng')
+    close
+end
+
+cd(currPath)
