c508b42729f2d6f1c3687e5bd14ff2127c00db78
Ricardo Torres <ricardort@gmail.com>
2013-11-25 17:38:38 +0000

    git-svn-id: http://citadel.physiology.northwestern.edu/svn/s1_analysis@1344 984b1e3c-1c57-0410-ae62-c18f90d3b645

--- proc/ricardo/UF_analysis/UF_analysis.m
+++ proc/ricardo/UF_analysis/UF_analysis.m
@@ -32,15 +32,15 @@ if filedate < datenum('2013-06-19')
     file_details.rot_handle = 0;
 end
 
-reload_data = 1;
-plot_behavior = 0;
-plot_emg = 0;
+reload_data = 0;
+plot_behavior = 1;
+plot_emg = 1;
 plot_units = 1;
 plot_STAEMG = 0;
 plot_SSEP = 0;
 plot_decode = 0;
 decode_input = {'kinematics','emg','units'};
-save_figs = 0;
+save_figs = 1;
 
 wrong_file_loaded = 0;
 if exist('UF_struct','var')
