46c08d61cd7f844e1b7e00ce4f25a371555d86ab
Mike Scheid <michaelscheid2015@northwestern.edu>
2014-01-06 23:40:52 +0000

    git-svn-id: http://citadel.physiology.northwestern.edu/svn/s1_analysis@1382 984b1e3c-1c57-0410-ae62-c18f90d3b645

--- proc/mike/MRSstartupLAPTOP.m
+++ proc/mike/MRSstartupLAPTOP.m
@@ -2,14 +2,15 @@
 
 %addpath(genpath('C:\Documents and Settings\Administrator\Desktop\RobertF\my code_mods'))
 
-addpath(genpath('C:\Users\M.R.Scheid\Desktop\s1_analysis'))
+addpath(genpath('C:\Users\M.R.Scheid\Desktop\Monkey Data Analysis\s1_analysis'))
 
 addpath(genpath('C:\Users\M.R.Scheid\Desktop\Local Code'))
+addpath(genpath('C:\Users\M.R.Scheid\Desktop\Monkey Data Analysis\chronux_2_10\chronux'))
 
 addpath C:\Users\M.R.Scheid\Desktop\fieldtrip-20131124
-ft_defaults
+%ft_defaults
 
 %rmpath('C:\Documents and Settings\Administrator\My Documents\s1_analysis\proc\Tucker')
-cd('C:\Users\M.R.Scheid\Documents\Mike_Data')
+cd('C:\Users\M.R.Scheid\Desktop\Monkey Data Analysis')
 dbstop if error
 clc;
