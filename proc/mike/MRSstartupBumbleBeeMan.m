df84fbbabf34e9fe38ee2184f1dfaeee2dbb2626
Mike Scheid <michaelscheid2015@northwestern.edu>
2014-01-07 23:00:15 +0000

    Startup for Bumblebeeman with correct paths
    git-svn-id: http://citadel.physiology.northwestern.edu/svn/s1_analysis@1384 984b1e3c-1c57-0410-ae62-c18f90d3b645

---
+++ proc/mike/MRSstartupBumbleBeeMan.m
@@ -0,0 +1,6 @@
+addpath(genpath('\\Citadel\limblab\user_folders\Marc\matlab code'))
+addpath(genpath('C:\Users\Mike\Desktop\s1_analysis'))
+rmpath('C:\Users\Mike\Desktop\s1_analysis\proc\nick')
+cd('C:\Users\Mike\Desktop\Mike_Data')
+dbstop if error
+clc;
