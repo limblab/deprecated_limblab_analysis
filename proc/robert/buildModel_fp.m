28b2f7fb81f882abcc3ba1ad42c446e615284111
Robert Flint <r-flint@northwestern.edu>
2014-04-14 21:24:57 +0000

    git-svn-id: http://citadel.physiology.northwestern.edu/svn/s1_analysis@1439 984b1e3c-1c57-0410-ae62-c18f90d3b645

--- proc/robert/buildModel_fp.m
+++ proc/robert/buildModel_fp.m
@@ -236,9 +236,13 @@ PB(7,:,:)=mean(PA(freqs>30 & freqs<50,:,:),1);
 % PB(1,:,:)=mean(PA(gam1,:,:),1);
 % PB(2,:,:)=mean(PA(gam2,:,:),1);
 % PB(3,:,:)=mean(PA(gam3,:,:),1);
+
 % % test a combined gamma band alone (11/13/2012).
 % PB=[];
 % PB(1,:,:)=mean(PA(freqs>70 & freqs<300,:,:),1);
+% 
+% PB=[];
+% PB(1,:,:)=mean(PA(freqs>30 & freqs<50,:,:),1);
 
 % PB has dims freqs X chans X bins
 disp('4th part: calculate bandpower')
