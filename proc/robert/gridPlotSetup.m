c2205f2460aad92ada1ef3b24780fc4a4a8cfe6d
Robert Flint <r-flint@northwestern.edu>
2014-04-14 21:33:05 +0000

    git-svn-id: http://citadel.physiology.northwestern.edu/svn/s1_analysis@1445 984b1e3c-1c57-0410-ae62-c18f90d3b645

--- proc/robert/gridPlotSetup.m
+++ proc/robert/gridPlotSetup.m
@@ -1,7 +1,10 @@
-function axH=gridPlotSetup(figH,numPlots)
+function axH=gridPlotSetup(figH,numPlots,layoutOverride)
 
-% syntax axH=gridPlotSetup(figH,numPlots)
+% syntax axH=gridPlotSetup(figH,numPlots,layoutOverride)
 %
+% layoutOverride is a [rows cols] vector.  If omitted, defaults to a 
+% square arrangement.
+% 
 % to add: variables for if we want to take up some fraction of the
 % figure window instead of the whole thing
 
@@ -14,9 +17,13 @@ else
     numXplots=numPlotsRoot;
 end
 heightOfPlot=1/numPlotsRoot;
+if nargin > 2
+    numXplots=layoutOverride(2);
+    heightOfPlot=1/layoutOverride(1);
+end
 widthOfPlot=1/numXplots;
-pushRight=0; pushDown=0;
 
+pushRight=0; pushDown=0;
 for n=1:numPlots
     if ~mod(n-1,numXplots), pushRight=0; end
     x=1-widthOfPlot*numXplots+pushRight*widthOfPlot;
