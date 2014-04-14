cf4d2fdee5800db867ce2c5c6bcf47ee7121362c
Robert Flint <r-flint@northwestern.edu>
2014-04-14 21:37:26 +0000

    added several enhancements, notably: (1) the ability to look at different frequency bands individually (or as a group), and (2) the ability to designate a feature index array (e.g. for direct or indirect features)
    git-svn-id: http://citadel.physiology.northwestern.edu/svn/s1_analysis@1448 984b1e3c-1c57-0410-ae62-c18f90d3b645

--- proc/robert/LFP MSP stability/plot_r2_cellArray_singleFeatures.m
+++ proc/robert/LFP MSP stability/plot_r2_cellArray_singleFeatures.m
@@ -1,4 +1,4 @@
-function plot_r2_cellArray_singleFeatures(r2Array,bestc,bestf)
+function plot_r2_cellArray_singleFeatures(r2Array,bestc,bestf,bandsToUse,featIndFromDecoder)
 
 % this function only works with 2D r2Array.  If there is a monkey dimension
 % in the input array, squeeze it out before passing it to this function.
@@ -10,12 +10,29 @@ for n=1:size(r2Array,1)
 end
 % r2Avg(sum(isnan(r2Avg),2)==size(r2Avg,2),:)=[];
 
+if nargin > 3
+    if isempty(bandsToUse)
+        bandsToUse=1:6;
+    end
+    bandInd=zeros(96,length(bandsToUse));
+    for n=1:length(bandsToUse)
+        bandInd(:,n)=bandsToUse(n):6:size(r2Avg,1);
+    end
+else
+    bandInd=rowBoat(1:size(r2Avg,1));
+end
+if nargin > 4
+    featToKeep=intersect(sort(bandInd(:)),featIndFromDecoder);
+else
+    featToKeep=sort(bandInd(:));
+end
+r2Avg=r2Avg(featToKeep,:);
 figure, imagesc(r2Avg)
 
 try                                                                         %#ok<TRYNC>
     [~,~,~,~,~,~] = CorrCoeffMap(r2Avg,0);
 end
-
+caxis([0 1])
 temp=nanmean(get(get(gca,'Children'),'CData'),2); 
 figure
 plot(temp,'ok','LineWidth',2)
