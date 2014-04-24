28dde54df78d9dba413bebb8bbb0c2cc7217a4c6
Tucker Tomlinson <tuckertomlinson@gmail.com>
2014-04-24 23:28:14 +0000

    git-svn-id: http://citadel.physiology.northwestern.edu/svn/s1_analysis@1458 984b1e3c-1c57-0410-ae62-c18f90d3b645

---
+++ proc/Tucker/PDs/get_move_pds_function.m
@@ -0,0 +1,29 @@
+function [figure_list,data_struct]=get_move_pds_function(folderpath,input_data)
+
+%%set params
+    ts = 50;%binning size
+    offset=-0.015; %a positive offset compensates for neural data leading kinematic data, a negative offset compensates for a kinematic lead
+
+    %load data
+    fname=follow_links([folderpath,input_data.filename]);
+    disp(strcat('converting: ',fname))
+    bdf=get_cerebus_data(fname,3,'verbose','noeye');
+    data_struct.bdf=bdf;
+
+    %identify time vector for binning
+    vt = bdf.vel(:,1);
+    t = vt(1):ts/1000:vt(end);
+
+    disp('computing PDs and plotting results')
+    array_map_path='C:\Users\limblab\Desktop\kramer_array_map\6251-0922.cmp';
+    [outdata,H_upper,H_lower]=PD_plot(bdf,array_map_path,2,1);
+    data_struct.PD_data=outdata;
+    
+      set(H_upper,'Name','Upper_half')
+      set(H_lower,'Name','Lower_half')
+      set(H_upper,'Position',[100 100 1200 1200])
+      set(H_lower,'Position',[100 100 1200 1200])
+     figure_list(1)=H_upper;
+     figure_list(2)=H_lower;
+end
+    
