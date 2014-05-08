fc024ce2985d3a49f7ea8ced8a7c96c0348d52e3
Mike Scheid <michaelscheid2015@northwestern.edu>
2014-05-08 18:44:14 +0000

    Startup for GOB
    git-svn-id: http://citadel.physiology.northwestern.edu/svn/s1_analysis@1476 984b1e3c-1c57-0410-ae62-c18f90d3b645

---
+++ proc/mike/MRSstartupGOB.m
@@ -0,0 +1,7 @@
+addpath(genpath('C:\Users\Scheidmeister\Desktop\s1_analysis'))
+addpath(genpath('Y:\user_folders\Marc\circ_stat'))
+rmpath('C:\Users\Scheidmeister\Desktop\s1_analysis\proc\Tucker')
+rmpath('C:\Users\Scheidmeister\Desktop\s1_analysis\proc\Nick')
+%cd('C:\Documents and Settings\Administrator\Desktop\Mike_Data\Spike LFP Decoding')
+dbstop if error
+clc;
