1b360a900e5d41d4b445b108d32eb2a52605211f
Robert Flint <r-flint@northwestern.edu>
2014-05-23 17:05:18 +0000

    updated for (apparently) changed code in BDF2binned.m, and to return the channel numbers of the identified shunted channels.
    git-svn-id: http://citadel.physiology.northwestern.edu/svn/s1_analysis@1485 984b1e3c-1c57-0410-ae62-c18f90d3b645

--- proc/robert/LFP MSP stability/unitCorrAnalysis(shunting).m
+++ proc/robert/LFP MSP stability/unitCorrAnalysis(shunting).m
@@ -6,7 +6,9 @@ for n=1:length(allChewieNames)
     try
         load(findBDFonCitadel(allChewieNames{n}))
         stoptime=out_struct.meta.duration;
-        binnedData = convertBDF2binned('out_struct',binsize,starttime,stoptime,5,0,MinFiringRate);
+        binnedData = convertBDF2binned(out_struct,struct('binsize',binsize, ...
+            'starttime',starttime,'stoptime',stoptime,'EMG_hp',5,'EMG_lp',0, ...
+            'minFiringRate',MinFiringRate)); disp('done')
         units_Chewie{n}=binnedData.spikeguide;                              %#ok<*SAGROW>
         clear out_struct
         for k=1:size(binnedData.spikeratedata,2)
@@ -40,6 +42,9 @@ for n=1:length(allMiniNames)
 end, clear n
 clear MinFiringRate binsize *time
 
+%% to use pre-existing analysis
+load('E:\personnel\RobertF\monkey_analyzed\LFP MSP stability\allUnitsXcorr(shunt_analysis).mat')
+
 %% post-processing of corrMat_* using units_*
 units_Chewie_num=cellfun(@(x) cellfun(@str2num,x,'UniformOutput',0), ...
     cellfun(@(x) regexp(x,'(?<=ee)[0-9]{2}(?=u1)','match','once'), ...
@@ -51,18 +56,29 @@ units_Mini_num=cellfun(@(x) cellfun(@str2num,x,'UniformOutput',0), ...
     cellfun(@cellstr,units_Mini,'UniformOutput',0),'UniformOutput',0),'UniformOutput',0)';
 units_Mini_num=cellfun(@(x) cat(1,x{:}),units_Mini_num,'UniformOutput',0);
 
-%
+%%
 corrMatAll_Chewie=zeros(96,96,length(corrMat_Chewie));
 for n=1:length(corrMat_Chewie)
     [~,unitsIncluded,~]=intersect(1:96,units_Chewie_num{n});
     corrMatAll_Chewie(unitsIncluded,unitsIncluded,n)=corrMat_Chewie{n};
 end, clear n unitsIncluded
 figure, imagesc(mean(corrMatAll_Chewie,3))
-
+if ~exist('ind','var')
+    ind=1:size(corrMatAll_Chewie,3);
+end
+[r,c]=find(triu(mean(corrMatAll_Chewie(:,:,ind),3)>0.3,1)); clear ind
+Chewie_shuntedCh=unique([r; c]); clear r c
+%%
 corrMatAll_Mini=zeros(96,96,length(corrMat_Mini));
 for n=1:length(corrMat_Mini)
     [~,unitsIncluded,~]=intersect(1:96,units_Mini_num{n});
     corrMatAll_Mini(unitsIncluded,unitsIncluded,n)=corrMat_Mini{n};
 end, clear n unitsIncluded
 figure, imagesc(mean(corrMatAll_Mini,3))
+if ~exist('ind','var')
+    ind=1:size(corrMatAll_Mini,3);
+end
+[r,c]=find(triu(mean(corrMatAll_Mini(:,:,ind),3)>0.3,1)); clear ind
+Mini_shuntedCh=unique([r; c]); clear r c
+
 
