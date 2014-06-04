cded09a1748d3fc067521a13491c01d94673af29
Tucker Tomlinson <tuckertomlinson@gmail.com>
2014-06-04 20:39:54 +0000

    git-svn-id: http://citadel.physiology.northwestern.edu/svn/s1_analysis@1494 984b1e3c-1c57-0410-ae62-c18f90d3b645

---
+++ proc/Tucker/data processing helper functions/remove_sorting.m
@@ -0,0 +1,38 @@
+function bdf_nosort=remove_sorting(bdf)
+    %removes the unit sorting from a bdf. This function takes all spikes
+    %that are sorted into units and adds them to unit 0 (unsorted spikes)
+    
+    %copy the bdf for the output and erase the old spike data
+    bdf_nosort=bdf;
+    bdf_nosort.units=[];
+    
+    
+    %get a list of all the channels
+    channel_list=-1*ones(length(bdf.units),1);
+    for i=1:length(bdf.units)
+        channel_list(i)=bdf.units(1,i).id(1);
+    end
+    channel_list=sort(unique(channel_list));
+        
+    %for each channel 
+    for i=1:length(channel_list)
+        %find all the units
+        unit_list=[];
+        for j=1:length(bdf.units)
+            if bdf.units(1,j).id(1)==channel_list(i)
+                unit_list=[unit_list j];
+            elseif bdf.units(1,j).id(1)>channel_list(i)
+                continue
+            end
+        end
+        %put all units for the channel into unit 0 of the ouput bdf
+        bdf_nosort.units(1,i).id=[channel_list(i),0];
+        bdf_nosort.units(1,i).ts=[];
+        for k=1:length(unit_list)
+            bdf_nosort.units(1,i).ts=[bdf_nosort.units(1,i).ts;bdf.units(1,unit_list(k)).ts];
+        end
+        bdf_nosort.units(1,i).ts=sort(bdf_nosort.units(1,i).ts);
+    end
+    
+    
+end
