2d17719cba610335f4089308008a99cfef6c64b0
Raeed Chowdhury <raeed.chowdhury@gmail.com>
2014-07-11 21:08:32 +0000

    Added personal library files
    git-svn-id: http://citadel.physiology.northwestern.edu/svn/s1_analysis@1528 984b1e3c-1c57-0410-ae62-c18f90d3b645

---
+++ proc/raeed/lib/merge_spikes.m
@@ -0,0 +1,23 @@
+function file_names = merge_spikes(file_path,file_prefix)
+% MERGE_SPIKES Merge spikes from separate NEVs for easier sorting.
+%   MERGE_SPIKES(file_path,file_prefix) merges all NEVs in file_path,
+%   prefixed by file_prefix into one file, named <file_prefix>_spikes.nev,
+%   creating a metatags file for later unmerging. Returns file_names, a
+%   list of file names to separate merged sorted file into. Also saves file
+%   names into mat file.
+%
+%   See also UNMERGE_SPIKES.
+
+% Author: Raeed Chowdhury
+% Last Revision: 2014/07/02
+
+% get cell array of file names
+files = dir([file_path file_prefix '*.nev']);
+file_names = cell(size(files));
+for i = 1:length(file_names)
+    file_names{i} = files(i).name;
+end
+save([file_path file_prefix '_filenames.mat'],'file_names');
+
+% merge files and create metatags file
+processSpikesForSorting(file_path,file_prefix);
---
+++ proc/raeed/lib/remove_sorting.m
@@ -0,0 +1,35 @@
+function bdf_nosort=remove_sorting(bdf)
+    %removes the unit sorting from a bdf. This function takes all spikes
+    %that are sorted into units and adds them to unit 0 (unsorted spikes)
+    
+    %copy the bdf for the output and erase the old spike data
+    bdf_nosort=bdf;
+    bdf_nosort.units=[];
+    
+    
+    %get a list of all the valid channels
+    ul = unit_list(bdf,1);
+    channel_list=sort(unique(ul(:,1)));
+        
+    %for each channel 
+    for i=1:length(channel_list)
+        %find all the units
+        list=[];
+        for j=1:length(bdf.units)
+            if bdf.units(1,j).id(1)==channel_list(i)
+                list=[list j];
+            elseif bdf.units(1,j).id(1)>channel_list(i)
+                continue
+            end
+        end
+        %put all units for the channel into unit 0 of the ouput bdf
+        bdf_nosort.units(1,i).id=[channel_list(i),0];
+        bdf_nosort.units(1,i).ts=[];
+        for k=1:length(list)
+            bdf_nosort.units(1,i).ts=[bdf_nosort.units(1,i).ts;bdf.units(1,list(k)).ts];
+        end
+        bdf_nosort.units(1,i).ts=sort(bdf_nosort.units(1,i).ts);
+    end
+    
+    
+end
