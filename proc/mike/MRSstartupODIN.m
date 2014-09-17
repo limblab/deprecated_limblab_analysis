1f45c647bcd1f43465201c5fa461ff0d42edb340
Mike Scheid <michaelscheid2015@northwestern.edu>
2014-09-17 18:26:41 +0000

    Startup script for new computer Odin
    git-svn-id: http://citadel.physiology.northwestern.edu/svn/s1_analysis@1559 984b1e3c-1c57-0410-ae62-c18f90d3b645

--- proc/mike/MRSstartupODIN.m
+++ proc/mike/MRSstartupODIN.m
@@ -1,5 +1,7 @@
 addpath(genpath('C:\Users\M.R\Desktop\s1_analysis'))
-addpath(genpath('Z:\user_folders\Marc\circ_stat'))
+addpath(genpath('C:\Users\M.R\Desktop\ident'))
+addpath(genpath('Y:\user_folders\Marc\circ_stat'))
+addpath(genpath('Y:\user_folders\Marc\matlab code'))
 rmpath('C:\Documents and Settings\Administrator\My Documents\s1_analysis\proc\Tucker')
 cd('C:\Users\M.R\Desktop')
 dbstop if error
