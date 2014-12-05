06328e3f66552d84e46afd56e84a970ce421ef02
Ricardo Torres <ricardort@gmail.com>
2014-12-05 22:59:27 +0000

    Modified output of read_cmp.
    git-svn-id: http://citadel.physiology.northwestern.edu/svn/s1_analysis@1591 984b1e3c-1c57-0410-ae62-c18f90d3b645

--- lib/read_cmp.m
+++ lib/read_cmp.m
@@ -22,7 +22,7 @@ for iText = 1:length(text)
     temp_text = text{iText};
     temp_text = temp_text(1:strfind(text{iText},char(13))-1);
     temp_text = regexp(temp_text,'\t','split');
-    if length(temp_text)== 5 && str2double(temp_text{5})~=0 && ~strcmp(temp_text{1}(1),'/') && length(temp_text{2}>0)
+    if length(temp_text)>5 && length(temp_text{2}>0) && ~strcmp(temp_text{1}(1),'/') && str2double(temp_text{5})~=0
         switch temp_text{3}
             case 'A'
                 bank = 0;
@@ -31,12 +31,12 @@ for iText = 1:length(text)
             case 'C'
                 bank = 64;
         end
-        elec_map{end+1,1} = {str2double(temp_text{1}) str2double(temp_text{2}) bank+str2double(temp_text{4}) temp_text{5}};
+        elec_map(end+1,:) = {str2double(temp_text{1}) str2double(temp_text{2}) bank+str2double(temp_text{4}) temp_text{5}};
     end
 end
 all_num = 1;
 for iElec = 1:size(elec_map,1)
-    if isnan(str2double(elec_map{iElec}{4}))
+    if isnan(str2double(elec_map(iElec,4)))
         all_num = 0;
     end
 end
@@ -45,7 +45,7 @@ if all_num
     elec_map_temp = elec_map;
     elec_map = [];
     for iElec = 1:size(elec_map_temp,1)
-        elec_map(end+1,:) = [elec_map_temp{iElec}{1} elec_map_temp{iElec}{2}...
-            elec_map_temp{iElec}{3} str2double(elec_map_temp{iElec}{4})];
+        elec_map(end+1,:) = [elec_map_temp(iElec,1) elec_map_temp(iElec,2)...
+            elec_map_temp(iElec,3) str2double(elec_map_temp(iElec,4))];
     end
 end
