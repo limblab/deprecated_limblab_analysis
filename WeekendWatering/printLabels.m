2cc38a40a7cde7810869d78ce17fd3a3051d4863
Ricardo Torres <ricardort@gmail.com>
2015-01-21 20:11:42 +0000

    git-svn-id: http://citadel.physiology.northwestern.edu/svn/s1_analysis@1627 984b1e3c-1c57-0410-ae62-c18f90d3b645

--- WeekendWatering/printLabels.m
+++ WeekendWatering/printLabels.m
@@ -1,5 +1,5 @@
 WaterSheetFile = '\\citadel\limblab\lab_folder\Lab-Wide Animal Info\WeekendWatering\';
-WaterSheetFile = [WaterSheetFile 'Weekend water and food Miller-Slutzky 2015-01-10.xlsx'];
+WaterSheetFile = [WaterSheetFile 'Weekend water and food Miller-Slutzky 2015-01-17.xlsx'];
 WeekendWateringFile = '\\citadel\limblab\lab_folder\Lab-Wide Animal Info\WeekendWatering\MonkeyWaterData.xlsx';
 [~,WeekendWatering] = xlsread(WeekendWateringFile,3);
 existing_watering_weekends = datenum(WeekendWatering(2,3:end));
