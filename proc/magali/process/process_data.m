457592943509862ee64405513dc280c1f2451f3a
Magali Hanot <magali.hanot@gmail.com>
2015-03-09 23:27:20 +0000

    git-svn-id: http://citadel.physiology.northwestern.edu/svn/s1_analysis@1706 984b1e3c-1c57-0410-ae62-c18f90d3b645

--- proc/magali/process/process_data.m
+++ proc/magali/process/process_data.m
@@ -1,7 +1,7 @@
 %script to set input data and execute data processing
 %% process PDs using Raeed/Tucker functions
-folderpath='C:\Users\limblab\Documents\Matlab test';
-input_data.prefix='Chips_20150303_RW_tucker_004';
+folderpath='Z:\Han_13B1\Processed\experiment_20141202_RW';
+input_data.prefix='Han_20141202_raeed_tucker_3aB1B2_2A2B1_001-s - Shortcut';
 input_data.only_sorted=1
 function_name='get_PDs';
 input_data.labnum=6;
@@ -9,8 +9,14 @@ input_data.do_unit_pds=1;
 input_data.do_electrode_pds=1;
 data_struct = run_data_processing(function_name,folderpath,input_data);
 %% check for electrode stability
-folderpath='C:\Users\limblab\Documents\Matlab test\electrode stability';
+folderpath='Z:\Chips_12H1\processed\20150220-0303_electrode_stability';
 function_name='compute_electrode_stability';
 input_data.num_channels=96;
 input_data.min_moddepth=2*10^-4;
 electrode_stability=run_data_processing(function_name,folderpath,input_data);
+%% check for unit stability
+folderpath='Z:\Chips_12H1\processed\20150220-0303_unit_stability';
+function_name='compute_unit_stability';
+input_data.num_channels=96;
+input_data.min_moddepth=2*10^-4;
+unit_stability=run_data_processing(function_name,folderpath,input_data);
