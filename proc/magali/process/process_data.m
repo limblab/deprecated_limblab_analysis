d111ee1f696fe96750071127c7b2e2742270a512
Magali Hanot <magali.hanot@gmail.com>
2015-03-12 20:20:55 +0000

    git-svn-id: http://citadel.physiology.northwestern.edu/svn/s1_analysis@1713 984b1e3c-1c57-0410-ae62-c18f90d3b645

--- proc/magali/process/process_data.m
+++ proc/magali/process/process_data.m
@@ -9,14 +9,14 @@ input_data.do_unit_pds=1;
 input_data.do_electrode_pds=1;
 data_struct = run_data_processing(function_name,folderpath,input_data);
 %% check for electrode stability
-folderpath='Z:\Chips_12H1\processed\20150220-0303_electrode_stability';
+folderpath='Z:\Han_13B1\Processed\week_0309_0313_stability_RW\area_2 bank_A2\electrode_stability';
 function_name='compute_electrode_stability';
-input_data.num_channels=96;
+input_data.num_channels=32;
 input_data.min_moddepth=2*10^-4;
 electrode_stability=run_data_processing(function_name,folderpath,input_data);
 %% check for unit stability
-folderpath='Z:\Chips_12H1\processed\20150220-0303_unit_stability';
+folderpath='Z:\Han_13B1\Processed\week_0309_0313_stability_RW\area_2 bank_A2\unit_stability';
 function_name='compute_unit_stability';
-input_data.num_channels=96;
+input_data.num_channels=32;
 input_data.min_moddepth=2*10^-4;
 unit_stability=run_data_processing(function_name,folderpath,input_data);
