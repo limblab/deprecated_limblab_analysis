340381d3f813c60361a7dad062139845b6022a1a
Magali Hanot <magali.hanot@gmail.com>
2015-03-13 22:34:58 +0000

    git-svn-id: http://citadel.physiology.northwestern.edu/svn/s1_analysis@1717 984b1e3c-1c57-0410-ae62-c18f90d3b645

--- proc/magali/process/process_data.m
+++ proc/magali/process/process_data.m
@@ -9,7 +9,7 @@ input_data.do_unit_pds=1;
 input_data.do_electrode_pds=1;
 data_struct = run_data_processing(function_name,folderpath,input_data);
 %% check for electrode stability
-folderpath='Z:\Han_13B1\Processed\week_0309_0313_stability_RW\area_2 bank_A2\electrode_stability';
+folderpath='Z:\Han_13B1\Processed\week_0309_0313_stability_RW\area_3 bank_B2\electrode_stability';
 function_name='compute_electrode_stability';
 input_data.num_channels=32;
 input_data.min_moddepth=2*10^-4;
