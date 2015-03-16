d906e8c04010485a5195a61a71909dc3aa5de794
Magali Hanot <magali.hanot@gmail.com>
2015-03-16 20:08:51 +0000

    git-svn-id: http://citadel.physiology.northwestern.edu/svn/s1_analysis@1718 984b1e3c-1c57-0410-ae62-c18f90d3b645

---
+++ proc/magali/process/Process_list_of_PD_files.m
@@ -0,0 +1,121 @@
+%% process PDs using Raeed/Tucker functions
+close all
+clear
+function_name='get_PDs';
+input_data.labnum=6;
+input_data.do_unit_pds=1;
+input_data.do_electrode_pds=1;
+input_data.only_sorted=1;
+
+folderpath='Z:\Han_13B1\Processed\experiment_20150309_RW\experiment_20150309_RW_area2bankA2';
+input_data.prefix='Han_20150309_RW_Magali_2A2_001-s';
+data_struct = run_data_processing(function_name,folderpath,input_data);
+%% process PDs using Raeed/Tucker functions
+close all
+clear
+function_name='get_PDs';
+input_data.labnum=6;
+input_data.do_unit_pds=1;
+input_data.do_electrode_pds=1;
+input_data.only_sorted=1;
+
+folderpath='Z:\Han_13B1\Processed\experiment_20150309_RW\experiment_20150309_RW_area3bankB2\002';
+input_data.prefix='Han_20150309_RW_Magali_3B2_002-s';
+data_struct = run_data_processing(function_name,folderpath,input_data);
+%% process PDs using Raeed/Tucker functions
+clear
+close all
+clear
+function_name='get_PDs';
+input_data.labnum=6;
+input_data.do_unit_pds=1;
+input_data.do_electrode_pds=1;
+input_data.only_sorted=1;
+
+folderpath='Z:\Han_13B1\Processed\experiment_20150309_RW\experiment_20150309_RW_area3bankB2\003';
+input_data.prefix='Han_20150309_RW_Magali_3B2_003-s';
+data_struct = run_data_processing(function_name,folderpath,input_data);
+%% process PDs using Raeed/Tucker functions
+close all
+clear
+function_name='get_PDs';
+input_data.labnum=6;
+input_data.do_unit_pds=1;
+input_data.do_electrode_pds=1;
+input_data.only_sorted=1;
+
+folderpath='Z:\Han_13B1\Processed\experiment_20150310_RW\area_2 bank_a2';
+input_data.prefix='Han_20150310_RW_Magali_2A2_001-s';
+data_struct = run_data_processing(function_name,folderpath,input_data);
+%% process PDs using Raeed/Tucker functions
+close all
+clear
+function_name='get_PDs';
+input_data.labnum=6;
+input_data.do_unit_pds=1;
+input_data.do_electrode_pds=1;
+input_data.only_sorted=1;
+
+folderpath='Z:\Han_13B1\Processed\experiment_20150310_RW\area_3 bank_b2 recording_001';
+input_data.prefix='Han_20150310_RW_Magali_3B2_001-s';
+data_struct = run_data_processing(function_name,folderpath,input_data);
+%% process PDs using Raeed/Tucker functions
+close all
+clear
+function_name='get_PDs';
+input_data.labnum=6;
+input_data.do_unit_pds=1;
+input_data.do_electrode_pds=1;
+input_data.only_sorted=1;
+
+folderpath='Z:\Han_13B1\Processed\experiment_20150310_RW\area_3 bank_b2 recording_002';
+input_data.prefix='Han_20150310_RW_Magali_3B2_002-s';
+data_struct = run_data_processing(function_name,folderpath,input_data);
+%% process PDs using Raeed/Tucker functions
+close all
+clear
+function_name='get_PDs';
+input_data.labnum=6;
+input_data.do_unit_pds=1;
+input_data.do_electrode_pds=1;
+input_data.only_sorted=1;
+
+folderpath='Z:\Han_13B1\Processed\experiment_20150311_RW\area_2 bank_A2';
+input_data.prefix='Han_20150311_RW_Magali_2A2_001-s';
+data_struct = run_data_processing(function_name,folderpath,input_data);
+%% process PDs using Raeed/Tucker functions
+close all
+clear
+function_name='get_PDs';
+input_data.labnum=6;
+input_data.do_unit_pds=1;
+input_data.do_electrode_pds=1;
+input_data.only_sorted=1;
+
+folderpath='Z:\Han_13B1\Processed\experiment_20150311_RW\area_3 bank_B2';
+input_data.prefix='Han_20150311_RW_Magali_3B2_001-s';
+data_struct = run_data_processing(function_name,folderpath,input_data);
+%% process PDs using Raeed/Tucker functions
+close all
+clear
+function_name='get_PDs';
+input_data.labnum=6;
+input_data.do_unit_pds=1;
+input_data.do_electrode_pds=1;
+input_data.only_sorted=1;
+
+folderpath='Z:\Han_13B1\Processed\experiment_20150312_RW\area_2 bank_A2';
+input_data.prefix='Han_20150312_RW_Magali_area2_A2_001-s';
+data_struct = run_data_processing(function_name,folderpath,input_data);
+%% process PDs using Raeed/Tucker functions
+close all
+clear
+function_name='get_PDs';
+input_data.labnum=6;
+input_data.do_unit_pds=1;
+input_data.do_electrode_pds=1;
+input_data.only_sorted=1;
+
+folderpath='Z:\Han_13B1\Processed\experiment_20150312_RW\area_3 bank_B2';
+input_data.prefix='Han_20150312_RW_Magali_area3_B2_001-s';
+data_struct = run_data_processing(function_name,folderpath,input_data);
