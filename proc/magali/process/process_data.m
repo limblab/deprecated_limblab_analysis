64841fcaa691a78e22658c2cd2981754122818f0
Magali Hanot <magali.hanot@gmail.com>
2015-03-24 19:49:22 +0000

    git-svn-id: http://citadel.physiology.northwestern.edu/svn/s1_analysis@1727 984b1e3c-1c57-0410-ae62-c18f90d3b645

--- proc/magali/process/process_data.m
+++ proc/magali/process/process_data.m
@@ -1,7 +1,7 @@
 %script to set input data and execute data processing
 %% process PDs using Raeed/Tucker functions
-folderpath='Z:\Han_13B1\Processed\experiment_20150310_RW\area_3 bank_b2 recording_001';
-input_data.prefix='Han_20150310_RW_Magali_3B2_001-s';
+folderpath='Z:\Han_13B1\Processed\experiment_20150320_RW';
+input_data.prefix='Han_20150320_RW_Magali_area2_A2_after_stim_session_002-s';
 input_data.only_sorted=1
 function_name='get_PDs';
 input_data.labnum=6;
@@ -9,8 +9,8 @@ input_data.do_unit_pds=1;
 input_data.do_electrode_pds=1;
 data_struct = run_data_processing(function_name,folderpath,input_data);
 %% check for electrode stability
-folderpath='Z:\Han_13B1\Processed\week_0309_0313_stability_RW\area_3 bank_B2\electrode_stability';
-function_name='compute_electrode_stability';
+folderpath='Z:\Han_13B1\Processed\week_0309_0313_stability_RW\area_2 bank_A2\electrode_stability';
+function_name='compute_electrode_stability_plasticity';
 input_data.num_channels=32;
 input_data.min_moddepth=2*10^-4;
 electrode_stability=run_data_processing(function_name,folderpath,input_data);
--- proc/magali/process/process_for_sorting.m
+++ proc/magali/process/process_for_sorting.m
@@ -1,8 +1,8 @@
 %% strip digital data from NEV
-basepath='Z:\Han_13B1\Processed\experiment_20150310_RW\area_3 bank_b2 recording_001';
-basefilename='Han_20150310_RW_Magali_3B2_001.nev';
-nodigitalfilename='Han_20150310_RW_Magali_3B2_001_nodigital.nev';
-spikelessfilename='Han_20150310_RW_Magali_3B2_001_nospikes.mat';
+basepath='Z:\Han_13B1\Processed\experiment_20150320_RW';
+basefilename='Han_20150320_RW_Magali_area2_A2_after_stim_session_002.nev';
+nodigitalfilename='Han_20150320_RW_Magali_area2_A2_after_stim_session_002_nodigital.nev';
+spikelessfilename='Han_20150320_RW_Magali_area2_A2_after_stim_session_002_nospikes.mat';
 
 
 %open data
@@ -14,10 +14,10 @@ saveNEVOnlyDigital(NEV,[basepath '\' spikelessfilename]);
 
 
 %% save append digital data back on and re-save
-basepath='Z:\Han_13B1\Processed\experiment_20150310_RW\area_3 bank_b2 recording_001';
-sortedfilename='Han_20150310_RW_Magali_3B2_001_nodigital.nev';
-spikelessfilename='Han_20150310_RW_Magali_3B2_001_nospikes.mat';
-processfilename='Han_20150310_RW_Magali_3B2_001-s.nev';
+basepath='Z:\Han_13B1\Processed\experiment_20150320_RW';
+sortedfilename='Han_20150320_RW_Magali_area2_A2_after_stim_session_002_nodigital.nev';
+spikelessfilename='Han_20150320_RW_Magali_area2_A2_after_stim_session_002_nospikes.mat';
+processfilename='Han_20150320_RW_Magali_area2_A2_after_stim_session_002-s.nev';
 
 oldnevdata=load([basepath '\' spikelessfilename]);
 sortednev=openNEV([basepath '\' sortedfilename],'nosave','nomat','report');
