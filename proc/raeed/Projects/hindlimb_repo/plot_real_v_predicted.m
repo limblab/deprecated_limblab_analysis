48b5cf763bebf6160a27228844c977fa2c3573eb
Raeed Chowdhury <raeed.chowdhury@gmail.com>
2015-05-01 16:11:29 +0000

    Removed projects folder from proc/raeed/
    git-svn-id: http://citadel.physiology.northwestern.edu/svn/s1_analysis@1778 984b1e3c-1c57-0410-ae62-c18f90d3b645

--- proc/raeed/Projects/hindlimb_repo/plot_real_v_predicted.m
+++
@@ -1,6 +0,0 @@
-function plot_real_v_predicted(real_act, model, position)
-
-%find predicted value
-pred_act = predict(model,position);
-
-plot(real_act,pred_act,'o',real_act,real_act,'-')
--- proc/raeed/Projects/two_workspace/README.md
+++
@@ -1,4 +0,0 @@
-two_workspace
-=============
-
-Code to analyze neural data taken in two separate workspaces
