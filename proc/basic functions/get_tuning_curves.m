ad0616aaadf72821a801f60a27b75dd6d1d01f7d
Raeed Chowdhury <raeed.chowdhury@gmail.com>
2015-05-01 16:01:22 +0000

    added functionality to get_tuning_curves
    git-svn-id: http://citadel.physiology.northwestern.edu/svn/s1_analysis@1777 984b1e3c-1c57-0410-ae62-c18f90d3b645

--- proc/basic functions/get_tuning_curves.m
+++ proc/basic functions/get_tuning_curves.m
@@ -1,2 +1,27 @@
 function [figure_handles, output_data]=get_tuning_curves(folder,options)
-% Nothing here yet.
+% GET_TUNING_CURVES
+
+% if behaviors is in options, use it
+if(~isfield(options,'behaviors'))
+    error('behaviors not in options; haven''t yet implemented parsing in function')
+else
+    behaviors = options.behaviors;
+end
+
+% find velocities and directions
+armdata = behaviors.armdata;
+vel = armdata([armdata.name=='vel']).data;
+dir = atan2(vel(:,2),vel(:,1));
+
+% bin directions
+dir_bins = round(dir/(pi/4))*(pi/4);
+
+% average firing rates for directions
+bins = -135:45:180;
+for i = 1:length(bins)
+    binned_FR(i,:) = sum(behaviors.FR(dir_bins==bins(i),:));
+end
+
+% plot first  40 neurons
+for i=1:length(behaviors.FR)
+end
