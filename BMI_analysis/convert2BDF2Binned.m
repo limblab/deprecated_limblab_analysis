fcf1823bff919d96916de94790b81351bdd7da9c
Christian Ethier <christian.ethier@gmail.com>
2015-06-11 23:12:24 +0000

    ignore jumps by default
    git-svn-id: http://citadel.physiology.northwestern.edu/svn/s1_analysis@1842 984b1e3c-1c57-0410-ae62-c18f90d3b645

--- BMI_analysis/convert2BDF2Binned.m
+++ BMI_analysis/convert2BDF2Binned.m
@@ -26,7 +26,7 @@ end
     
 fprintf('Converting %s to BDF structure...\n',cerebus_filename);
 % BDF = get_cerebus_data(cerebus_filename,'verbose');
-BDF = get_nev_mat_data(cerebus_filename,'verbose');
+BDF = get_nev_mat_data(cerebus_filename,'verbose','ignore_jumps');
 
 fprintf('Saving BDF structure %s...\n',BDF_filename);
 save([datapath filesep BDF_filename], 'BDF');
