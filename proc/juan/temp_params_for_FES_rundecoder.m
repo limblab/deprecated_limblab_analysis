34cf54af683c7cedb2af8958dbd95b72d1785e22
Juan Gallego <jotagalabe@gmail.com>
2015-08-27 23:00:15 +0000

    irrelevant changes
    git-svn-id: http://citadel.physiology.northwestern.edu/svn/s1_analysis@1939 984b1e3c-1c57-0410-ae62-c18f90d3b645

--- proc/juan/temp_params_for_FES_rundecoder.m
+++ proc/juan/temp_params_for_FES_rundecoder.m
@@ -1,13 +1,13 @@
-clear all, close all
+clear all, close all, clc
 
 bmi_params = bmi_params_defaults;
 
-%params.neuron_decoder   = '/Users/juangallego/Documents/NeuroPlast/Data/Jango/Decoders/20150320_Jango_WF_001_binned_Decoder.mat';
-params.neuron_decoder   = 'E:\Data-lab1\12A1-Jango\SavedFilters\Jango_WF_2014_05_29_HC_001_N2E_Decoder.mat';
+params.neuron_decoder   = '/Users/juangallego/Documents/NeuroPlast/Data/Jango/Decoders/20150320_Jango_WF_001_binned_Decoder.mat';
+%params.neuron_decoder   = 'E:\Data-lab1\12A1-Jango\SavedFilters\Jango_WF_2014_05_29_HC_001_N2E_Decoder.mat';
 params.output           = 'stimulator';
 params.mode             = 'emg_only';
-%params.offline_data     = '/Users/juangallego/Documents/NeuroPlast/Data/Jango/BinnedData/behavior plasticity/20150320_Jango_WF_001_binned.mat';
-params.offline_data     = 'E:\Data-lab1\12A1-Jango\BinnedData\20140529\Jango_WF_2014_05_29_HC_001.mat';
+params.offline_data     = '/Users/juangallego/Documents/NeuroPlast/Data/Jango/BinnedData/behavior plasticity/20150320_Jango_WF_001_binned.mat';
+%params.offline_data     = 'E:\Data-lab1\12A1-Jango\BinnedData\20140529\Jango_WF_2014_05_29_HC_001.mat';
 params.n_emgs           = 12;
 params.online           = false; % true if you want to read from the monkey
 params.display_plots    = false;
