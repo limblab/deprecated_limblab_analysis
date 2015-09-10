e2d65ac2fc498026a5d9f380b29cdb31390cc047
Raeed Chowdhury <raeed.chowdhury@gmail.com>
2015-09-10 05:23:28 +0000

    Finished split_workspaces.m, though untested
    git-svn-id: http://citadel.physiology.northwestern.edu/svn/s1_analysis@1950 984b1e3c-1c57-0410-ae62-c18f90d3b645

--- proc/raeed/Projects/two_workspace/split_workspaces.m
+++ proc/raeed/Projects/two_workspace/split_workspaces.m
@@ -3,6 +3,11 @@ function [bdf_DL,bdf_PM,bdf] = split_workspaces(folder, options)
 % distal-lateral workspace and a proximal-medial workspace to compare the
 % tunings in the two.
 
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% CHECK IF OUTPUT BEHAVIORS HAS A SET OF ZEROS WHERE THERE SHOULD BE
+% NOTHING
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 if(~isfield(options,'bdf'))
     if(folder(end)~=filesep)
         folder = [folder filesep];
@@ -11,15 +16,74 @@ if(~isfield(options,'bdf'))
 else
     bdf = options.bdf;
 end
-bdf_DL = bdf;
-bdf_PM = bdf;
 
 pos = bdf.pos;
 
 DL_ind = pos(:,2)<mean(pos(:,2)) & pos(:,3)>mean(pos(:,3));
 PM_ind = pos(:,2)>mean(pos(:,2)) & pos(:,3)<mean(pos(:,3));
 
-% find starts and stops of all reaches in each workspace
-% save only those reach kinematics into new, copied bdfs
-% For each unit, for each DL reach and PM reach, split up spikes into
-% different bdfs
+bdf_DL = extract_workspace(bdf,DL_ind);
+bdf_PM = extract_workspace(bdf,PM_ind);
+
+end
+
+function [bdf_new,iStart,iStop] = extract_workspace(bdf,ind)
+    bdf_new = bdf;
+    t = bdf.pos(:,1);
+    
+    % find starts and stops of all reaches in each workspace
+    iStart = find(diff(ind)>0);
+    iStop  = find(diff(ind)<0);
+    % A little Kluge to eleminate any partial trajectories at the beginning or
+    % end of the file
+    if iStart(1) > iStop(1)
+        iStop = iStop(2:end);
+    end
+
+    if length(iStart) > length(iStop)
+        iStart = iStart(1:length(iStop));
+    end
+    
+    % discard some paths less than minimum length
+    lMin = 3;
+    keepers = true(size(iStart));
+    for i = 1:length(keepers)
+        snip = bdf_new.pos(iStart(i):iStop(i), 2:3);
+
+        % Reject paths that are too short
+        steps = diff(snip);
+        len = sum(sqrt(steps(:,1).^2+steps(:,2).^2));
+        keepers(i) = keepers(i) & len > lMin;
+    end
+    
+    % Dump all the rejected trajectories
+    iStart = iStart(keepers);
+    iStop = iStop(keepers);
+    
+    % compile new fields for bdf_new
+    fr = zeros(length(iStart),length(unit_list(bdf)));
+    new_pos = [];
+    new_vel = [];
+    new_acc = [];
+    new_force = [];
+    for i=1:length(iStart)
+        new_pos = [new_pos;bdf.pos(iStart:iStop,:)];
+        new_vel = [new_vel;bdf.vel(iStart:iStop,:)];
+        new_acc = [new_acc;bdf.acc(iStart:iStop,:)];
+        new_force = [new_force;bdf.force(iStart:iStop,:)];
+    end
+    bdf_new.pos = new_pos;
+    bdf_new.vel = new_vel;
+    bdf_new.acc = new_acc;
+    bdf_new.force = new_force;
+    
+    % get new spikes
+    for uid = 1:length(bdf.units)
+        spikes = [];
+        s = bdf.units(uid).ts;
+        for i=1:length(iStart)
+            spikes = [spikes;s(s < t(iStop(i)) & s > t(iStart(i)))];
+        end
+        bdf_new.units(uid).ts = spikes;
+    end % foreach unit
+end
