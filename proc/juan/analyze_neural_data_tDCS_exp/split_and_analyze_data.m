cd5abaf1a96195a76b6a560938257bc3470d3507
Juan Gallego <jotagalabe@gmail.com>
2015-09-28 04:50:51 +0000

    added code to analyze LFPs. Still on the works
    git-svn-id: http://citadel.physiology.northwestern.edu/svn/s1_analysis@1978 984b1e3c-1c57-0410-ae62-c18f90d3b645

--- proc/juan/analyze_neural_data_tDCS_exp/split_and_analyze_data.m
+++ proc/juan/analyze_neural_data_tDCS_exp/split_and_analyze_data.m
@@ -72,7 +72,6 @@ for i = 1:nbr_files
                 load( bin_file_name );
                 binned_data = binnedData; clear binnedData;
             else
-
                 binned_data = convert2BDF2Binned( [folder_name filesep my_file.name] );
             end
     end
@@ -81,6 +80,11 @@ for i = 1:nbr_files
     binned_data.neuronIDs   = binned_data.neuronIDs(sad_params.chosen_neurons,:);
     binned_data.spikeratedata   = binned_data.spikeratedata(:,sad_params.chosen_neurons);
     
+    % 'chop' the LFPs to the chosen channels
+    if sad_params.lfp
+        BDF.lfp.lfpnames    = BDF.lfp.lfpnames(sad_params.chosen_neurons);
+        BDF.lfp.data        = BDF.lfp.data(:,[1 (sad_params.chosen_neurons+1)]);
+    end
     
     % ---------------------------------------------------------------------
     % The calculations themselves
@@ -100,7 +104,7 @@ for i = 1:nbr_files
             % behavior
         case 'emg'
             % Discard periods of neural data based on the EMG level
-
+            
         case 'pos'
             % rectify the data, if specified in
             % 'sad_params.rectify_behavior'
@@ -112,7 +116,7 @@ for i = 1:nbr_files
             % (force)
             
             % create a vector with the threshold for each axis
-            switch sad_params.thr_statistic 
+            switch sad_params.thr_statistic
                 case 'none'
                     threshold = sad_params.thr_behavior*ones(1,size(binned_data.cursorposbin,2));
                 case 'std'
@@ -121,7 +125,7 @@ for i = 1:nbr_files
             end
             
             % find bins where rectified cursor position is above the
-            % threshold 
+            % threshold
             pos_above_thr = [];
             for ii = 1:size(binned_data.cursorposbin,2)
                 pos_above_thr = [pos_above_thr; ...
@@ -139,23 +143,23 @@ for i = 1:nbr_files
             % Discard periods of neural data based on the cursor velocity
             
             % create a vector with the threshold for each axis
-            switch sad_params.thr_statistic 
+            switch sad_params.thr_statistic
                 case 'none'
                     threshold = sad_params.thr_behavior*ones(1,size(binned_data.cursorposbin,2));
                 case 'std'
                     threshold = mean(binned_data.velocbin)+...
                         sad_params.thr_behavior*std(binned_data.velocbin);
             end
-                        
+            
             % find bins where rectify cursor velocity is above the
-            % threshold 
+            % threshold
             pos_above_thr = [];
             for ii = 1:size(binned_data.cursorposbin,2)
                 pos_above_thr = [pos_above_thr; ...
                     find( abs(binned_data.velocbin(:,ii)) > threshold(ii) )]; %#ok<AGROW>
             end
             pos_above_thr = unique(pos_above_thr);
-        
+            
         case 'word'
             % find time at which the selected word occurs
             word_t = binned_data.words( binned_data.words(:,2) == hex2dec(num2str(sad_params.word_hex)), 1 );
@@ -166,15 +170,14 @@ for i = 1:nbr_files
             win_word_bins = round(sad_params.win_word/1000/bin_length);
             
             % take the window of data defined in 'sad_params.win_word'
-            % ... but before check if some of the windows are out of
+            % ... but before check if some of the windows are outside
             % boundaries
-            
             word_bin( (word_bin+win_word_bins(1) ) < 0 ) = [];
             word_bin( (word_bin+win_word_bins(2) ) > binned_data.timeframe(end)/bin_length ) = [];
             % Warn when two targets are too close (closer than the length
-            % of the specified window) 
+            % of the specified window)
             if ~isempty( word_bin(diff(word_bin)<0) )
-                disp('warning two targets are too close!')
+                disp('warning: two targets are too close!')
             end
             
             nbr_words = length(word_bin);
@@ -192,7 +195,7 @@ for i = 1:nbr_files
             
             
             % Store the firing rate of each neuron during each window...
-    
+            
             % retrieve number neural channels
             nbr_neural_ch   = size(binned_data.spikeratedata,2);
             
@@ -203,13 +206,51 @@ for i = 1:nbr_files
             end
             for ii = 1:nbr_neural_ch
                 for iii = 1:nbr_words
-                    neural_activity.firing_rate_in_win{ii}(iii,:) = binned_data.spikeratedata(analysis_windows(iii,:),ii);
+                    neural_activity.firing_rate_in_win{ii}(iii,:) = ...
+                        binned_data.spikeratedata(analysis_windows(iii,:),ii);
                 end
+            end
+            
+            % If analysing LFPs, 'chop' them to the desired analysis
+            % windows
+            if sad_params.lfp
+                
+                % find the sample at which each word ends
+                word_sample_lfp = arrayfun( @(x) find(BDF.lfp.data(:,1) > x,1), word_t);
+                
+                % convert the desired window from time to number of samples
+                win_word_samples = sad_params.win_word/1000*BDF.lfp.lfpfreq;
+                
+                % take the window of data defined in 'sad_params.win_word'
+                word_sample_lfp( (word_sample_lfp+win_word_samples(1) ) < 0 ) = [];
+                word_sample_lfp( (word_sample_lfp+win_word_samples(2) ) > ...
+                    BDF.lfp.data(end,1)*1000 ) = [];
+                
+                % return an error if the number of words in the LFPs are
+                % different to the spikes
+                if nbr_words ~= length(word_sample_lfp)
+                    error('inconsistency in LFP and spike windows');
+                end
+                
+                samples_per_word = abs(diff(win_word_samples))+1;
+                analysis_windows_lfp = zeros(nbr_words,samples_per_word);
+                for ii = 1:nbr_words
+                    analysis_windows_lfp(ii,:) = word_sample_lfp(ii) + (win_word_samples(1):win_word_samples(2));
+                end
+                
+                % create matrix with pos_above_threshold_lfp, which will
+                % contain all the LFP samples that are not in the windows
+                % to analyze
+                
+                pos_above_thr_lfp = 1:size(BDF.lfp.data,1);
+                pos_above_thr_lfp = pos_above_thr_lfp( find(~ismember(pos_above_thr_lfp,analysis_windows_lfp)) );
             end
             
             % store analysis windows
             neural_activity.analysis_windows        = analysis_windows;
     end
+    
+  
     
     % discard those bins in the relevant variables
     if exist('pos_above_thr','var')
@@ -221,14 +262,16 @@ for i = 1:nbr_files
         binned_data.velocbin(pos_above_thr,:)       = [];
         binned_data.accelbin(pos_above_thr,:)       = [];
         binned_data.spikeratedata(pos_above_thr,:)  = [];
+        
+        if sad_params.lfp
+           BDF.lfp.data(pos_above_thr_lfp,:)        = []; 
+        end
     end
     
     % store bin length and the analysis windows
     binned_data.bin_length                          = bin_length;
     
-    
-    % ToDo: see what to do with the words
-    
+        
     
     % 3. Calculate the mean firing rate for the specified neurons in each
     % window of duration 'sad_params.win_duration'
@@ -265,6 +308,12 @@ for i = 1:nbr_files
         for ii = 1:nbr_neural_ch
             neural_activity.mean_firing_rate_in_win(:,ii) = mean(neural_activity.firing_rate_in_win{ii},1);
             neural_activity.std_firing_rate_in_win(:,ii) = std(neural_activity.firing_rate_in_win{ii},1);
+        end
+        
+        % If want to look at the LFPs in the words, calculate the amplitude
+        % of the LFPs in each channel and the PSD
+        if sad_params.lfp
+            psd_lfp( BDF.lfp, sad_params.win_word, word_t )
         end
     end
     
