b69e4b1bb52605b6c5d9f0ba44df54df106c1885
Tucker Tomlinson <tuckertomlinson@gmail.com>
2015-11-10 23:41:16 +0000

    revised to handle error associated with double files eparators
    git-svn-id: http://citadel.physiology.northwestern.edu/svn/s1_analysis@2009 984b1e3c-1c57-0410-ae62-c18f90d3b645

--- lib/run_data_processing.m
+++ lib/run_data_processing.m
@@ -44,8 +44,8 @@ function data_struct = run_data_processing(main_function_name,target_directory,v
 
     %% make directory structure if it does not already exist
     
-    if exist(strcat(target_directory,'\Code'),'file')~=7
-        mkdir(strcat(target_directory,'\Code'))
+    if exist(strcat(target_directory,'Code'),'file')~=7
+        mkdir(strcat(target_directory,'Code'))
     else
         warning('RUN_DATA_PROCESSING:FOLDER_EXISTS','A folder with processed data already exists, you may lose data if you continue')
         yesno=questdlg('The target folder already exists. If you continue data may be lost. Do you want to continue?','Folder already exists','Yes','No','No');
@@ -53,29 +53,29 @@ function data_struct = run_data_processing(main_function_name,target_directory,v
             return
         end
     end
-    if exist(strcat(target_directory,'\Raw_Figures'),'file')~=7
-        mkdir(strcat(target_directory,'\Raw_Figures'))
+    if exist(strcat(target_directory,'Raw_Figures'),'file')~=7
+        mkdir(strcat(target_directory,'Raw_Figures'))
     end
-    if exist(strcat(target_directory,'\Raw_Figures\PDF'),'file')~=7
-        mkdir(strcat(target_directory,'\Raw_Figures\PDF'))
+    if exist(strcat(target_directory,'Raw_Figures\PDF'),'file')~=7
+        mkdir(strcat(target_directory,'Raw_Figures\PDF'))
     end
-    if exist(strcat(target_directory,'\Raw_Figures\FIG'),'file')~=7
-        mkdir(strcat(target_directory,'\Raw_Figures\FIG'))
+    if exist(strcat(target_directory,'Raw_Figures\FIG'),'file')~=7
+        mkdir(strcat(target_directory,'Raw_Figures\FIG'))
     end
-    if exist(strcat(target_directory,'\Raw_Figures\EPS'),'file')~=7
-        mkdir(strcat(target_directory,'\Raw_Figures\EPS'))
+    if exist(strcat(target_directory,'Raw_Figures\EPS'),'file')~=7
+        mkdir(strcat(target_directory,'Raw_Figures\EPS'))
     end
-    if exist(strcat(target_directory,'\Raw_Figures\PNG'),'file')~=7
-        mkdir(strcat(target_directory,'\Raw_Figures\PNG'))
+    if exist(strcat(target_directory,'Raw_Figures\PNG'),'file')~=7
+        mkdir(strcat(target_directory,'Raw_Figures\PNG'))
     end
-    if exist(strcat(target_directory,'\Edited_Figures'),'file')~=7
-        mkdir(strcat(target_directory,'\Edited_Figures'))
+    if exist(strcat(target_directory,'Edited_Figures'),'file')~=7
+        mkdir(strcat(target_directory,'Edited_Figures'))
     end
-    if exist(strcat(target_directory,'\Output_Data'),'file')~=7
-        mkdir(strcat(target_directory,'\Output_Data'))
+    if exist(strcat(target_directory,'Output_Data'),'file')~=7
+        mkdir(strcat(target_directory,'Output_Data'))
     end
-    if exist(strcat(target_directory,'\Input_Data'),'file')~=7
-        mkdir(strcat(target_directory,'\Input_Data'))
+    if exist(strcat(target_directory,'Input_Data'),'file')~=7
+        mkdir(strcat(target_directory,'Input_Data'))
     end
 
     %% save all the custom functions in the analysis to the code folder.
@@ -83,7 +83,7 @@ function data_struct = run_data_processing(main_function_name,target_directory,v
     %functions or toolboxes
     command_list=[get_user_dependencies(main_function_name);{strcat(mfilename('fullpath'),'.m')}];
     for i=1:length(command_list)
-        [SUCCESS,MESSAGE,MESSAGEID] = copyfile(command_list{i},strcat(target_directory,'\Code'));
+        [SUCCESS,MESSAGE,MESSAGEID] = copyfile(command_list{i},strcat(target_directory,'Code'));
         if SUCCESS
             disp(strcat('successfully copied ',command_list{i},' to the code folder'))
         else
@@ -106,21 +106,21 @@ function data_struct = run_data_processing(main_function_name,target_directory,v
             fname=strcat('Figure_',num2str(i));
         end
         fname(fname==' ')='_';%replace spaces in name for saving
-        print('-dpdf',figure_list(i),strcat(target_directory,'\Raw_Figures\PDF\',fname,'.pdf'))
-        print('-deps',figure_list(i),strcat(target_directory,'\Raw_Figures\EPS\',fname,'.eps'))
-        print('-dpng',figure_list(i),strcat(target_directory,'\Raw_Figures\PNG\',fname,'.png'))
-        saveas(figure_list(i),strcat(target_directory,'\Raw_Figures\FIG\',fname,'.fig'),'fig')
+        print('-dpdf',figure_list(i),strcat(target_directory,'Raw_Figures\PDF\',fname,'.pdf'))
+        print('-deps',figure_list(i),strcat(target_directory,'Raw_Figures\EPS\',fname,'.eps'))
+        print('-dpng',figure_list(i),strcat(target_directory,'Raw_Figures\PNG\',fname,'.png'))
+        saveas(figure_list(i),strcat(target_directory,'Raw_Figures\FIG\',fname,'.fig'),'fig')
     end
 
     %% save the input and output data structures
     if ~isempty(varargin)
         temp=varargin{1};
-        save(strcat(target_directory,'\Input_Data\Input_structure.mat'),'temp','-mat')
+        save(strcat(target_directory,'Input_Data\Input_structure.mat'),'temp','-mat')
     end
-    fid=fopen(strcat(target_directory,'\Input_Data\target_directory.txt'),'w+');
+    fid=fopen(strcat(target_directory,'Input_Data\target_directory.txt'),'w+');
             fprintf(fid,'%s',target_directory);
             fclose(fid);
-    fid=fopen(strcat(target_directory,'\Input_Data\main_function_name.txt'),'w+');
+    fid=fopen(strcat(target_directory,'Input_Data\main_function_name.txt'),'w+');
             fprintf(fid,'%s',main_function_name);
             fclose(fid);    
     
@@ -129,21 +129,21 @@ function data_struct = run_data_processing(main_function_name,target_directory,v
         temp=getfield(data_struct,data_list{i});
         %if the object is a session summary, write a summary text file
         if strcmp(data_list{i},'session_summary')
-            write_session_summary(data_struct.session_summary,strcat(target_directory,'\Output_data\','session_summary.txt'))
+            write_session_summary(data_struct.session_summary,strcat(target_directory,'Output_data\','session_summary.txt'))
         end
         if ischar(temp)%if the field is just a string like a list of file names
-            fid=fopen(strcat(target_directory,'\Output_Data\',data_list{i},'.txt'),'w+');
+            fid=fopen(strcat(target_directory,'Output_Data\',data_list{i},'.txt'),'w+');
             fprintf(fid,'%s',temp);
             fclose(fid);
         else
             eval([data_list{i} '= data_struct.(data_list{i});'])            
-            save(strcat(target_directory,'\Output_Data\',data_list{i},'.mat'),data_list{i},'-mat')
+            save(strcat(target_directory,'Output_Data\',data_list{i},'.mat'),data_list{i},'-mat')
             warn_message = lastwarn;
             %% Check to make sure that Matlab saved the data, if not, save as v7.3
             if strfind(warn_message,'Variable ')
                 disp('Ignore previous warning, data is being saved')
                 dummy_var = [];  % Matlab will compress the first variable saved, making it slower to load, so we compress an empty array.
-                save(strcat(target_directory,'\Output_Data\',data_list{i},'.mat'),'dummy_var',data_list{i},'-mat','-v7.3')
+                save(strcat(target_directory,'Output_Data\',data_list{i},'.mat'),'dummy_var',data_list{i},'-mat','-v7.3')
                 lastwarn('')
             end
                     
