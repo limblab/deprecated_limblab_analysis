d3e6c10787623129c8f5914880604061157ef7f5
Tucker Tomlinson <tuckertomlinson@gmail.com>
2015-11-11 19:37:43 +0000

    updates to histogram binning
    git-svn-id: http://citadel.physiology.northwestern.edu/svn/s1_analysis@2012 984b1e3c-1c57-0410-ae62-c18f90d3b645

--- lib/testEncoderSkips.m
+++ lib/testEncoderSkips.m
@@ -11,12 +11,11 @@ function [figures,data]=testEncoderSkips(folderPath)
     fileList={};
     numPoints=[];
     allSteps=[];
-    allStepsNorm=[];
     tSteps=[];
-    tStepsNorm=[];
+    minSteps=[];
+    maxSteps=[];
     stepHists=[];
-    stepHistsNorm=[];
-    bins=0:.05:.95;
+    bins=0:.005:.95;
     for i=1:length(foldercontents)
         if (length(fnames{i})>3)
             if exist(strcat(folderPath,fnames{i}),'file')~=2
@@ -70,21 +69,24 @@ function [figures,data]=testEncoderSkips(folderPath)
                     end   
 
                     %now that we have the encoder strobes, convert those to actual encoder values  
-                    enc = strobed2encoder(encStrobes,[0 NEV.MetaTags.DataDurationSec]);
+                    enc = strb2enc(encStrobes,[0 NEV.MetaTags.DataDurationSec]);
                     
-                    %get our sig, figs for rounding based on the nominal sampling rate:
-                    temp=mode(diff(enc(:,1)));
-                    SF=0;
-                    while temp<1
-                        SF=SF+1;
-                        temp=temp*10;
-                    end
-                    
-                    %get our actual timesteps
-                    tSteps=round(diff(enc(:,1)),SF);%the rounding allows jitter at ~ 10% of the sample frequency because SF is #sig figs+1 after the above while statement
+%                     %get our sig, figs for rounding based on the nominal sampling rate:
+%                     temp=mode(diff(enc(:,1)));
+%                     SF=0;
+%                     while temp<1
+%                         SF=SF+1;
+%                         temp=temp*10;
+%                     end
+%                     
+%                     %get our actual timesteps
+%                     tSteps=round(diff(enc(:,1)),SF);%the rounding allows jitter at ~ 10% of the sample frequency because SF is #sig figs+1 after the above while statement
+                    tSteps=diff(enc(:,1));
                     allSteps=[allSteps;tSteps];
                     numPoints(end+1)=numel(tSteps);
-                    tSteps=tSteps(tSteps>mode(tSteps));
+                    minSteps(end+1)=min(tSteps);
+                    maxSteps(end+1)=max(tSteps);
+                    %tSteps=tSteps(tSteps>mode(tSteps));
                     
                     %get the breakdown of sample spacing for this specific
                     %file
@@ -98,18 +100,7 @@ function [figures,data]=testEncoderSkips(folderPath)
                     title(['Histogram of lags between data points ',fname])
                     xlabel('latency between samples(s)')
                     ylabel('log count of number of points')
-                    %get the normalized histogram
-                    bins=0:.05:.95;
-                    stepHistsNorm(end+1,:)=stepHists(end,:)/numPoints(end);
-                    %plot the histogram and set the figure properties
-                    figures(end+1)=figure;
-                    bar(bins,stepHistsNorm(end,:));
-                    set(gca,'YScale','log')
-                    set(figures(end),'Name',['Norm_Hist_',fname])
-                    set(figures(end),'Position',[100 100 1200 1200])
-                    title(['Normalized histogram of lags between data points ',fname])
-                    xlabel('latency between samples(s)')
-                    ylabel('log of normalized count of number of points')
+                    
                 catch temperr
                     disp(strcat('Failed to process: ', temppath))
                     disp(temperr.identifier)
@@ -124,7 +115,6 @@ function [figures,data]=testEncoderSkips(folderPath)
     end
     %get histogram data for the whole data set:
     %file
-    bins=0:.05:.95;
     allHist=hist(allSteps,bins);
     %plot the histogram and set the figure properties
     figures(end+1)=figure;
@@ -135,27 +125,94 @@ function [figures,data]=testEncoderSkips(folderPath)
     title(['Histogram of lags between data points for all data'])
     xlabel('latency between samples(s)')
     ylabel('log count of number of points')
-    %get the normalized histogram
-    bins=0:.05:.95;
-    allHistNorm=allHist/numel(allSteps);
-    %plot the histogram and set the figure properties
-    figures(end+1)=figure;
-    bar(bins,allHistNorm);
-    set(gca,'YScale','log')
-    set(figures(end),'Name','Norm_Hist_All_Data')
-    set(figures(end),'Position',[100 100 1200 1200])
-    title(['Normalized histogram of lags between data points for all data'])
-    xlabel('latency between samples(s)')
-    ylabel('log of normalized count of number of points')
-    
-    
+
     %build our ourput data structure:
     data.fileList=fileList;
     data.stepHists=stepHists;
-    data.stepHistNorm=stepHistsNorm;
     data.numPoints=numPoints;
     data.allHist=allHist;
-    data.allHistNorm=allHistNorm;
     data.allNumPoints=numel(allSteps);
+    data.minSteps=minSteps;
+    data.maxSteps=maxSteps;
     
+end
+function varargout = strb2enc(strobed_events,varargin)
+    if ~isempty(varargin)
+        %variable input used to pass times that this function will ignore step
+        %shifts in the output. particularly important for ignoring shifts that
+        %occur when files are concatenated together.
+        ignore_windows=varargin{1};
+    else
+        ignore_windows=[];
+    end
+
+    if (size(strobed_events,2) ~= 2)
+        error('get_encoder:BadMatrix','input strobed_events must be a two column matrix');
+    end
+
+    % get time-stamps of the first strobe in a set of four
+    ts = strobed_events(:,1);
+    ts_index = find( diff(ts) > .000275 )+1;
+    ts_index = ts_index( diff(ts_index) == 4 ); % throw out bad points
+    time_stamps = ts( ts_index );
+
+    %make mask vector to use as flag for ignoring timepoints
+    mask=ones(size(time_stamps));
+    temp=[];
+    if ~isempty(ignore_windows)
+        for i=1:size(ignore_windows,1)
+            range=[find(time_stamps>=ignore_windows(i,1),1,'first'),find( time_stamps<=ignore_windows(i,2),1,'last')];
+            %if there are no points inside the window, as the case with
+            %fileseparateions, the first point of range will be larger than the
+            %second. Thus we use min and max to get the actual window for all
+            %cases
+            temp=[temp;[min(range):max(range)]'];
+        end
+        mask(temp)=0;
+    end
+
+    % assemble encoder signals
+    encoder = zeros(length(ts_index)-2, 3);
+    if (length(ts_index)-2>=1)
+        encoder(:,1) = time_stamps(1:end-2);
+        encoder(:,2) = strobed_events(ts_index(1:end-2),2) + strobed_events(ts_index(1:end-2)+1,2)*2^8 - 32765;
+        encoder(:,3) = strobed_events(ts_index(1:end-2)+2,2) + strobed_events(ts_index(1:end-2)+3,2)*2^8 - 32765;
+    end
+
+    %fix steps in encoder 1
+    temp_indices = find( (diff(encoder(:,2))>50 | diff(encoder(:,2))<-50) & mask(1:end-3));
+    data_jumps=0;
+    jump_times=encoder(temp_indices,1);
+    if ~isempty(temp_indices)
+        for i=length(temp_indices):-1:1
+            if mask(temp_indices(i))
+                encoder(temp_indices(i)+1:end,2) = encoder(temp_indices(i)+1:end,2)-(encoder(temp_indices(i)+1,2)-encoder(temp_indices(i),2));
+            end
+        end
+        data_jumps=length(temp_indices);
+    end
+
+    %fix steps in encoder 2
+    temp_indices = find( (diff(encoder(:,3))>50 | diff(encoder(:,3))<-50) & mask(1:end-3));
+    jump_times=[jump_times;encoder(temp_indices,1)];
+    if ~isempty(temp_indices)
+        for i=length(temp_indices):-1:1
+            if mask(temp_indices(i))
+                encoder(temp_indices(i)+1:end,3) = encoder(temp_indices(i)+1:end,3)-(encoder(temp_indices(i)+1,3)-encoder(temp_indices(i),3));
+            end
+        end
+        data_jumps=data_jumps+length(temp_indices);
+    end
+
+    if data_jumps
+        warning('get_encoder:corruptEncoderSignal','The encoder data contains large jumps. These jumps were removed in get_encoder')
+        disp(['Found',num2str(data_jumps),' step offsets in the data'])
+        if ~isempty(ignore_windows)
+            disp('Steps associated with some time points such as file concatination times may have been ignored')
+        end
+    end
+    varargout{1}=encoder;
+    if nargout>1
+        varargout{2}=jump_times;
+    end
 end
