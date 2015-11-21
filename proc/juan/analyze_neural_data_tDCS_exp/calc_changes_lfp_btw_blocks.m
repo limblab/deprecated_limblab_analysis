313669dda3b95dc99d7cfb9662893792bbe38cd6
Juan Gallego <jotagalabe@gmail.com>
2015-11-21 23:11:04 +0000

    file for a function to calculate changes in LFP power
    git-svn-id: http://citadel.physiology.northwestern.edu/svn/s1_analysis@2024 984b1e3c-1c57-0410-ae62-c18f90d3b645

---
+++ proc/juan/analyze_neural_data_tDCS_exp/calc_changes_lfp_btw_blocks.m
@@ -0,0 +1,6 @@
+
+function [neural_activity_1, neural_activity_2, neural_activity_3] = ...
+    calc_changes_lfp_btw_blocks( neural_activity_1, neural_activity_2, neural_activity_3 )
+
+
+% Block 2 wrt to Block 1
