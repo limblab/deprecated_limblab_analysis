1447d4ab6d208b4880b32daffc2886e825b70c97
Raeed Chowdhury <raeed.chowdhury@gmail.com>
2015-11-24 02:01:30 +0000

    Add scripts for processing vibration data
    git-svn-id: http://citadel.physiology.northwestern.edu/svn/s1_analysis@2034 984b1e3c-1c57-0410-ae62-c18f90d3b645

--- proc/raeed/Projects/SensorStim/SpindleStim/find_vibe_sensitivity.m
+++ proc/raeed/Projects/SensorStim/SpindleStim/find_vibe_sensitivity.m
@@ -22,7 +22,7 @@ function [figure_handles, output_data]=find_vibe_sensitivity(folder,options)
             if(folder(end)~=filesep)
                 folder = [folder filesep];
             end
-            bdf = get_nev_mat_data([folder options.prefix],options.labnum);
+            bdf = get_nev_mat_data([folder options.prefix],options.labnum,'nokin','noforce');
         else
             bdf = options.bdf;
         end
@@ -33,10 +33,37 @@ function [figure_handles, output_data]=find_vibe_sensitivity(folder,options)
         opts.do_trial_table=0;
         opts.do_firing_rate=1;
         bdf=postprocess_bdf(bdf,opts);
-        output_data.bdfpost_DL=bdf;
+        output_data.bdf=bdf;
+        
+        if(isfield(options,'which_units'))
+            which_units = options.which_units;
+        elseif options.only_sorted
+            for i=1:length(bdf.units)
+                temp(i)=bdf.units(i).id(2)~=0 && bdf.units(i).id(2)~=255;
+            end
+            ulist=1:length(bdf.units);
+            which_units=ulist(temp);
+        end
 
         %% figures
         figure_handles = [];
+        
+        % basic raster plot over vibration
+        h = figure('name','Vibe Raster');
+        figure_handles = [figure_handles h];
+        
+        plot(bdf.analog.ts',bdf.analog.data/max(abs(bdf.analog.data)),'-b');
+        hold on
+        
+        num_units = length(which_units);
+        for i=1:num_units
+            spike_times = bdf.units(which_units(i)).ts;
+            plot(spike_times,i,'k.')
+        end
+        
+        labels = strcat(repmat({'Unit '},num_units,1),cellstr(strtrim(num2str((1:num_units)'))))';
+        labels = [{'Vibration'} labels];
+        set(gca,'ylim',[-1 i+1],'ytick',0:i,'yticklabels',labels,'tickdir','out')
         
     catch MExc
         output_data.MExc = MExc;
--- proc/raeed/Projects/SensorStim/SpindleStim/vibe_analysis.m
+++ proc/raeed/Projects/SensorStim/SpindleStim/vibe_analysis.m
@@ -7,3 +7,33 @@ function_name='find_vibe_sensitivity';
 options.labnum = 6;
 
 vibe_data = run_data_processing(function_name,folder,options);
+
+%% Load bdf
+folder='C:\Users\rhc307\Box Sync\Research\Cuneate\UChicago\Cuneate Data\Whitslepig_20151113_Millerlab\';
+clear options
+options.prefix='Whitslepig_20151113_bicepsvibetendon_cuneate';
+options.only_sorted=1;
+function_name='find_vibe_sensitivity';
+options.labnum = 6;
+
+vibe_data = run_data_processing(function_name,folder,options);
+
+%% Load bdf
+folder='C:\Users\rhc307\Box Sync\Research\Cuneate\UChicago\Cuneate Data\Whitslepig_20151113_Millerlab\';
+clear options
+options.prefix='Whitslepig_20151113_tricepsvibe_cuneate';
+options.only_sorted=1;
+function_name='find_vibe_sensitivity';
+options.labnum = 6;
+
+vibe_data = run_data_processing(function_name,folder,options);
+
+%% Load bdf
+folder='C:\Users\rhc307\Box Sync\Research\Cuneate\UChicago\Cuneate Data\Whitslepig_20151113_Millerlab\';
+clear options
+options.prefix='Whitslepig_20151113_tricepsvibetendon_cuneate';
+options.only_sorted=1;
+function_name='find_vibe_sensitivity';
+options.labnum = 6;
+
+vibe_data = run_data_processing(function_name,folder,options);
