28fbab0f3e92b241a2387517ed7883c84e8cee6a
Rummi Ganguly <surobhiganguly2015@northwestern.edu>
2015-12-02 02:21:36 +0000

    STA analysis added
    git-svn-id: http://citadel.physiology.northwestern.edu/svn/s1_analysis@2043 984b1e3c-1c57-0410-ae62-c18f90d3b645

--- proc/rummi/SpindleStim/find_vibe_sensitivity.m
+++ proc/rummi/SpindleStim/find_vibe_sensitivity.m
@@ -40,21 +40,21 @@ function [figure_handles, output_data]=find_vibe_sensitivity(folder,options)
             which_units = options.which_units;
         elseif options.only_sorted
             for i=1:length(bdf.units)
-                temp(i)=bdf.units(i).id(2)~=0 && bdf.units(i).id(2)~=255;
+                temp(i)=bdf.units(i).id(2)~=0 && bdf.units(i).id(2)~=255; %==0 for multi unit channels
             end
             ulist=1:length(bdf.units);
             which_units=ulist(temp);
         end
 
-        %% figures
+        %% FIGURES
         figure_handles = [];
-        figure_title = options.figure_title;
+        figure_title = strcat(options.figure_title,'1');
         
-        % basic raster plot over vibration
+        % BASIC RASTOR PLOT OVER VIBRATION
+        
         h = figure('name',figure_title);
         figure_handles = [figure_handles h];
         
-        figure(1)
         plot(bdf.analog.ts',bdf.analog.data/max(abs(bdf.analog.data)),'-b');
         hold on
         
@@ -68,58 +68,116 @@ function [figure_handles, output_data]=find_vibe_sensitivity(folder,options)
         labels = [{'Vibration'} labels];
         set(gca,'ylim',[-1 i+1],'ytick',0:i,'yticklabels',labels,'tickdir','out')
         
-        % gaussian convolution
+        % GAUSSIAN CONVOLUTION
+        
         for i=1:num_units
-            FR_time = bdf.units(which_units(i)).FR(:,1);
-            FR_data = bdf.units(which_units(i)).FR(:,2);
-            G = fspecial('gaussian', [20,1], 5);
-            FR_data_conv = conv(FR_data, G, 'same');
-            FR_data_conv_norm = FR_data_conv/(max((abs(FR_data_conv))));
-            plot(FR_time,FR_data_conv_norm+i, 'g')
-        end
-      
-        % fft of stim and unit response
-        figure(2)
-        
-        data_length = length(bdf.analog.data);
-        Y1 = fft(bdf.analog.data)';
-
-        P2 = abs(Y1/data_length);
-        P1 = P2(1:data_length/2+1);
-        P1(2:end-1) = 2*P1(2:end-1);
-
-        f1 = 1000*(0:(data_length/2))/data_length;
-
-        plot(f1(3:end),P1(3:end)/max(abs(P1)),'k')
-        hold on
-        
-        for k=1:num_units
-           spike(:,1) = bdf.analog.ts;
-           spike(:,2) = 0;
-           unit_ts_rounded = round(bdf.units(which_units(k)).ts*1000)/1000;
-           
+            
+           spikes(:,1) = bdf.analog.ts;
+           spikes(:,2) = 0;
+           unit_ts_rounded = round(bdf.units(which_units(i)).ts*1000)/1000;
 
             for m=1:length(unit_ts_rounded)
-               for n=1:length(spike)
-                  if(spike(n,1) == unit_ts_rounded(m))
-                     spike(n,2) = 1;
+               for n=1:length(spikes)
+                  if(spikes(n,1) == unit_ts_rounded(m))
+                     spikes(n,2) = 1;
                   else
                   end
                end
             end
-           
-        
-        Y2 = fft(spike(:,2))';
-
-        P2 = abs(Y2/data_length);
-        P1 = P2(1:data_length/2+1);
-        P1(2:end-1) = 2*P1(2:end-1);
-
-        f2 = 1000*(0:(data_length/2))/data_length;
-
-        plot(f2(3:end),P1(3:end)/max(abs(P1))+k)
-        hold all
+            
+            G = fspecial('gaussian', [150,1], 60);
+            data_conv = conv(spikes(:,2), G, 'same');
+            
+            data_conv_norm = data_conv/(max((abs(data_conv))));
+            plot(spikes(:,1),data_conv_norm*.95+i, 'g')
         end
+      
+        % FFT OF SPIKES AND VIBRATION
+        
+% % %         figure_title = strcat(options.figure_title,'2');
+% % %         h = figure('name',figure_title);
+% % %         figure_handles = [figure_handles h];
+% % %         
+% % %         data_length = length(bdf.analog.data);
+% % %         Y1 = fft(bdf.analog.data)';
+% % % 
+% % %         P2 = abs(Y1/data_length);
+% % %         P1 = P2(1:data_length/2+1);
+% % %         P1(2:end-1) = 2*P1(2:end-1);
+% % % 
+% % %         f1 = 1000*(0:(data_length/2))/data_length;
+% % % 
+% % %         plot(f1(3:end),P1(3:end)/max(abs(P1)),'k')
+% % %         hold on
+% % %         
+% % %         for k=1:num_units
+% % %            spikes(:,1) = bdf.analog.ts;
+% % %            spikes(:,2) = 0;
+% % %            unit_ts_rounded = round(bdf.units(which_units(k)).ts*1000)/1000;
+% % %            
+% % % 
+% % %             for m=1:length(unit_ts_rounded)
+% % %                for n=1:length(spikes)
+% % %                   if(spikes(n,1) == unit_ts_rounded(m))
+% % %                      spikes(n,2) = 1;
+% % %                   else
+% % %                   end
+% % %                end
+% % %             end
+% % %            
+% % %         Y2 = fft(spikes(:,2))';
+% % % 
+% % %         P2 = abs(Y2/data_length);
+% % %         P1 = P2(1:data_length/2+1);
+% % %         P1(2:end-1) = 2*P1(2:end-1);
+% % % 
+% % %         f2 = 1000*(0:(data_length/2))/data_length;
+% % % 
+% % %         plot(f2(3:end),P1(3:end)/max(abs(P1))+k)
+% % %         hold all
+% % %         end
+        
+        % STA - 20ms before and after spike occurs
+        figure_title = strcat(options.figure_title,' 3');
+        h = figure('name',figure_title);
+        figure_handles = [figure_handles h];
+        
+        for i=1:num_units %for each unit
+            spike_times = bdf.units(which_units(i)).ts; %vector or spike times
+            st_rounded = round(bdf.units(which_units(i)).ts*1000)/1000; %round to match sig fig of analog signal
+            sta_matrix = zeros(length(st_rounded),41);
+            sta=zeros(41,1);
+
+            
+            for k=1:length(st_rounded) %for each spike time
+
+                for m=1:length(bdf.analog.ts) %check each time point of the analog vibration signal
+                    
+                    if(bdf.analog.ts(m)==st_rounded(k) && m-20>0 && m+20<length(bdf.analog.ts)) %if the analog time point matches spike time, store analog.data 40ms snippet in new col. in matrix
+                       sta_matrix(k,:) = bdf.analog.data(m-20:m+20);
+                    end
+                end
+            end
+            for n=1:41
+               sta(n) = mean(sta_matrix(:,n));   
+            end
+            
+            t = linspace(-.02,.02,41);
+            
+            
+            plot(t,sta)
+            yL = get(gca,'YLim');
+            line([0 0],yL,'Color','k');
+            hold all
+            
+        end
+        
+
+        % ISI - to do
+        
+        
+        % MULTI-UNIT ANALYSIS
+        
         
         
         
