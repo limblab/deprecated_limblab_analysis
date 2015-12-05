a5b804bc87125776bee38e1fda6cd20222d16ab8
Rummi Ganguly <surobhiganguly2015@northwestern.edu>
2015-12-05 01:00:45 +0000

    testing new data set
    git-svn-id: http://citadel.physiology.northwestern.edu/svn/s1_analysis@2053 984b1e3c-1c57-0410-ae62-c18f90d3b645

--- proc/rummi/SpindleStim/vibe_analysis.m
+++ proc/rummi/SpindleStim/vibe_analysis.m
@@ -1,7 +1,29 @@
+% %% Load bdf
+% folder='C:\Users\limblab\Desktop\rummi_vibe_cuneate_analysis\cuneate_day1\Whitslepig_20151113_bicepsvibe_cuneate_004\';
+% clear options
+% options.prefix='Whitslepig_20151113_bicepsvibe_cuneate_004';
+% options.only_sorted=1;
+% function_name='find_vibe_sensitivity';
+% options.labnum = 6;
+% options.figure_title = 'bicepsvibe';
+% 
+% vibe_data = run_data_processing(function_name,folder,options);
+% 
+% % Load bdf
+% folder='C:\Users\limblab\Desktop\rummi_vibe_cuneate_analysis\cuneate_day1\Whitslepig_20151113_tricepsvibe_cuneate_006\';
+% clear options
+% options.prefix='Whitslepig_20151113_tricepsvibe_cuneate_006';
+% options.only_sorted=1;
+% function_name='find_vibe_sensitivity';
+% options.labnum = 6;
+% options.figure_title = 'tricepsvibe';
+% 
+% vibe_data = run_data_processing(function_name,folder,options);
+
 %% Load bdf
-folder='C:\Users\limblab\Desktop\rummi_vibe_cuneate_analysis\Vibration\Whitslepig_20151113_bicepssweep_cuneate_012\';
+folder='C:\Users\limblab\Desktop\rummi_vibe_cuneate_analysis\cuneate_day2\Whistlepig_20151124_bicepssweep_011\';
 clear options
-options.prefix='Whitslepig_20151113_bicepssweep_cuneate_012';
+options.prefix='Whistlepig_20151124_bicepssweep_011';
 options.only_sorted=1;
 function_name='find_vibe_sensitivity';
 options.labnum = 6;
@@ -10,9 +32,9 @@ options.figure_title = 'bicepssweep';
 vibe_data = run_data_processing(function_name,folder,options);
 
 %% Load bdf
-folder='C:\Users\limblab\Desktop\rummi_vibe_cuneate_analysis\Vibration\Whitslepig_20151113_bicepsvibe_cuneate_004\';
+folder='C:\Users\limblab\Desktop\rummi_vibe_cuneate_analysis\cuneate_day2\Whistlepig_20151124_bicepsvibe_002\';
 clear options
-options.prefix='Whitslepig_20151113_bicepsvibe_cuneate_004';
+options.prefix='Whistlepig_20151124_bicepsvibe_002';
 options.only_sorted=1;
 function_name='find_vibe_sensitivity';
 options.labnum = 6;
@@ -21,42 +43,96 @@ options.figure_title = 'bicepsvibe';
 vibe_data = run_data_processing(function_name,folder,options);
 
 %% Load bdf
-folder='C:\Users\limblab\Desktop\rummi_vibe_cuneate_analysis\Vibration\Whitslepig_20151113_bicepsvibetendon_cuneate_005\';
+folder='C:\Users\limblab\Desktop\rummi_vibe_cuneate_analysis\cuneate_day2\Whistlepig_20151124_deltoidvibe_007\';
 clear options
-options.prefix='Whitslepig_20151113_bicepsvibetendon_cuneate_005';
+options.prefix='Whistlepig_20151124_deltoidvibe_007';
 options.only_sorted=1;
 function_name='find_vibe_sensitivity';
 options.labnum = 6;
-options.figure_title = 'bicepsvibetendon';
+options.figure_title = 'deltoidvibe';
 
 vibe_data = run_data_processing(function_name,folder,options);
 
 %% Load bdf
-folder='C:\Users\limblab\Desktop\rummi_vibe_cuneate_analysis\Vibration\Whitslepig_20151113_deltvibe_cuneate_011\';
+folder='C:\Users\limblab\Desktop\rummi_vibe_cuneate_analysis\cuneate_day2\Whistlepig_20151124_deltsweep_017';
 clear options
-options.prefix='Whitslepig_20151113_deltvibe_cuneate_011';
+options.prefix='Whistlepig_20151124_deltsweep_017';
 options.only_sorted=1;
 function_name='find_vibe_sensitivity';
 options.labnum = 6;
-options.figure_title = 'deltvibe';
+options.figure_title = 'deltsweep';
 
 vibe_data = run_data_processing(function_name,folder,options);
 
 %% Load bdf
-folder='C:\Users\limblab\Desktop\rummi_vibe_cuneate_analysis\Vibration\Whitslepig_20151113_pectvibe_cuneate_010\';
+folder='C:\Users\limblab\Desktop\rummi_vibe_cuneate_analysis\cuneate_day2\Whistlepig_20151124_latsvibe_008';
 clear options
-options.prefix='Whitslepig_20151113_pectvibe_cuneate_010';
+options.prefix='Whistlepig_20151124_latsvibe_008';
+options.only_sorted=1;
+function_name='find_vibe_sensitivity';
+options.labnum = 6; 
+options.figure_title = 'latsvibe';
+
+vibe_data = run_data_processing(function_name,folder,options);
+%% Load bdf
+folder='C:\Users\limblab\Desktop\rummi_vibe_cuneate_analysis\cuneate_day2\Whistlepig_20151124_latsweep_018\';
+clear options
+options.prefix='Whistlepig_20151124_latsweep_018';
 options.only_sorted=1;
 function_name='find_vibe_sensitivity';
 options.labnum = 6;
-options.figure_title = 'pectvibe';
+options.figure_title = 'latsweep';
 
 vibe_data = run_data_processing(function_name,folder,options);
 
 %% Load bdf
-folder='C:\Users\limblab\Desktop\rummi_vibe_cuneate_analysis\Vibration\Whitslepig_20151113_tricepsvibe_cuneate_006\';
+folder='C:\Users\limblab\Desktop\rummi_vibe_cuneate_analysis\cuneate_day2\Whistlepig_20151124_pecsweep_013\';
 clear options
-options.prefix='Whitslepig_20151113_tricepsvibe_cuneate_006';
+options.prefix='Whistlepig_20151124_pecsweep_013';
+options.only_sorted=1;
+function_name='find_vibe_sensitivity';
+options.labnum = 6;
+options.figure_title = 'pecsweep';
+
+vibe_data = run_data_processing(function_name,folder,options);
+
+%% Load bdf
+folder='C:\Users\limblab\Desktop\rummi_vibe_cuneate_analysis\cuneate_day2\Whistlepig_20151124_pecsweep_014\';
+clear options
+options.prefix='Whistlepig_20151124_pecsweep_014';
+options.only_sorted=1;
+function_name='find_vibe_sensitivity';
+options.labnum = 6;
+options.figure_title = 'pecsweep';
+
+vibe_data = run_data_processing(function_name,folder,options);
+
+%% Load bdf
+folder='C:\Users\limblab\Desktop\rummi_vibe_cuneate_analysis\cuneate_day2\Whistlepig_20151124_pecvibe_004\';
+clear options
+options.prefix='Whistlepig_20151124_pecvibe_004';
+options.only_sorted=1;
+function_name='find_vibe_sensitivity';
+options.labnum = 6;
+options.figure_title = 'pecvibe';
+
+vibe_data = run_data_processing(function_name,folder,options);
+
+%% Load bdf
+folder='C:\Users\limblab\Desktop\rummi_vibe_cuneate_analysis\cuneate_day2\Whistlepig_20151124_tricepssweep_012\';
+clear options
+options.prefix='Whistlepig_20151124_tricepssweep_012';
+options.only_sorted=1;
+function_name='find_vibe_sensitivity';
+options.labnum = 6;
+options.figure_title = 'tricepssweep';
+
+vibe_data = run_data_processing(function_name,folder,options);
+
+%% Load bdf
+folder='C:\Users\limblab\Desktop\rummi_vibe_cuneate_analysis\cuneate_day2\Whistlepig_20151124_tricepsvibe_003\';
+clear options
+options.prefix='Whistlepig_20151124_tricepsvibe_003';
 options.only_sorted=1;
 function_name='find_vibe_sensitivity';
 options.labnum = 6;
@@ -65,20 +141,20 @@ options.figure_title = 'tricepsvibe';
 vibe_data = run_data_processing(function_name,folder,options);
 
 %% Load bdf
-folder='C:\Users\limblab\Desktop\rummi_vibe_cuneate_analysis\Vibration\Whitslepig_20151113_tricepsvibetendon_cuneate_007\';
+folder='C:\Users\limblab\Desktop\rummi_vibe_cuneate_analysis\cuneate_day2\Whistlepig_20151124_wristextsweep_015\';
 clear options
-options.prefix='Whitslepig_20151113_tricepsvibetendon_cuneate_007';
+options.prefix='Whistlepig_20151124_wristextsweep_015';
 options.only_sorted=1;
 function_name='find_vibe_sensitivity';
 options.labnum = 6;
-options.figure_title = 'tricepsvibetendon';
+options.figure_title = 'wristextsweep';
 
 vibe_data = run_data_processing(function_name,folder,options);
 
 %% Load bdf
-folder='C:\Users\limblab\Desktop\rummi_vibe_cuneate_analysis\Vibration\Whitslepig_20151113_wristextvibe_cuneate_009\';
+folder='C:\Users\limblab\Desktop\rummi_vibe_cuneate_analysis\cuneate_day2\Whistlepig_20151124_wristextvibe_005\';
 clear options
-options.prefix='Whitslepig_20151113_wristextvibe_cuneate_009';
+options.prefix='Whistlepig_20151124_wristextvibe_005';
 options.only_sorted=1;
 function_name='find_vibe_sensitivity';
 options.labnum = 6;
@@ -87,22 +163,23 @@ options.figure_title = 'wristextvibe';
 vibe_data = run_data_processing(function_name,folder,options);
 
 %% Load bdf
-folder='C:\Users\limblab\Desktop\rummi_vibe_cuneate_analysis\Vibration\Whitslepig_20151113_wristflexvibe_cuneate_008\';
+folder='C:\Users\limblab\Desktop\rummi_vibe_cuneate_analysis\cuneate_day2\Whistlepig_20151124_wristflexsweep_016\';
 clear options
-options.prefix='Whitslepig_20151113_wristflexvibe_cuneate_008';
+options.prefix='Whistlepig_20151124_wristflexsweep_016';
+options.only_sorted=1;
+function_name='find_vibe_sensitivity';
+options.labnum = 6;
+options.figure_title = 'wristflexsweep';
+
+vibe_data = run_data_processing(function_name,folder,options);
+
+%% Load bdf
+folder='C:\Users\limblab\Desktop\rummi_vibe_cuneate_analysis\cuneate_day2\Whistlepig_20151124_wristflexvibe_006\';
+clear options
+options.prefix='Whistlepig_20151124_wristflexvibe_006';
 options.only_sorted=1;
 function_name='find_vibe_sensitivity';
 options.labnum = 6;
 options.figure_title = 'wristflexvibe';
-
-vibe_data = run_data_processing(function_name,folder,options);
-%% Load bdf
-folder='C:\Users\limblab\Desktop\rummi_vibe_cuneate_analysis\Vibration\Whitslepig_20151113_wristsweep_cuneate_013\';
-clear options
-options.prefix='Whitslepig_20151113_wristsweep_cuneate_013';
-options.only_sorted=1;
-function_name='find_vibe_sensitivity';
-options.labnum = 6;
-options.figure_title = 'wristsweep';
 
 vibe_data = run_data_processing(function_name,folder,options);
