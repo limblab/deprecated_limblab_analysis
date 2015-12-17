5bd80925e1049c684e648848d12ecfa6a1c49b68
Raeed Chowdhury <raeed.chowdhury@gmail.com>
2015-12-17 21:02:59 +0000

    git-svn-id: http://citadel.physiology.northwestern.edu/svn/s1_analysis@2072 984b1e3c-1c57-0410-ae62-c18f90d3b645

--- proc/raeed/Projects/MotionTracking/test/test_sync.m
+++ proc/raeed/Projects/MotionTracking/test/test_sync.m
@@ -1,6 +1,6 @@
 %% load file
-folder = 'C:\Users\rhc307\Documents\Data\experiment_20151117_actpas\';
-prefix = 'Chips_20151117_COactpas';
+folder = 'C:\Users\rhc307\Documents\Data\experiment_20151120_RW_003\';
+prefix = 'Chips_20151120_RW_003';
 bdf = get_nev_mat_data([folder prefix],6,'ignore_jumps');
 
 %% load color tracking
@@ -17,5 +17,11 @@ plot(analog_ts-t_start,KinectSyncPulse/max(KinectSyncPulse),'-b',times-led_start
 
 %% change kinect times
 kinect_times = times-led_start+t_start;
+
+%% save new times vector and handle position into file
+handle_pos = bdf.pos(:,2:3);
+handle_times = bdf.pos(:,1);
+
+save([folder prefix '-spatiotemporalSync'],'handle_pos', 'handle_times', 'kinect_times', 'all_medians', 'all_medians2')
 
 %% put Kinect data into bdf
