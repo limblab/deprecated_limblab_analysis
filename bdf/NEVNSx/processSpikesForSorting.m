a34b09a2dde19b0f157b64953ad954aa972e8b8c
Raeed Chowdhury <rhcggb@gmail.com>
2016-01-08 18:35:45 -0600

    strip out digital and analog data for sorting, using processSpikesForSorting

--- bdf/NEVNSx/processSpikesForSorting.m
+++ bdf/NEVNSx/processSpikesForSorting.m
@@ -10,17 +10,43 @@ function status = processSpikesForSorting(file_path,file_prefix)
         NEVlist = dir([file_path file_prefix '*.nev']);
         NEVlist = NEVlist(cellfun('isempty',(regexp({NEVlist(:).name},'-s'))));
         disp(['Merging ' num2str(length(NEVlist)) ' files.'])
-        NEVNSx_all = cerebus2NEVNSx(file_path,file_prefix);
+        NEVNSx_all = cerebus2NEVNSx(file_path,file_prefix,'noanalog');
         NEVNSx_all.NEV = artifact_removal(NEVNSx_all.NEV,10,0.0005,1);
+        
+        % Strip digital data from NEV into new variable
+        disp(['Stripping digital data.'])
+        NEVdigital.TimeStamp = NEVNSx_all.NEV.Data.SerialDigitalIO.TimeStamp;
+        NEVdigital.TimeStampSec = NEVNSx_all.NEV.Data.SerialDigitalIO.TimeStampSec;
+        NEVdigital.InsertionReason = NEVNSx_all.NEV.Data.SerialDigitalIO.InsertionReason;
+        NEVdigital.UnparsedData = NEVNSx_all.NEV.Data.SerialDigitalIO.UnparsedData;
+        NEVNSx_all.NEV.Data.SerialDigitalIO.TimeStamp=[];
+        NEVNSx_all.NEV.Data.SerialDigitalIO.TimeStampSec=[];
+        NEVNSx_all.NEV.Data.SerialDigitalIO.InsertionReason=[];
+        NEVNSx_all.NEV.Data.SerialDigitalIO.UnparsedData=[];
+        save([file_path file_prefix '-digital'],'NEVdigital');
+        
+        % Save spikes only data
         saveNEVSpikesLimblab(NEVNSx_all.NEV, file_path, [file_prefix '-spikes.nev'])
+        
+        % Save metatags for unmerging
         MetaTags = NEVNSx_all.MetaTags;
         save([file_path file_prefix '-spikes-metatags'],'MetaTags')
         status = 'merged spikes';
     else
         % Un-merge
         load([file_path file_prefix '-spikes-metatags'],'MetaTags')
-        if exist([file_path file_prefix '-spikes-s.nev'],'file')        
+        if exist([file_path file_prefix '-spikes-s.nev'],'file')
             NEV_sorted = openNEVLimblab('read',[file_path file_prefix '-spikes-s.nev']);
+            if exist([file_path file_prefix '-digital.mat'],'file')
+                disp('Re-integrating digital data.')
+                load([file_path file_prefix '-digital'],'NEVdigital')
+                NEV_sorted.Data.SerialDigitalIO.TimeStamp = NEVdigital.TimeStamp;
+                NEV_sorted.Data.SerialDigitalIO.TimeStampSec = NEVdigital.TimeStampSec;
+                NEV_sorted.Data.SerialDigitalIO.InsertionReason = NEVdigital.InsertionReason;
+                NEV_sorted.Data.SerialDigitalIO.UnparsedData = NEVdigital.UnparsedData;
+                delete([file_path file_prefix '-digital.mat'])
+            end
+            
             disp(['Separating ' num2str(length(MetaTags.NEVlist) ) ' files.'])
             for iFile = 1:length(MetaTags.NEVlist) 
                 t_offset = (MetaTags.FileStartSec(iFile))*30000;
