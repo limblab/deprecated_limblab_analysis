a82ca26067b0f5a91e26e2aac08a08b6d857c585
Tucker Tomlinson <tuckertomlinson@gmail.com>
2016-01-08 18:19:25 -0600

    merged Ricardo's updated encoder parsing to get rid of dropped encoder points into new data processing code

--- proc/Tucker/newDataProcessing/strobed2encoder.m
+++ proc/Tucker/newDataProcessing/strobed2encoder.m
@@ -33,12 +33,71 @@ if (size(strobed_events,2) ~= 2)
     error('get_encoder:BadMatrix','input strobed_events must be a two column matrix');
 end
 
-% get time-stamps of the first strobe in a set of four
-ts = strobed_events(:,1);
-ts_index = find( diff(ts) > .000275 )+1;
-ts_index = ts_index( diff(ts_index) == 4 ); % throw out bad points
-time_stamps = ts( ts_index );
+% Old encoding scheme would create problems whenever two consecutive bytes
+% had the exact same value (the second one was not recorded). The new
+% scheme is more complicated but it gets rid of the problem. The first bit
+% of every byte is a "clock" signal that is toggled between bytes. The
+% encoder data is encoded in five bytes as follows:
+% Bit 1 of every encoder is most significant bit, bit 16 is least
+% significant.
+% Byte 1 = [clock encoder1(1:7)];
+% Byte 2 = [clock encoder1(8:14)];
+% Byte 3 = [clock encoder1(15:16) encoder2(1:5)];
+% Byte 4 = [clock encoder2(6:12)];
+% Byte 5 = [clock encoder2(13:16) 0 0 0];
 
+% New encoding mode sends a new byte faster, every 125 usec.
+if mode(diff(strobed_events(:,1))) < 0.00016
+    encoding_scheme = 1;
+else
+    encoding_scheme = 0;
+end
+
+if encoding_scheme == 0
+    % Get rid of repeated numbers
+    strobed_events(diff(strobed_events(:,1))<1.9E-4,:) = []; 
+    
+    % get time-stamps of the first strobe in a set of four
+    ts = strobed_events(:,1);
+    
+    ts_index = find( diff(ts) > .000275 )+1;
+    ts_index(diff(ts(ts_index))<.0009) = [];
+
+    % Fix strobed events
+    ts_index = ts_index( diff(ts_index) == 4 ); % throw out bad points
+    time_stamps = ts( ts_index );
+
+    % assemble encoder signals
+    encoder = zeros(length(ts_index)-2, 3);
+
+    if (length(ts_index)-2>=1)
+        encoder(:,1) = time_stamps(1:end-2);
+        encoder(:,2) = strobed_events(ts_index(1:end-2),2) + strobed_events(ts_index(1:end-2)+1,2)*2^8 - 32765;
+        encoder(:,3) = strobed_events(ts_index(1:end-2)+2,2) + strobed_events(ts_index(1:end-2)+3,2)*2^8 - 32765;
+    end
+elseif encoding_scheme == 1
+    % Get rid of repeated numbers
+    strobed_events(diff(strobed_events(:,1))<5E-5,:) = [];  
+
+    % get time-stamps of the first strobe in a set of four
+    ts = strobed_events(:,1);
+    
+    byteDec = bitand(strobed_events(:,2),127);
+    ts_index = find( diff(ts) > .0004 )+1;
+    ts_index(diff(ts(ts_index))<.0008) = [];
+
+    % Fix strobed events
+    ts_index = ts_index( diff(ts_index) == 5 ); % throw out bad points
+    ts_index = ts_index(1:end-1);
+    time_stamps = ts( ts_index );
+    
+    byteMat = [byteDec(ts_index) byteDec(ts_index+1) byteDec(ts_index+2) byteDec(ts_index+3) byteDec(ts_index+4)];
+    
+    s_dec = (bitand(byteMat(:,1),127)*2 + bitshift(byteMat(:,2),-6))*2^8 + bitand(byteMat(:,2),63)*4 + bitshift(byteMat(:,3),-5) - 32765;
+    e_dec = (bitand(byteMat(:,3),31)*8 + bitshift(byteMat(:,4),-4))*2^8 + bitand(byteMat(:,4),15)*16 + bitshift(byteMat(:,5),-3) - 32765;
+    
+    encoder = [time_stamps(1:end-2) s_dec(1:end-2) e_dec(1:end-2)];
+end
 %make mask vector to use as flag for ignoring timepoints
 mask=ones(size(time_stamps));
 temp=[];
@@ -52,14 +111,6 @@ if ~isempty(ignore_windows)
         temp=[temp;[min(range):max(range)]'];
     end
     mask(temp)=0;
-end
-
-% assemble encoder signals
-encoder = zeros(length(ts_index)-2, 3);
-if (length(ts_index)-2>=1)
-    encoder(:,1) = time_stamps(1:end-2);
-    encoder(:,2) = strobed_events(ts_index(1:end-2),2) + strobed_events(ts_index(1:end-2)+1,2)*2^8 - 32765;
-    encoder(:,3) = strobed_events(ts_index(1:end-2)+2,2) + strobed_events(ts_index(1:end-2)+3,2)*2^8 - 32765;
 end
 
 %fix steps in encoder 1
