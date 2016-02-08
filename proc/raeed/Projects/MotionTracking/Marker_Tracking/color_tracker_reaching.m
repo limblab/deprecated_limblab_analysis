b2cfdd1a06706686718e9e317311fc7e228d80df
jglaser2 <joshglaser88@gmail.com>
2016-02-08 10:22:10 -0600

    Color tracking for reaching task
    Whistlepig data

---
+++ proc/raeed/Projects/MotionTracking/Marker_Tracking/color_tracker_reaching.m
@@ -0,0 +1,15 @@
+%Get arm positions (normally)
+%Maybe I don't have to correct for the elbow??
+
+%Get approximate distances to hand points from elbow
+%--based on a few examples I type in
+
+%Get hand markers based on these approximate distances (assignments will be wrong)
+
+%Determine better hand marker distance metrics from elbow
+
+%Get hand markers again
+%--Only do it when we have the elbow markers
+%--Start a segment when there are at least 3 markers
+%--Stop the segment when there is 1 marker (or 0)
+%--Do assignments like usual (but w/ extra blue marker)
