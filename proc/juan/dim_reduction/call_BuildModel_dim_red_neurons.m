472a1285ff40b847cc4aa7d8bc8498286c89336e
jotagalabe <jotagalabe@gmail.com>
2016-02-09 15:53:15 -0600

    Fixed minor bugs

--- proc/juan/dim_reduction/call_BuildModel_dim_red_neurons.m
+++ proc/juan/dim_reduction/call_BuildModel_dim_red_neurons.m
@@ -1,6 +1,6 @@
 
 
-function [filter, varargout] = BuildModel_dim_red_neurons( binned_data, dim_red_FR, nbr_dims )
+function [filter, varargout] = call_BuildModel_dim_red_neurons( binned_data, dim_red_FR, nbr_dims, model_opts )
 
 
 % check bin width PC firing rates and EMGs is the same
@@ -11,12 +11,21 @@ if abs( bin_size_red_FR - bin_size_EMGs ) > 1E-6
     error('bin size neurons and EMG data has to be the same');
 end
 
-% Parameters to build the decoder
+% Parameters to build the decoder -- add as optional parameters
 options                     = ModelBuilding_dim_red_neurons_Default();
 % decode EMGs
 options.PredEMGs            = true;
 % use nbr_dims PCs in neural space
 options.numPCs              = nbr_dims;
+% overwrite other params, if passed
+if nargin == 4
+    if isfield(model_opts,'fillen')
+        options.fillen      = model_opts.fillen;
+    end
+    if isfield(model_opts,'PolynomialOrder')
+        options.PolynomialOrder     = model_opts.PolynomialOrder;
+    end
+end
 
 
 % add the PC transformed neural data that will be used for building the
