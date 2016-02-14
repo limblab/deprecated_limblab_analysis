f32143da42633ef2cf3de4e5b6056efa1e415315
jotagalabe <jotagalabe@gmail.com>
2016-02-14 01:14:32 -0600

    Added the possibility of passing as input the eigenvectors that the define the PC hyperplane for each task
    The function can take a matrix with the specific eigenvectors that
    define the hyperplane for each task. The reason is that in some tasks
    they may not appear in the same order, and therefore hide similarities.
    For example, this seems to happen when comparing 1D to 2D lab tasks.
    Tested (preliminarily) and working

--- proc/juan/dim_reduction/angle_btw_pcs.m
+++ proc/juan/dim_reduction/angle_btw_pcs.m
@@ -8,7 +8,10 @@
 %       basis_1             : eigenvectors that define hyperplane #1
 %       basis_2             : eigenvectors that define hyperplane #2
 %       (nbr_dims)          : [min(size(eigen_1,eigen_2))] number of
-%                               dimensions of the neural space 
+%                               dimensions of the neural space [scalar], or
+%                             2-D array with the eigenvectors in basis 1
+%                             (1st col) and basis 2 (2nd col) which angle
+%                             will be calculated
 %       (show_plots)        : [false] show plot that compares the eigenvals
 %                               for each pair of spaces
 %
@@ -16,9 +19,16 @@
 function angle = angle_btw_pcs( basis_1, basis_2, varargin ) 
 
 % read input parameters
+
 if nargin >= 3
-    % read number eigenvalues to define the hyperplane
-    nbr_dims            = varargin{1};
+    % argument 3 can be a vector or an array
+    if isscalar(varargin{1})
+        nbr_dims        = varargin{1};
+    elseif ismatrix(varargin{1})
+        nbr_dims        = size(varargin{1},1);
+        eigenvs_1       = varargin{1}(:,1);
+        eigenvs_2       = varargin{1}(:,2);
+    end
 else
     % if not specified, calculate the angle between the hyperplane in the
     % space with the minimum common nbr. of dimensions
@@ -32,17 +42,25 @@ end
 if nargin == 4
     show_plot            = varargin{2}; 
 else
-    show_plot            = true;
+    show_plot            = false;
 end
 
 
+% ------------------------------------------------------------------------
 % calculate angle between hyperplanes
-angle                   = subspace(basis_1(:,1:nbr_dims),basis_2(:,1:nbr_dims));
+
+if exist('eigenvs_1','var')
+    angle               = subspace(basis_1(:,eigenvs_1),basis_2(:,eigenvs_2));
+else
+    angle               = subspace(basis_1(:,1:nbr_dims),basis_2(:,1:nbr_dims));
+end
 
 
+% ------------------------------------------------------------------------
 % plot the relationship between eigenvalues and the angle
 if show_plot
-    figure, plot(basis_1(:,1:nbr_dims),basis_2(:,1:nbr_dims),'ob', 'markersize', 12)
+    figure, plot(basis_1(:,1:nbr_dims),basis_2(:,1:nbr_dims),'marker','o',...
+        'markersize', 12, 'linestyle','none')
     set(gca,'TickDir','out'), set(gca,'FontSize',14);
     xlabel('eigen values space 1','Fontsize',14);
     ylabel('eigen values space 2','Fontsize',14);
