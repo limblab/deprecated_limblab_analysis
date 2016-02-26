e1194b4ab4810f21b0599ae25c7536e17b99482e
maripeza <mkharp@gmail.com>
2016-02-26 17:44:16 -0600

    corrosion testing to imitate actual stimulation sessions

--- control_code/fns_program.m
+++ control_code/fns_program.m
@@ -45,8 +45,43 @@ optargs(1:numvarargs) = varargin;
 
 % Place optional args in memorable variable names
 [polarity, int_time, no_pulses, run_time] = optargs{:};
+polarity = int2str(polarity); 
 
-%
+% Convert channel list to the necessary string format
+chSTR = get_chSTR(ch_list);
+
+% Convert all values into the inputs for the FNS
+I1 = int2str(amp*1e3);              % Pulse amplitude, phase 1 (uAmps)
+I2 = I1;                            % Pulse amplitude, phase 2 (uAmps)
+T1 = int2str(pw*1e3);               % Pulse width of phase 1 (uSec)
+T2 = T1;                            % Pulse width of phase 2 (uSec)
+TIP = int2str(stim_tip*1e3);        % Inter-phase time (uSec)
+TT = int2str((1/freq)*1e6);         % Repetition time (uSec)
+NP = int2str(pulses);               % number of pulses in train
+TTR = int2str(time2run);            % time for train to run in msec
+        
+
+% Choose the appropriate type of program to send
+switch action
+    case {0}
+        %program to follow; load and run immediately
+    case {1}
+        %run all channels with programs; one character command
+    case {2}
+        %halt all running channels; one char command
+    case {3}
+        %run a list of channels
+    case {4} 
+        %halt a list of channels
+    case {5}
+        %program to follow; load but do not run ***
+    case {8}
+        %turn off high-voltage outputs; one char command
+    case {9}
+        %same action as 8
+end
+
+
 
 %TODO copy - 
 %current phases from amp
