6b1111df95de2c972bbba88f4563249ca74b8a37
Tucker Tomlinson <tuckertomlinson@gmail.com>
2016-05-04 14:59:06 -0500

    updated firing rate and binning computation to sanitize time by rouding off extraneous precision. Added method to unitData to return string with formatted unit name. added function to return sanitized time by rounding off extraneous precision

--- proc/Tucker/newDataProcessing/@experiment/binData.m
+++ proc/Tucker/newDataProcessing/@experiment/binData.m
@@ -61,7 +61,7 @@ function binData(ex,varargin)
         end
         if isempty(bins)
             %if we don't have a time column yet
-            bins=table(temp(:,1),'VariableNames',{'t'});
+            bins=table(roundTime(temp(:,1)),'VariableNames',{'t'});
         end
         %now append the non-time columns of the current data set to the
         %continousBins:
@@ -92,7 +92,17 @@ function binData(ex,varargin)
             ex.calcFiringRate;
         end
         %get indices of columns in the FR matrix that we want:
-        unitCols=[0,ex.binConfig.include(unitIdx).which]+1;%shift by 1 since the firing rate matrix has time as the first column but otherwise follows the units in order. including zero before the shift means we include the time column
+        if ~isempty(ex.binConfig.include(unitIdx).which)
+            unitCols=zeros(1,numel(ex.binConfig.include(unitIdx).which)+1);
+            unitCols(1)=find(strcmp('t',ex.firingRate.data.Properties.VariableNames));
+            for i=1:numel(ex.binConfig.include(unitIdx).which)
+                unitNum=ex.binConfig.include(unitIdx).which(i);
+                unitCols(i+1)=find(strcmp(ex.units.getUnitName(unitNum),ex.firingRate.data.Properties.VariableNames));
+            end
+        else
+            unitCols=1:size(ex.firingRate.data,2);
+        end
+        
         %decimate the Fr data if necessary:
         if ex.binConfig.filterConfig.sampleRate<ex.firingRate.meta.sampleRate
             temp=decimateData(ex.firingRate.data{:,unitCols},ex.binConfig.filterConfig);
@@ -130,21 +140,9 @@ function binData(ex,varargin)
         contEnd=find(bins.t<=tEnd,1,'last');
         FRStart=find(temp.t>=tStart,1);
         FREnd=find(temp.t<=tEnd,1,'last');
-        %get the indicees of units that we want to include in the bin
-        %table:
-        if isempty(ex.binConfig.include(unitIdx).which)
-            incMask=true(numel(ex.units.data),1);
-        else
-            incMask=false(numel(ex.units.data),1);
-            incMask(ex.binConfig.include(unitIdx).which)=true;
-        end
-        %strip rows from the mask that would be invalidated spikes:
-        incMask([ex.units.data.ID]==255)=[];
-        %get indexes and shift by 1 to account for the time column in the
-        %temp table:
-        incMask=find(incMask)+1;
+        
         %put the joint continuous and unit bins into ex.bin.data
-        ex.bin.updateBins([bins(contStart:contEnd,:),temp(FRStart:FREnd,incMask)])
+        ex.bin.updateBins([bins(contStart:contEnd,:),temp(FRStart:FREnd,~strcmp('t',temp.Properties.VariableNames))])
     else
         %if no units flag was evident, just put the analog data into bins
         ex.bin.updateBins(bins);
