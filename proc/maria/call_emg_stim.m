58f24b287b786c497f526d1b88064554569c7dbf
maripeza <mkharp@gmail.com>
2016-06-03 14:48:57 -0500

    muscles to stimulate 16-6-3

--- proc/maria/call_emg_stim.m
+++ proc/maria/call_emg_stim.m
@@ -24,10 +24,10 @@ goodChannelsWithBaselines =  [ 1 ,  0 ,  0 , 0 , 1 , 1 ,  1 ,  0 , 1 , 1 , 0 , 0
     1 ,  0 ,  1 , 1 , 0 , 1 ,  1 ,  1 , 1 , 1 , 0 , 0 , 1 , 1 , 1 ];
 
 animals = [1:8];
-muscles = [1 3 4 9 6 8 12];
+muscles = [1 3 4 6 8 9 12 15];
 n = 4;
 Wn = 30/(5000/2); %butter parameters (30 Hz)
-channels = [1 2 3 4 5 6 7];
+channels = [1 2 3 4 5 6 7 8];
 freq = 20; %hz
 pw = .2; %ms
 
@@ -51,10 +51,10 @@ end
 
 
 %define thresholds
-emglow_limit = [.15 .13 .2 .1 .1 .2]; %get rid of low noise
-emghigh_limit = [1 1 1 1 1 1]; %get rid of excessively high spikes
-amplow_limit = [.7 .6 .9 .5 .2 .8]; %lowest level of stim to twitch (err on low side)
-amphigh_limit = [3 2 1.8 1.7 1 2.7];  %highest level of stim to use
+emglow_limit = [.15 .13 .13 .13 .13 .13 .13 .13]; %get rid of low noise
+emghigh_limit = [1 1 1 1 1 1 1 1]; %get rid of excessively high spikes
+amplow_limit = [.5 .5 .5 .5 .5 .5 .5 .5]; %lowest level of stim to twitch (err on low side)
+amphigh_limit = [3 3 3 3 3 3 3 3];  %highest level of stim to use
 
 %check that limits are all defined
 lm = length(channels);
