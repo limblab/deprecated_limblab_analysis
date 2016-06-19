17ac68074e65f3debb54eb925b86ed6b5c6b3a7c
Raeed Chowdhury <rhcggb@gmail.com>
2016-06-19 18:49:03 -0500

    add some functionality to fit_and_predict

--- proc/raeed/Projects/two_workspace/fit_and_predict.m
+++ proc/raeed/Projects/two_workspace/fit_and_predict.m
@@ -1,7 +1,8 @@
 % fit to each coordinate in one workspace and predict the PD in the next
 
 %% read files in
-folder = '/home/raeed/Projects/limblab/FSMRes/limblab/User_folders/Raeed/Arm Model/Data/Chips/experiment_20151203_RW_002/';
+% folder = '/home/raeed/Projects/limblab/FSMRes/limblab/User_folders/Raeed/Arm Model/Data/Chips/experiment_20151203_RW_002/';
+folder = 'Z:\limblab\User_folders\Raeed\Arm Model\Data\Chips\experiment_20151203_RW_002\';
 prefix =  'Chips_20151203_RW_002';
 labnum =  6;
 opensim_prefix = 'Chips_20151203_0-320';
@@ -33,6 +34,46 @@ clear muscle_pos_mat
 
 % use first 5 PCs
 muscle_scores = score(:,1:5);
+
+%% Get kinematics
+t = bdf.pos(:,1);
+pos = bdf.pos(:,2:3);
+vel = bdf.vel(:,2:3);
+spd = sqrt(sum(vel.^2,2));
+endpoint_kin = [pos vel spd];
+
+% toss everything outside of time window
+endpoint_kin( t<time_window(1) | t>time_window(2), :) = [];
+
+% clear unneeded things
+clear t
+clear pos
+clear vel
+clear spd
+
+%% Fit neurons to endpoint
+bootfunc = @(X,y) GeneralizedLinearModel.fit(X,y,'Distribution','poisson');
+
+tic
+for unit_ctr = 1:length(bdf.units)
+    % skip unsorted or invalid units
+    if bdf.units(unit_ctr).id(2)==0 || bdf.units(unit_ctr).id(2)==255
+        continue;
+    end
+    
+    % get unit id
+    uid = bdf.units(unit_ctr).id;
+    
+    % get firing rate
+    FR = bdf.units(unit_ctr).FR(:,2);
+    FR_t = bdf.units(unit_ctr).FR(:,1);
+    FR(FR_t<time_window(1) | FR_t>time_window(2)) = [];
+    
+    endpoint_tuning = calc_PD_helper(bootfunc,endpoint_kin,FR,['Calculating endpoint chan ' num2str(uid(1)) ', unit ' num2str(uid(2)) ' (Time: ' num2str(toc) ')']);
+    endpoint_curve = get_single_tuning_curve(endpoint_kin(:,3:4),FR);
+    
+    plot_tuning(endpoint_tuning,endpoint_curve,max(FR),[0 0 0],['chan ' num2str(uid(1)) ', unit ' num2str(uid(2)) 'tuning curve'])
+end
 
 %% Fit neuron to muscle in one workspace (bootstrap)
 
--- proc/raeed/Projects/two_workspace/plot_tuning.m
+++ proc/raeed/Projects/two_workspace/plot_tuning.m
@@ -1,6 +1,7 @@
-function [handle] = plot_tuning(modeled_tuning,curve,max_FR,fig_name)
+function [handle] = plot_tuning(modeled_tuning,curve,max_FR,color,fig_name)
 % PLOT_TUNING makes a single figure showing the tuning curve and PD with
-% confidence intervals. Leave either entry blank to skip plotting it.
+% confidence intervals. Leave either entry blank to skip plotting it. Color
+% is a 3 element vector for the color of the plotted tuning curve and PD.
 
 % set up figure
 handle = figure('name',fig_name);
@@ -13,19 +14,19 @@ hold all
 % tuning curve
 if(~isempty(curve))
     h=polar(repmat(curve.bins,2,1),repmat(curve.FR,2,1));
-    set(h,'linewidth',2,'color',[1 0 0])
+    set(h,'linewidth',2,'color',color)
     th_fill = [flipud(curve.bins); curve.bins(end); curve.bins(end); curve.bins];
     r_fill = [flipud(curve.CI_high); curve.CI_high; curve.CI_low; curve.CI_low];
     [x_fill,y_fill] = pol2cart(th_fill,r_fill);
-    patch(x_fill,y_fill,[1 0 0],'facealpha',0.3,'edgealpha',0);
+    patch(x_fill,y_fill,color,'facealpha',0.3,'edgealpha',0);
 end
 
 % PD
 if(~isempty(modeled_tuning))
     h=polar(modeled_tuning.dir,max_FR*[0;1]);
-    set(h,'linewidth',2,'color',[1 0 0])
+    set(h,'linewidth',2,'color',color)
     th_fill = [modeled_tuning.dir_CI(2) modeled_tuning.dir modeled_tuning.dir_CI(1) 0];
     r_fill = [max_FR max_FR max_FR 0];
     [x_fill,y_fill] = pol2cart(th_fill,r_fill);
-    patch(x_fill,y_fill,[1 0 0],'facealpha',0.3);
+    patch(x_fill,y_fill,color,'facealpha',0.3);
 end
