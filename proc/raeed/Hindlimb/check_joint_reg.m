f6ba46f8289f6baa1ddc283af32185d475dcaf0c
Raeed Chowdhury <raeed.chowdhury@gmail.com>
2016-06-30 17:18:08 -0500

    move raeed proc folder over to new repo, proc-raeed.git

--- proc/raeed/Hindlimb/check_joint_reg.m
+++
@@ -1,8 +0,0 @@
-%% checking joint regression consistency
-joint_fit_same = zeros(size(joint_fit_con));
-for i = 1:length(joint_fit_con)
-    con_pred = joint_fit_con{i}.PredictorNames;
-    unc_pred = joint_fit_unc{i}.PredictorNames;
-    
-    joint_fit_same(i) = isempty(setxor(con_pred,unc_pred));
-end
