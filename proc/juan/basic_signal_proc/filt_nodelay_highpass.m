6b4dc903ec1542306c7f649df47b70bceab58e4c
jotagalabe <jotagalabe@gmail.com>
2016-08-03 22:40:04 -0500

    added basic filtering, R2 and VAF fcns --need to be tested & improved

---
+++ proc/juan/basic_signal_proc/filt_nodelay_highpass.m
@@ -0,0 +1,26 @@
+%
+%
+% Function to remove tremor from the data. The input data must be a row
+% vector.
+% By default the sampling rate of the signal is considered to be 1kHz. The
+% cutoff frequency is provided in Hz.
+%
+%   y = filt_nodelay_highpass(x, sampling_freq, cutoff_freq)
+%
+%
+%       
+
+
+function y = filt_nodelay_highpass(x, sampling_freq, cutoff_freq)
+
+
+fs = sampling_freq;
+order = 1;
+
+fc = cutoff_freq; % The cut off freq of the LP filter
+Wn = fc/(fs/2);
+[a b] = butter(order,Wn,'high'); % a, b are the coefficients of the filter
+
+% To filter with no delay
+y_t = filtfilt(a,b,x');
+y = y_t'; % to provide a row vector
